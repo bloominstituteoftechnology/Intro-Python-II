0f4ee8cc6a498faa39c65d483ad5e363da740dc8
Katie Cleary <katiecleary@airmaster.lan>
2019-01-09 15:54:10 -0500

    added rough move logic for player

--- src/player.py
+++ src/player.py
@@ -11,3 +11,23 @@ class Player:
         self.inventory.append(item)
     
     def try_move(self, direction):
+        d = direction + "_to"
+
+        if not hasattr(self.currentRoom, d):
+            print("you can't go that way")
+            return self.currentRoom
+        else:
+            self.currentRoom[d]
+
+
+'''
+room['outside'].n_to = room['foyer']
+room['foyer'].s_to = room['outside']
+room['foyer'].n_to = room['overlook']
+room['foyer'].e_to = room['narrow']
+room['overlook'].s_to = room['foyer']
+room['narrow'].w_to = room['foyer']
+room['narrow'].n_to = room['treasure']
+room['treasure'].s_to = room['narrow']
+
+#'''
