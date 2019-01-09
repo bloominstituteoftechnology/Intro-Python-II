221c36048be7c2ad2637492dde57ddd17914dc91
Kevin <brackkevin@gmail.com>
2019-01-09 15:11:37 -0600

    Creates placeholder classes for Player and Room

--- src/player.py
+++ src/player.py
@@ -1,2 +1,5 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    """A placeholder for the Player class. Needs to be updated"""
--- src/room.py
+++ src/room.py
@@ -1,2 +1,6 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    """A placeholder for the room class. Needs to be updated"""
+    pass
