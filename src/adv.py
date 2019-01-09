7e85235c9ae6d76ee7b367e92a6ae3c870fbcee4
jsmnglng <jsmn.glng@gmail.com>
2019-01-09 13:54:46 -0800

    Made player move north, east, south, west

--- src/adv.py
+++ src/adv.py
@@ -45,9 +45,26 @@ room["treasure"].s_to = room["narrow"]
 # Main
 #
 
+
+def prompt(message):
+    print(f">> {message}")
+
+
 # Make a new player object that is currently in the 'outside' room.
-player = Player("Pepito")
+player_name = input("Enter player name: ")
+player = Player(player_name)
 player.room = room["outside"]
+
+prompt(f"Welcome {player.name}, you are now in the {player.room.name}")
+prompt(f"{player.room.desc}")
+prompt(
+    f"""Commands:
+    N - go to North
+    E - go to East
+    S - go to South
+    W - go to West
+    """
+)
 
 # Write a loop that:
 #
@@ -61,8 +78,41 @@ player.room = room["outside"]
 # If the user enters "q", quit the game.
 
 while True:
-    print(f"Welcome {player.name}, you are now in the {player.room.name}")
-    command = input("What do you want to do? (q to quit): ")
+    command = input("What do you want to do? (q to quit): ").lower()
 
-    if command.lower() == "q":
+    # Make player move NESW
+    if command == "n":
+        if hasattr(player.room, "n_to"):
+            player.room = player.room.n_to
+            prompt(f"You are now in the {player.room.name}")
+            prompt(f"{player.room.desc}")
+        else:
+            prompt("There's no way out here")
+    elif command == "e":
+        if hasattr(player.room, "e_to"):
+            player.room = player.room.e_to
+            prompt(f"You are now in the {player.room.name}")
+            prompt(f"{player.room.desc}")
+        else:
+            prompt("There's no way out here")
+    elif command == "s":
+        if hasattr(player.room, "s_to"):
+            player.room = player.room.s_to
+            prompt(f"You are now in the {player.room.name}")
+            prompt(f"{player.room.desc}")
+        else:
+            prompt("There's no way out here")
+    elif command == "w":
+        if hasattr(player.room, "w_to"):
+            player.room = player.room.w_to
+            prompt(f"You are now in the {player.room.name}")
+            prompt(f"{player.room.desc}")
+        else:
+            prompt("There's no way out here")
+    else:
+        prompt(f"I don't quite understand")
+
+    if command == "q":
         break
+
+prompt("Thank you for playing!")
