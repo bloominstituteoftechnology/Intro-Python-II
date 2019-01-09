2836bfd83fd4ca7be36b72a5727051f1a9f44b45
Kevin <brackkevin@gmail.com>
2019-01-09 15:32:53 -0600

    Created constructor for Player class

--- src/player.py
+++ src/player.py
@@ -2,4 +2,7 @@
 # currently.
 
 class Player:
-    """A placeholder for the Player class. Needs to be updated"""
+    """The main hero player for our text based adventure."""
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
