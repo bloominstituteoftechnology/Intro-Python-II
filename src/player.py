cdfa84e67b0f159cb37da1cf99595d1992e4d79d
jmbradley <jmbradley808@gmail.com>
2019-01-09 16:40:06 -0500

    adding player class and player object representing outside room

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def current_room(self):
+        print("Player is currently in the outside room.")
+
+    def next_room(self):
+        print("Player is moving to the next room.")
