875be6d9700115339416796a2e9e8d850234d3f1
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-01-09 17:53:54 -0500

    extended Room class with items data

--- src/room.py
+++ src/room.py
@@ -3,10 +3,10 @@
 
 class Room:
     # constructor
-    def __init__(self, name, description):
+    def __init__(self, name, description, items = []):
         self.name = name
         self.description = description
+        self.items = items
     # adding a default representation
     def __repr__(self):
         return(f'Location: {self.name}. You see that {self.description}.') # need to add a bit to this that changes first letter of description to lowercase
-        
