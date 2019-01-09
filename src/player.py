aebda5b84eda29fd90703c5cd879f5e7ec84fc3f
jmbradley <jmbradley808@gmail.com>
2019-01-09 17:04:16 -0500

    setting instance variables

--- src/player.py
+++ src/player.py
@@ -3,6 +3,11 @@
 
 
 class Player:
+    def __init__(self, name, current_room, next_move):
+        self.name = name
+        self.current_room = name + 'is in' + current_room
+        self.next_move = name + 'is walking' + next_move
+
     # def current_room(self):
     #     print("Player is currently in the current room.")
     # I think that I need to define the methods more generally and then use the specific objects to reference the generalized methods.
