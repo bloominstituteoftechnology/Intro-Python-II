abec32793658f1d1f56a37441845d475a7a7b854
Conner Alegre <cga0@pm.me>
2019-01-09 14:10:22 -0700

    Add inventory to Player

--- src/player.py
+++ src/player.py
@@ -6,3 +6,7 @@ class Player:
     def __init__(self, name, current_room = "outside"):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
+
+    def add_item_to_inventory(self, item):
+        self.inventory.append(item)
