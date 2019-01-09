28cac315323fffec26f7855486e26b2fcb002e20
David247365 <ds051128@gmail.com>
2019-01-09 16:25:57 -0600

    Initial Commit and working on player and room classes

--- src/player.py
+++ src/player.py
@@ -1,2 +1,19 @@
+from room import Room
+
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+     # Here we are describing our players attributes.
+    def __init__(self, currentRoom, health):
+        self.currentRoom = currentRoom
+        self.health = 100
+        self.inventory = []
+        self.movement_speed = 10
+    # Here we are defining the methods for the Player class
+
+    def pickup_item(self, item):
+        self.inventory.append(item)
+
+    # def try_move(self, direction):
