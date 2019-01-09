2497706f71d6b4113c33907e114f1642d43abb06
Kevin Sooter <kevin.m.sooter@gmail.com>
2019-01-09 15:20:34 -0600

    Completed room class.  Getting a couple errors at the moment.

--- src/room.py
+++ src/room.py
@@ -1,2 +1,24 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init__(self, name, description, items=[]):
+        self.name = name
+        self.description = description
+        self.items = items
+
+    def room_direction(self, direction):
+        if direction == 'n':
+            return self.n_to
+        elif direction == 's':
+            return self.s_to
+        elif direction == 'w':
+            return self.w_to
+        elif direction == 'e':
+            return self.e_to
+        else:
+            return None
+
+    def __repr__(self):
+        return str(f'{self.name}')
