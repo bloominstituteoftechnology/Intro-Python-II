270c65c0cf3011f7e238424460f520cbf70d98a2
Ryan Walker <rytwalker@gmail.com>
2019-01-09 17:15:27 -0500

    Linked up the rooms

--- src/adv.py
+++ src/adv.py
@@ -52,10 +52,38 @@ while True:
     # * Waits for user input and decides what to do.
     move = input('Where are you going? n, s, e, or w? ')
     # If the user enters a cardinal direction, attempt to move to the room there.
-    if move == 'n' and room_key == 'outside':
-        room_key = room[player.current_room].n_to
-        room_name = room[player.current_room].n_to.name
-        room_desc = room[player.current_room].n_to.description
+    if room_key == 'outside' and move == 'n':
+        room_name = room[room_key].n_to.name
+        room_desc = room[room_key].n_to.description
+        room_key = [key for key in room.keys() if key == 'foyer'][0]
+    elif room_key == 'foyer' and move == 's':
+        room_name = room[room_key].s_to.name
+        room_desc = room[room_key].s_to.description
+        room_key = [key for key in room.keys() if key == 'outside'][0]
+    elif room_key == 'foyer' and move == 'n':
+        room_name = room[room_key].n_to.name
+        room_desc = room[room_key].n_to.description
+        room_key = [key for key in room.keys() if key == 'overlook'][0]
+    elif room_key == 'foyer' and move == 'e':
+        room_name = room[room_key].e_to.name
+        room_desc = room[room_key].e_to.description
+        room_key = [key for key in room.keys() if key == 'narrow'][0]
+    elif room_key == 'overlook' and move == 's':
+        room_name = room[room_key].s_to.name
+        room_desc = room[room_key].s_to.description
+        room_key = [key for key in room.keys() if key == 'foyer'][0]
+    elif room_key == 'narrow' and move == 'w':
+        room_name = room[room_key].w_to.name
+        room_desc = room[room_key].w_to.description
+        room_key = [key for key in room.keys() if key == 'foyer'][0]
+    elif room_key == 'narrow' and move == 'n':
+        room_name = room[room_key].n_to.name
+        room_desc = room[room_key].n_to.description
+        room_key = [key for key in room.keys() if key == 'treasure'][0]
+    elif room_key == 'treasure' and move == 's':
+        room_name = room[room_key].s_to.name
+        room_desc = room[room_key].s_to.description
+        room_key = [key for key in room.keys() if key == 'narrow'][0]
     elif move == 'q':
         # If the user enters "q", quit the game.
         print('Game over! Byeee')
@@ -63,4 +91,3 @@ while True:
     else:
         # Print an error message if the movement isn't allowed.
         print('There is nothing over there. Try another direction: ')
-        continue
