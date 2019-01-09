18bde59ae403c79a8ff5db0e73905f91463ef390
Katie Cleary <katiecleary@airmaster.lan>
2019-01-09 15:37:44 -0500

    initial player and room class setup

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
+from room import Room
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, currentRoom):
+        self.currentRoom = currentRoom
+        self.inventory = []
+
+    def pickup_item(self, item):
+        self.inventory.append(item)
+    
+    def try_move(self, direction):
