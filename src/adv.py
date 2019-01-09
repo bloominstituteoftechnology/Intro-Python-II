0804ee1fe236d1a5867e12c6c6703a0fa71b402c
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-01-09 15:33:29 -0700

    Add Movement And Rename Room to Location

--- src/adv.py
+++ src/adv.py
@@ -3,45 +3,45 @@ import os
 import textwrap
 
 # Local Imports
-from room import Room
+from location import Location
 from player import Player
 from item import Item
 
-# Declare all the rooms
+# Declare all the locations
 
-room = {
-    'outside':  Room("Outside Cave Entrance",
+location = {
+    'outside':  Location("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer':    Location("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Location("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Location("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Location("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
 
-# Link rooms together
+# Link locations together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+location['outside'].n_to = location['foyer']
+location['foyer'].s_to = location['outside']
+location['foyer'].n_to = location['overlook']
+location['foyer'].e_to = location['narrow']
+location['overlook'].s_to = location['foyer']
+location['narrow'].w_to = location['foyer']
+location['narrow'].n_to = location['treasure']
+location['treasure'].s_to = location['narrow']
 
 # Declare Player
-player = Player('Chosen One', room['outside'])
+player = Player('Chosen One', location['outside'])
 
 # Centers Header Text
 
@@ -53,6 +53,25 @@ player = Player('Chosen One', room['outside'])
 clear = lambda: os.system('cls')
 clear()
 
+# Enter to Continue
+def enter_to_continue():
+    input('--\nPress Enter to Continue...')
+
+print(r'''   
+             /\    
+            /  \   
+           / /\ \  
+          / /  \ \ 
+         /_/    \_\
+
+Welcome to Lambda Adventure! 
+============================
+''')
+
+player_name = input('Please enter your name: ')
+player.name = player_name
+
+print(chr(27) + "[2J") # Scroll screen down so that the next loop begins at the top of the screen.
 
 print(r'''   
              /\    
@@ -70,41 +89,44 @@ Enter 'q' to quit the game.
 ---
 ''')
 
-player_name = input('Please enter your name: ')
-player.name = player_name
-
-print(chr(27) + "[2J") # Scroll screen down so that the next loop begins at the top of the screen.
-
 #
 # Main
 #
 
-# Make a new player object that is currently in the 'outside' room.
+# Make a new player object that is currently in the 'outside' location.
 
 # Write a loop that:
 #
-# * Prints the current room name
+# * Prints the current location name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
 #
-# If the user enters a cardinal direction, attempt to move to the room there.
+# If the user enters a cardinal direction, attempt to move to the location there.
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
 
 while True:
 
-    # Room Name
+    # Location Name
     print(f'Location: {player.location.name} \n')
 
-    # Room Description
+    # Location Description
     print(f'Description: {player.location.description}')
     
 
-    # Main Input - Convert to Lowercase
+    # Main Input + Convert to Lowercase and a List
     print('________________________________________________________________________________')
     command = input("Enter Command Here: ")
-    command = command.lower()
+    command = command.lower().split(" ", 1)
+
+    # Define Command Action
+    command_action = command[0]
+
+    if len(command) == 1:
+        command_object = ""
+    else:
+        command_object = command[1]
 
     #
     # Commands --------------------------------------------------------------------------------
@@ -115,11 +137,61 @@ while True:
     #
 
     # Commands to Quit Game
-    if command in ["q", "quit", "esc", "end"]:
+    if command_action in ("q", "quit", "esc", "end"):
         break
     
     #
     # Movement Commands
-    #     
+    #
+
+    move_commands = ("walk", "go", "move", "travel", "venture", "proceed", "n", "s", "e", "w")
+    move_directions = {
+        'north': ("northward", "north", "n"),
+        'south': ("southward", "south", "s"),
+        'east': ("eastward", "east", "e"),
+        'west': ("westward", "west", "w")
+    }
+    
+    # Movement Error Message
+    def invalid_direction():
+        print(chr(27) + "[2J") # Scroll screen down so that the next loop begins at the top of the screen.
+        print('You are unable to see a way to move in that direction!')
+        enter_to_continue()
+
+    # Check if Command Was a Movement Command
+    if command_action in move_commands:
+        
+        # Check Movement Type and Whether Movement is Valid
+        
+        # North
+        if command_action == "n" or command_object in move_directions['north']:
+            if hasattr(player.location, 'n_to'):
+                player.location = player.location.n_to
+            else:
+                invalid_direction()
+
+        # South 
+        elif command_action == "s" or command_object in move_directions['south']:
+            if hasattr(player.location, 's_to'):
+                player.location = player.location.s_to
+            else:
+                invalid_direction()
+
+        # East
+        elif command_action == "e" or command_object in move_directions['east']:
+            if hasattr(player.location, 'e_to'):
+                player.location = player.location.e_to
+            else:
+                invalid_direction()
+
+        # West
+        elif command_action == "w" or command_object in move_directions['west']:
+            if hasattr(player.location, 'w_to'):
+                player.location = player.location.w_to
+            else:
+                invalid_direction()
+        
+        else:
+            invalid_direction()
 
     print(chr(27) + "[2J") # Scroll screen down so that the next loop begins at the top of the screen.
--- src/location.py
+++ src/location.py
@@ -1,7 +1,7 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
-class Room:
+class Location:
   def __init__(self, name, description):
     self.name = name
     self.description = description
