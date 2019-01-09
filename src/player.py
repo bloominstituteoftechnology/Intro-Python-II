83905a83bf60e0add8920536f760d962d1a5268d
lndubose <lndubose13@gmail.com>
2019-01-09 17:01:58 -0600

    Made Player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
