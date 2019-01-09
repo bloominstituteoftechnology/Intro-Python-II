c8f0d83cbe1a3bcb7f97d2f155d3c96333163013
codejoncode <jonathanjholloway@gmail.com>
2019-01-09 16:54:53 -0600

    modified the item.py file adding LightSource and Treasure classes

--- src/item.py
+++ src/item.py
@@ -21,3 +21,37 @@ class Item (object):
         returns a string upon the user dropping an item.
         """
         return "No use for this item right now but remember where you dropped it. Might come in handy later.."
+class Treasure(Item):
+    def __init__(self,value, name, description):
+        """
+        initialized value, name and description on the Treasure class. 
+        """
+        self.value = value
+        super(Treasure,self).__init__(name, description)
+    def on_take(self):
+        """
+        returns  string for when user takes a treasure
+        """
+        return "Points collected maybe time to dump this sucker."
+    def on_drop(self):
+        """
+        returns a string for when the user drops treasure.
+        """
+        return "Well you have already collected the points."
+    class LightSource(Item):
+        def __init__(self, name, description):
+            """
+            Initialize the name and description for a LightSource
+            """
+            self.light = True
+            return super(LightSource, self).__init__(name, description)
+        def on_take(self):
+            """
+            returns a string for when a user takes a light source. 
+            """
+            return "Keep your light with you at all times."
+        def on_drop(self):
+            """
+            returns a string for when a user drops a light source.
+            """
+            return "It's not wise to drop your source of light!"
