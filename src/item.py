7e5018b659238720cfe19f4f7062e1c258d3e56c
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-01-09 17:36:09 -0500

    added default representation for Item class

--- src/item.py
+++ src/item.py
@@ -3,4 +3,7 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+    # default representation
+    def __repr__(self):
+        return(f'{self.name} has description: {self.description}')
     
