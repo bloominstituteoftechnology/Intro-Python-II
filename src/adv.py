69f63aceb49510f492241c3d7d37551c67276d3d
jmbradley <jmbradley808@gmail.com>
2019-01-09 17:17:40 -0500

    setting instances in Room class

--- src/adv.py
+++ src/adv.py
@@ -39,7 +39,7 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
-player.current_room = 'outside'
+room.current_room = 'outside'
 
 # Write a loop that:
 #
--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init__(self, room_name, room_description, current_room, next_move, next_room):
+        self.room_name = room_name
+        self.room_description = room_description
+        self.current_room = current_room
+        self.next_move = next_move
+        self.next_room = next_room
