bd970c1cb349cbfe04bc0d56a1d8e0e9dc8c8fc9
Jeanette <jeanfern555@gmail.com>
2019-01-09 17:02:21 -0600

    Added basic player class to player.py

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    '''
+     base class for player
+    '''
+    def __init__(self, currentRoom, inventory):
+        self.currentRoom = currentRoom
+        self.inventory = inventory
