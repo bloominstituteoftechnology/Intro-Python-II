6a2f11ee54ae680ded7a0abc37d6132cd25d7753
Jeanette <jeanfern555@gmail.com>
2019-01-09 16:54:21 -0600

    Added basic room constructor in room.py

--- src/room.py
+++ src/room.py
@@ -1,2 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    '''
+     base class for rooms
+    '''
+    def __init__(self, name, desc):
+        self.name = name
+        self.desc = desc
