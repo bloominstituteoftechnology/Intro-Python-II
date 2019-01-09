b899e7b212abb0f38c54d8613593273fe4015b46
Troy Amlee <troyamlee19@gmail.com>
2019-01-09 16:05:35 -0600

    Troy Amlee [initial commit]

--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, name, description, light, is_inside):
+        self.name = name
+        self.description = description
+        self.light = light
+        self.is_inside = is_inside
+
+    def __repr__(self):
+        return f"<You Are In {self.name}"
