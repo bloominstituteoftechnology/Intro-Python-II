82b5293dff69d8c41adbb72d1e2716706101dfc7
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-01-09 17:05:40 -0500

    added default representation to Room class

--- src/room.py
+++ src/room.py
@@ -6,3 +6,7 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+    # adding a default representation
+    def __repr__(self):
+        return(f'Location: {self.name}. You see that {self.description}.') # need to add a bit to this that changes first letter of description to lowercase
+        
