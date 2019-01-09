eb40862fd6e395f0b46f6b131c608867ff3569b5
Lee Len Formento <leebatef@gmail.com>
2019-01-09 15:47:58 -0500

    Room class and addItem() created

--- src/room.py
+++ src/room.py
@@ -1,3 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
-##
+
+class Room():
+    def __init__(self, name, text, item):
+        self.name = name
+        self.text = text
+        self.item = item
+        
+        def addItem(self, item):
+            self.items.append(item)
