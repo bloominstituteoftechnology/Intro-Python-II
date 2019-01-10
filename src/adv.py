be862e97f05ce13b31c119679b5f03d106427309
lndubose <lndubose13@gmail.com>
2019-01-10 15:33:03 -0600

    Change items array in rooms, add more functionality
    Changed items in arrays to be classes, also added take functionality. Still working out the
    kinks for it, but I am close.

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
@@ -8,20 +9,25 @@ room = {
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""", ['sword']),
+passages run north and east.""",
+                     [Item('sword', 'Normal'), Item('coin', 'dull')]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", ['knife']),
+the distance, but there is no way across the chasm.""",
+                     [Item('knife', 'Ouch sharp!')]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""", ['bat']),
+to north. The smell of gold permeates the air.""",
+                     [Item('bat', """Don\'t know why you would take a
+                     live animal but ok.""")]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
+# print(room['foyer'].items['sword'].description)
 
 # Link rooms together
 
@@ -65,6 +71,7 @@ def try_direction(direction, currentRoom):
 
 userName = input('What is your name? ')
 player = Player(userName, room['outside'])
+print(player.items)
 
 print(f'Hello {userName}! Let\'s go on an adventure!')
 
@@ -86,7 +93,17 @@ while not userInput == 'q':
         player.currentRoom = try_direction(userInput, player.currentRoom)
 
     elif 'take' in userInput or 'drop' in userInput:
-        print('yep')
+        action = userInput.split()
+        actionVerb = action[0]
+        actionItem = action[1]
+        if actionVerb == 'take':
+            if actionItem.name in player.currentRoom.items:
+                player.currentRoom.removeItem(actionItem)
+                player.takeItem(actionItem)
+                player.currentRoom.items[actionItem].on_take
+                print(player.currentRoom.items)
+
+        print(player.items)
 
     elif userInput == 'q':
         print('Thanks for playing!!!')
