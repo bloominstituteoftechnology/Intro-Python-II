415aa2d732bd7195487b0b5a35d2aa343096766a
michaelagard <mdagard@gmail.com>
2019-01-10 10:36:34 -0500

    Add classes for rooms and the player

--- src/room.py
+++ src/room.py
@@ -6,3 +6,6 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def __str__(self):
+        return f'{self.name}' + '\n' + f'{self.description}'
