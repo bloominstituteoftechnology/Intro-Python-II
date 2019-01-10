5dcde4ced65354b0c3c2ab8c596d7890cb7d4b3d
Eva <evalee904@gmail.com>
2019-01-10 03:30:34 -0600

    Changed naming conventions, added functions

--- src/player.py
+++ src/player.py
@@ -1,6 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-  def __init__(self, name, location):
+  def __init__(self, name, current_room):
     self.name = name
-    self.location = location
+    self.current_room = current_room
+
+  def move(self, room):
+    if room:
+      self.current_room = self.current_room.new_room(room)
+    else:
+      print('You can not go that way')
--- src/room.py
+++ src/room.py
@@ -4,3 +4,15 @@ class Room:
   def __init__(self, name, description):
     self.name = name,
     self.description = description
+
+  def new_room(self, direction):
+    if direction == 'n' and hasattr(self, 'n_to'):
+      return self.n_to
+    elif direction == 's' and hasattr(self, 's_to'):
+      return self.s_to
+    elif direction == 'w' and hasattr(self, 'w_to'):
+      return self.w_to
+    elif direction == 'e' and hasattr(self, 'e_to'):
+      return self.e_to
+    else:
+      print('You can not go to that direction.')
