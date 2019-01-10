e5c52ce58915d94fe5830961aafc3c4992a2cc5e
Steven O'Neill <oneillsteven957@gmail.com>
2019-01-10 17:45:13 -0600

    working item inventory and display of items
    Forgot to commit a few times so there are a few files that are getting changed

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
@@ -22,6 +23,11 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
+items = {
+    'sword': Item('Sword', 'it cuts stuff'),
+    'tourch': Item('Tourch', 'provides light')
+}
+
 
 # Link rooms together
 
@@ -34,6 +40,9 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+#items in room
+room['foyer'].items = [items['sword'].name, items['tourch'].name]
+
 #
 # Main
 #
@@ -41,10 +50,7 @@ def adv_game():
     # Make a new player object that is currently in the 'outside' room.
     player = Player("Bob", room['outside'])
     print(player)
-    #player = Player("Bob", "outside")
-    #roomKey = player.location
-    #roomName = room[player.location].name
-    #roomDesc = room[player.location].description
+    
 
     def try_direction(direction, location):
         attribute = direction + '_to'
@@ -62,44 +68,45 @@ def adv_game():
         print(player.location.name)
         # * Prints the current description (the textwrap module might be useful here).
         print(player.location.description)
+        # displaying items
+        if len(player.location.items):
+            print(f"The items in this room are: {player.location.items}")
+        else:
+            print("There is nothing in this room")
         # * Waits for user input and decides what to do.
-        s = input("\n").lower()[0]
+        s = input("Input a command: \n> ").lower().split()
 
-        # s = input("\n> ").lower().split()
         #check to see if one  or two word command
-        # if len(s) == 1:
-            #the user passed us a direction
+        if len(s) == 1:
             # grab the first character of the first word
-            # s = s[0][0]
-            #if s == 'q':
-            #print("You quit")
-            #break
+            if s[0] == 'q':
+                print("You quit")
+                break
 
-            #player.location = try_direction(s, player.location)
-        # elif len(s) ==2:
+            if s[0] == 'i' or s[0] =='inventory':
+                if player.inventory == []:
+                    print("You aren't holding aynthing")
+                else:
+                    print(f"your inventory has: {player.inventory}")
+
+            player.location = try_direction(s[0], player.location)
+        elif len(s) ==2:
             #user passed us a two-word command
-            # first_word = s[0]
-            # second_word = s[1]
+            if s[0] == 'get':
+                item_count = len(player.location.items)
+                for i, item in enumerate(player.location.items):
+                    if item.lower() == s[1]:
+                        player.inventory.append(player.location.items.pop(i))
+                        player.inventory
+            elif s[0] == 'drop':
+                for i, item in enumerate(player.location.items):
+                    if item.lower() == s[1]:
+                        player.inventory.remove(player.location.items.pop(i))
+                        players.inventory
 
-            # if first_word in ['get', 'drop']:
-
-        # else:
-            #print("I don't understand that")
-            #continue
-
-        # If the user enters "q", quit the game.
-        if s == 'q':
-            print("You quit")
-            break
-
-        player.location = try_direction(s, player.location)
-
-        """
-        North, South, East, West, north, south, east, west
-        N, S, E, W, n, s, e, w
-        """
-            
-        # If the user enters a cardinal direction, attempt to move to the room there.
+        else:
+            print("I don't understand that")
+            continue
         
 
 if __name__ == '__main__':
--- src/player.py
+++ src/player.py
@@ -1,9 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, location):
+    def __init__(self, name, location, inventory = [] * 2):
         self.name = name
         self.location = location
+        self.inventory = inventory
 
     def __repr__(self):
         return f"{self.name} is in {self.location}"
+
