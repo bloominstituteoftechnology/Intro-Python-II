d3bdc89f738b6c0c7a2e875a2afc33bf072df897
Kevin Sooter <kevin.m.sooter@gmail.com>
2019-01-10 17:08:48 -0600

    Tried to implement monsters.  Still a bit buggy.

---
+++ src/monster.py
@@ -0,0 +1,8 @@
+class Monster:
+    def __init__(self, name, description, hp):
+        self.name = name
+        self.description = description
+        self.hp = hp
+
+    def __repr__(self):
+        return str(f"{self.name}")
