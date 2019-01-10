9e4c21be5eb2b2ddfb3d5289de89db4738e6e05f
lndubose <lndubose13@gmail.com>
2019-01-10 15:30:30 -0600

    Add on_take function

--- src/item.py
+++ src/item.py
@@ -1,4 +1,13 @@
+from player import Player
+
+
 class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def __repr__(self):
+        return self.name
+
+    def on_take(self, player, item):
+        return f'Picked up {self.name}\n {self.description}'
