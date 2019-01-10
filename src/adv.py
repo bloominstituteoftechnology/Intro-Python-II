6448b1b73ed24cb8c9f9675e55b8c5a42625e175
Eva <evalee904@gmail.com>
2019-01-10 03:31:07 -0600

    Player able to move to another room, edge cases not solved.

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,9 @@
 from room import Room
+from player import Player
+from item import Item
+
+# Textwrap module
+import textwrap
 
 # Declare all the rooms
 
@@ -38,7 +43,12 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+print('Hello there!')
+new_player = input('What is your name?')
+if len(new_player) == 0:
+    print('Please enter your name.\n')
+player = Player(new_player, room['outside'])
+print(f'Welcome to the game, {player.name}!')
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +59,42 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+
+while True:
+    # * Prints the current room name
+    print(f'\nCurrent Location: {player.current_room.name[0]}')
+
+    # * Prints the current description (the textwrap module might be useful here).
+    print(f'\nLocation Description: {player.current_room.description}')
+
+    # commands
+    print('''
+        Explore the map by entering one of the following: 
+        "n" for North
+        "s" for South
+        "w" for West
+        "e" for East
+        ''')
+
+    print('Enter "q" to quit the game.')
+
+    # * Waits for user input and decides what to do.
+    current_room = player.current_room.name
+    choice = input('What would you like to do?')
+    if len(choice) != 1:
+        print('Invalid input, please try again.')
+    if choice == 'q':
+        print(f'Goodbye, {player.name}!')
+        break
+    if choice == 'n':
+        player.move(choice)
+    if choice == 's':
+        player.move(choice)
+    if choice == 'w':
+        player.move(choice)
+    if choice == 'e':
+        player.move(choice)
+    else:
+        print('Not a valid choice, please choose again.')
+
