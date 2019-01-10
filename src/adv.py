a03895b2cfca9117b31139462be3741921683b3c
luiscmartinez <mrluismartinezzz@gmail.com>
2019-01-09 18:47:09 -0800

    defined new instance of Player class and passed initial location

--- src/adv.py
+++ src/adv.py
@@ -40,7 +40,7 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 # Make a new player object that is currently in the 'outside' room.
-player = Player('foyer')
+player = Player('outside')
 
 # Write a loop that:
 #
@@ -48,7 +48,6 @@ player = Player('foyer')
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
 #
-
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 
