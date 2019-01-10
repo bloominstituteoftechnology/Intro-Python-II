e9475b632cf17768a2113d3cb0d8b20585d75b63
ckirkwood94 <ckirkwood94@gmail.com>
2019-01-10 16:13:20 -0500

    Made move_player function DRY

--- src/actions.py
+++ src/actions.py
@@ -1,20 +1,10 @@
 import os
 
 
-def move_player(direction, player):
-    try:
-        if direction == 'n':
-            player.room = player.room.n_to
-            os.system('cls' if os.name == 'nt' else 'clear')
-        elif direction == 'e':
-            player.room = player.room.e_to
-            os.system('cls' if os.name == 'nt' else 'clear')
-        elif direction == 's':
-            player.room = player.room.s_to
-            os.system('cls' if os.name == 'nt' else 'clear')
-        else:
-            player.room = player.room.w_to
-            os.system('cls' if os.name == 'nt' else 'clear')
-    except AttributeError:
+def move_player(attribute, player):
+    if hasattr(player.room, attribute):
+        player.room = getattr(player.room, attribute)
+        os.system('cls' if os.name == 'nt' else 'clear')
+    else:
         os.system('cls' if os.name == 'nt' else 'clear')
         print("Can't go that way. Try another direction.\n")
--- src/parser.py
+++ src/parser.py
@@ -9,7 +9,7 @@ def input_command(player):
     command = input("Enter command: ").lower()
     if command in ('quit', 'q'):
         raise Quit
-    elif command in ('n', 'e', 's', 'w'):
-        move_player(command, player)
+    elif command in ('n', 'e', 's', 'w', 'north', 'east', 'south', 'west'):
+        move_player(command[0] + '_to', player)
     else:
         return command
