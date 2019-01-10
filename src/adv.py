22b31499b6fc73b2b8770fc9ea8d9189fb30f68f
Kevin <brackkevin@gmail.com>
2019-01-10 14:32:19 -0600

    Changes formatting, and preps for user input

--- src/adv.py
+++ src/adv.py
@@ -5,7 +5,7 @@ from player import Player
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+"North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -38,41 +38,55 @@ room['treasure'].s_to = room['narrow']
 
 # Create a function to give a player a random name if
 # they leave it blank
+
 def random_name():
     return 'Fanny Pack Shaman'
 
-#
-# Main
-#
+# Create a function that interprets the user input
+
+def analize_input(s):
+    pass
+
+
+# MAIN
 
 # Make a new player object that is currently in the 'outside' room.
-current_room = 'outside'
+
+current_room = room['outside']
 game_active = True
-print("Welcome!")
-print(f"Current Room {room[current_room]}")
 
 # Prompt the player to name thier character and store it in a variable
-char_name = input("Please name your hero to begin your quest:\n")
+
+char_name = input("Please name your hero to begin your quest:\n>")
 
 if char_name == '':
     char_name = random_name()
 
 player1 = Player(char_name, current_room)
 
-print(f'Welcome {player1.name}!')
+print(f'\nWelcome {player1.name}!\n')
 
 # Write a loop that:
-#
+
 while game_active:
 
     # * Prints the current room name
-    print(f'Current location: {player1.location}')
+
+    print(f'Current location: {player1.location.name}')
+
     # * Prints the current description (the textwrap module might be useful here).
-    print(f'--')
+
+    print(f'-{player1.location.description}-\n')
+
     # * Waits for user input and decides what to do.
-    #
-    # If the user enters a cardinal direction, attempt to move to the room there.
-    # Print an error message if the movement isn't allowed.
-    #
+    print('Please choose an action')
+    print('[M]ove: [N]orth, [S]outh, [E]ast, [W]est')
+    print('[G]rab -item-, [D]rop -item-, [U]se -item-, [I]nventory')
+    print('[Q]uit')
+
+    in_loop = input("\n>").lower()
+
+    analize_input(in_loop)
+    
+
     game_active = False
-    # If the user enters "q", quit the game.
