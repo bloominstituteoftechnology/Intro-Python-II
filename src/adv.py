3903cf7138ce21d784b4f48e448ccd4cdc6503fe
Troy Amlee <troyamlee19@gmail.com>
2019-01-10 15:34:19 -0600

    [mvp] added a whole bunch of puzzles, and game functionality

--- src/adv.py
+++ src/adv.py
@@ -8,26 +8,98 @@ import textwrap
 import time
 import random
 
+#### Map ####
+"""
+____________________________________________________________
+                                      ===================
+                                      |     TREASURE    |
+                                      |                 |
+                                      |     [_-__-_]    |
+                                      |                 |
+                                      |======   =======|
+            +_+_+_+_+_+_+_+_+_+_+           {   }
+            |                   |           |  |
+            |     OVERLOOK      |           |  |
+            |                   |           |  |
+            |_______   _________|      ____{   }___
+                 __|  |____          /             \
+                |         |_________|   NARROW     |
+                |  FOYER   _________               |
+                |___   ___|        |______________/
+                  |    |
+__________________|    |_____________________________________
+
+                OUTSIDE                     
+
+"""
+# zonemap = {
+#     'foyer': {
+#             ZONENAME: '',
+#             DESCRIPTION: 'description',
+#             EXAMINATION: 'examine',
+#             SOLVED: False,
+#             UP: 'up', 'north',
+#             DOWN: 'down', 'south',
+#             LEFT: 'left', 'west',
+#             RIGHT: 'right', 'east'
+#     }
+# }
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons",
+                     "You see the sun set, and this beauty is bitter sweet. Unscramble this word ==> `lusunec`",
+                     "False",
+                     'foyer',
+                     "False",
+                     "False",
+                     "False",
+                     ),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer",
+                     """Dim light filters in from the south. Dusty
+                     passages run north and east.""",
+                     'You hear the cries of tortured people.\nThey eagerly would like to know what `avogadros number` is. (4 sig figs)',
+                     "False",
+                     'overlook',
+                     'outside',
+                     "False",
+                     'narrow'
+                     ),
 
     'overlook': Room("Grand Overlook",
      """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""",
+                      "The door has locked behind you.\nAnother tortured scientist appears and asks:\nWhat is π/2 radians in degrees?",
+                      "False",
+                      "False",
+                      'foyer',
+                      "False",
+                      "False",
+),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""",
+                     "Ancient writing teaches you the code combination `1337`",
+                     "False",
+                     'treasure',
+                     "False",
+                     'foyer',
+                     "False"
+),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""",
+                     "Your palms are sweaty, knees weak, mom's spaghetti. The cake is a lie,\nbecause Leroy Jenkins ate it with what kind of food?",
+                     "False",
+                     "False",
+                     'narrow',
+                     "False",
+                     "False"
+),
 }
 
 
@@ -52,22 +124,42 @@ starting_assassin = [Item("Steel Dagger", "Something To Stab With."), Item("Lion
 # Main
 #
 def main():
-    print(os.environ)
+    print(os.getenv)
     os.system('clear')
-    print("Welcome To Troy's Text Adventure Game\n")
-    print("1.) Start")
-    print("2.) Load")
-    print("3.) Exit")
+    print("#########################################\n")
+    print("# Welcome To Troy's Text Adventure Game #\n")
+    print("#########################################\n")
+    print("#                1.) Start              #  ")
+    print("#                2.) Load               #  ")
+    print("#                3.) Help               #  ")
+    print("#                4.) Exit               #  ")
+    print("#########################################\n")
     option = input("-> ")
     if option == "1":
         start()
     elif option == "2":
         pass
     elif option == "3":
+        help_menu()
+    elif option == "4":
         sys.exit()
+
     else:
         print("Requires 1, 2, or 3 inputs")
         main()
+
+def help_menu():
+    
+    print("#########################################\n")
+    print("# Welcome To Troy's Text Adventure Game #\n")
+    print("#########################################\n")
+    print("# type 1, 2, 3, 4 for menu navigation\n         ")
+    print("# - Use `up`, `down`, `left`, `right` to move\n ")
+    print("# - Type The Following Commands For Actions:\n  ")
+    print("# - Use 'examine' to examine something\n        ")
+    print("# - Good Luck Adventurer.                     ")
+    print("#########################################\n")
+    main()
 
 
 def class_define(player_name):
@@ -75,6 +167,7 @@ def class_define(player_name):
     print("1.) Knight")
     print("2.) Magi")
     print("3.) Assassin")
+    
 
     player_class = input('Class: \n').upper()
     if player_class == "1" or player_class == "KNIGHT":
@@ -108,9 +201,227 @@ def start():
 
     class_define(options)
 
+
+### Game Interactivity ###
+def print_location():
+    print('\n' + ('#' * (4 + len(PlayerIG.current_room.name))))
+    print('# ' + PlayerIG.current_room.name.upper() + ' #')
+    print('# ' + PlayerIG.current_room.description + ' #')
+    print('' + ('#' * (4 + len(PlayerIG.current_room.name))))
+
+
+#### Game Functionality ####
+
+
+def player_move(myAction):
+    ask = "What direction would you like to {} to?\n".format(myAction)
+    print('    ____________________________________________________________')
+    print('                                      ===================       ')
+    print('                                      |     TREASURE    |       ')
+    print('                                      |                 |       ')
+    print('                                      |     [_-__-_]    |       ')
+    print('                                      |                 |       ')
+    print('                                      |======   =======|        ')
+    print('            +_+_+_+_+_+_+_+_+_+_+           {   }               ')
+    print('            |                   |           |  |                ')
+    print('            |     OVERLOOK      |           |  |                ')
+    print('            |                   |           |  |                ')
+    print('            |_______   _________|      ____{   }___             ')
+    print('                 __|  |____          /             |            ')
+    print('                |         |_________|   NARROW     |            ')
+    print('                |  FOYER   _________               |            ')
+    print('                |___   ___|        |______________/             ')
+    print('                  |    |                                        ')
+    print('__________________|    |_____________________________________   ')
+    print('                                                                ')
+    print('                OUTSIDE                                         ')
+
+    dest = input(ask)
+    if dest in ['up', 'north']:
+        destination = room[PlayerIG.current_room.up]
+        print(destination)
+        movement_handler(destination)
+    elif dest in ['down', 'south', 'd', 's']:
+        destination = room[PlayerIG.current_room.down]
+        movement_handler(destination)
+    elif dest in ['left', 'west', 'l', 'w']:
+        destination = room[PlayerIG.current_room.left]
+        movement_handler(destination)
+    elif dest in ['right', 'east', 'r', 'e']:
+        destination = room[PlayerIG.current_room.right]
+        movement_handler(destination)
+
+def movement_handler(destination):
+    print("\n" + "You are in the " + destination.name)
+    try:
+        PlayerIG.current_room = destination
+        print_location()
+        print("\n" + "======================================")
+        print("What would you like to do?")
+        action = input("--->" + "\n")
+        acceptable_actions = ['move', 'go', 'travel', 'walk', 'quit', 'q', 'examine', 'inspect', 'interact', 'look']
+        while action.lower() not in acceptable_actions:
+            print("Unknown Action, Try Again.\n")
+            action = input("-->")
+        if action.lower() in ['quit', 'q']:
+            sys.exit()
+            return
+        elif action.lower() in ['move', 'go', 'travel', 'walk']:
+            if PlayerIG.current_room.solved == "False":
+                print("\nPlease `examine` Your Surroundings")
+                movement_handler(destination)
+            else:
+                player_move(action.lower())
+        elif action.lower() in ['examine', 'inspect', 'interact', 'look']:
+            player_examine(action.lower())
+
+    except: 
+        if action.lower() in ['move', 'go', 'travel', 'walk']:
+            print("You Can't Go That Way, follow the clues.")
+            player_move(myAction='move')
+        else:
+            sys.exit()
+            
+
 def start1(x):
     os.system('clear')
-    print("Hello {} {}, how are you?".format(x ,PlayerIG.name))
+    # print_location()
+    print("Hello {} {},\nyou have been chosen to infiltrate and retrieve \nan item of great power.".format(x ,PlayerIG.name))
+    print("\n" + "======================================")
+    destination = PlayerIG.current_room
+    movement_handler(destination)
+    # print("What would you like to do?")
+    # action = input("--->")
+    # acceptable_actions = ['move', 'go', 'travel', 'walk', 'quit', 'examine', 'inspect', 'interact', 'look']
+    # while action.lower() not in acceptable_actions:
+    #     print("Unknown Action, Try Again.\n")
+    #     action = input("-->")
+    # if action.lower() == 'quit':
+    #     sys.exit()
+    # elif action.lower() in ['move', 'go', 'travel', 'walk']:
+    #     player_move(action.lower())
+    # elif action.lower() in ['examine', 'inspect', 'interact', 'look']:
+    #     player_examine(action.lower())
+    
+def player_examine(action):
+    destination = PlayerIG.current_room
+    print(PlayerIG.current_room.name)
+
+### Outside Cave Entrance EXAMINE PUZZLE ###
+
+    if PlayerIG.current_room.name == 'Outside Cave Entrance':
+        if PlayerIG.current_room.solved == True:
+            print("You have already exhausted this zone")
+        else:
+            print('# ' + PlayerIG.current_room.examination + ' #')
+            word = input('->')
+            if word == 'nucleus':
+                PlayerIG.current_room.solved = True
+                print(PlayerIG.current_room.solved)
+                print("Great Job, the word is `nucleus`, You May Move North.")
+
+                movement_handler(destination)
+            else:
+                print("Wrong, Try Again")
+                print(action)
+                player_examine(action)
+
+### Foyer EXAMINE PUZZLE ###
+
+    if PlayerIG.current_room.name == 'Foyer':
+        if PlayerIG.current_room.solved == True:
+            print("You have already exhausted this zone")
+        else:
+            print('# ' + PlayerIG.current_room.examination + ' #')
+            word = input('->')
+            if word == '6.022*10**23':
+                PlayerIG.current_room.solved = True
+                print(PlayerIG.current_room.solved)
+                print("Great Job, the tortured scientists rejoice, and laugh at you.\nThey unlock various passageways.")
+
+                movement_handler(destination)
+            else:
+                print("Wrong, Try Again")
+                print(action)
+                player_examine(action)
+
+### Grand Overlook EXAMINE PUZZLE ###
+
+    if PlayerIG.current_room.name == 'Grand Overlook':
+        if PlayerIG.current_room.solved == True:
+            print("You have already exhausted this zone")
+        else:
+            print('# ' + PlayerIG.current_room.examination + ' #')
+            word = input('->')
+            if word == '90' or word == '90 degrees':
+                PlayerIG.current_room.solved = True
+                print(PlayerIG.current_room.solved)
+                print("Great Job, the scientist realizes the meaning of his bald circular shaped head.\nThe door has now unlocked behind you.")
+
+                movement_handler(destination)
+            else:
+                print("Wrong, Try Again")
+                print(action)
+                player_examine(action)
+
+### Narrow Passage EXAMINE PUZZLE ###
+
+    if PlayerIG.current_room.name == 'Narrow Passage':
+        if PlayerIG.current_room.solved == True:
+            print("You have already exhausted this zone")
+        else:
+            print('# ' + PlayerIG.current_room.examination + ' #')
+            word = input('->')
+            if word == '1337':
+                PlayerIG.current_room.solved = True
+                print(PlayerIG.current_room.solved)
+                print("Great Job, but you aren't elite just yet, the next puzzle awaits.")
+
+                movement_handler(destination)
+            else:
+                print("Wrong, Try Again")
+                print(action)
+                player_examine(action)
+
+### Treasure Chamber EXAMINE PUZZLE ###
+
+    if PlayerIG.current_room.name == 'Treasure Chamber' and room['outside'].solved and room['foyer'].solved and room['overlook'].solved and room['narrow'].solved:
+        if PlayerIG.current_room.solved == True:
+            print("You have already exhausted this zone, you won now go home.")
+        elif room['outside'].solved and room['foyer'].solved and room['overlook'].solved and room['narrow'].solved:
+            print('# ' + PlayerIG.current_room.examination + ' #')
+            word = input('->')
+            if word == 'chicken':
+                PlayerIG.current_room.solved = True
+                print(PlayerIG.current_room.solved)
+                print("You Have Won The Game, gratz")
+
+                movement_handler(destination)
+            else:
+                print("Wrong, Try Again")
+                print(action)
+                player_examine(action)
+        else:
+            print("More Rooms Need Examining/Solving.")
+            movement_handler(destination)
+
+
+
+ZONENAME = ''
+DESCRIPTION = 'description'
+EXAMINATION = 'examine'
+SOLVED = False
+UP = 'up', 'north'
+DOWN = 'down', 'south'
+LEFT = 'left', 'west'
+RIGHT = 'right', 'east'
+
+solved_places = {'outside': False, 
+                 'foyer': False, 
+                 'overlook': False, 
+                 'narrow': False, 
+                 'treasure': False
+}
 
 
 main()
--- src/room.py
+++ src/room.py
@@ -1,9 +1,15 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, examination, solved, up, down, left, right):
         self.name = name
         self.description = description
+        self.examination = examination
+        self.solved = solved
+        self.up = up
+        self.down = down
+        self.left = left
+        self.right = right
         # self.light = light
         # self.is_inside = is_inside
         # self.items = items
