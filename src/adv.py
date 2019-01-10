6b4991d140488fcad839053b28b8ac9f3139627a
Andrew <ghobrial.andrew@gmail.com>
2019-01-10 17:26:18 -0500

    fixed bugs

--- src/adv.py
+++ src/adv.py
@@ -1,6 +1,6 @@
 from room import Room
 from player import Player
-from item import Item
+# from item import Item
 import textwrap as textwrap
 
 # Declare all the rooms!
@@ -46,73 +46,73 @@ player = Player(room['outside'])
 # Write a loop that:
 #
 while True:
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-print(textwrap.dedent(
-    f"""\
-    You are in room: {player.current_room.name}
-    {player.current_room.description}!\
-    """))
-# * Waits for user input and decides what to do.
-#
+    # * Prints the current room name
+    # * Prints the current description (the textwrap module might be useful here).
+    print(textwrap.dedent(
+        f"""\
+        You are in room: {player.current_room.name}
+        {player.current_room.description}!\
+        """))
+    # * Waits for user input and decides what to do.
+    #
 
-print(textwrap.dedent('''
-        What would you like to do?
-        [n] Go North [s] Go South [e] Go East [w] Go West
-        [take out <item>] or [return <item>]
-        [drop <item>] Remove item from bag
-        [b] look in bag
-        [q] Quit
-    '''))
+    print(textwrap.dedent('''
+            What would you like to do?
+            [n] Go North [s] Go South [e] Go East [w] Go West
+            [take out <item>] or [return <item>]
+            [drop <item>] Remove item from bag
+            [b] look in bag
+            [q] Quit
+        '''))
 
-s = input("\n>").lower().split()
-print("your input:" s)
+    s = input("\n>").lower().split()
+    # print('your input:{s}')
 
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
-if len(s) == 1:
-    # user has passed a direction
-    if s == 'n':
-        player.current_room = player.current_room.n_to
-    elif s == 's':
-        player.current_room = player.current_room.s_to
-    elif s == 'e':
-        player.current_room = player.current_room.e_to
-    elif s == 'w':
-        player.current_room = player.current_room.w_to
-    elif s == 'q'
-        print('see you later!')
-        break
-    else: 
-        print("Not a valid direction!")
+    # If the user enters a cardinal direction, attempt to move to the room there.
+    # Print an error message if the movement isn't allowed.
+    #
+    # If the user enters "q", quit the game.
+    if len(s) == 1:
+        # user has passed a direction
+        if s == 'n':
+            player.current_room = player.current_room.n_to
+        elif s == 's':
+            player.current_room = player.current_room.s_to
+        elif s == 'e':
+            player.current_room = player.current_room.e_to
+        elif s == 'w':
+            player.current_room = player.current_room.w_to
+        elif s == 'q':
+            print('see you later!')
+            break
+        else: 
+            print("Not a valid direction!")
 
-elif len(s) == 2:
-    # user has passed two word command to interact with items
-
-else:
-    print("Sorry, I don't know that command")
-    continue
-
-
-
-
-
-
-while True:
-    choice = input()
-
-    if choice.count(" ") > 1:
-        choice = input("\nMove not allowed.\n")
+    elif len(s) == 2:
+        # user has passed two word command to interact with items
+        print("you're trying to interact with items, I can't do that yet")
     else:
-        action = choice
+        print("Sorry, I don't know that command")
+        continue
 
-    if action == "take out" or action == "return":
-        ## do item action stuff
-    elif action == "drop":
-        if isinstance(itemName, str):
-            if itemName in items:
-                item = items[itemName]
-                if item in player.inventory:
-                    player.drop_item(item)
+
+
+
+
+
+# while True:
+#     choice = input()
+
+#     if choice.count(" ") > 1:
+#         choice = input("\nMove not allowed.\n")
+#     else:
+#         action = choice
+
+#     if action == "take out" or action == "return":
+#         ## do item action stuff
+#     elif action == "drop":
+#         if isinstance(itemName, str):
+#             if itemName in items:
+#                 item = items[itemName]
+#                 if item in player.inventory:
+#                     player.drop_item(item)
