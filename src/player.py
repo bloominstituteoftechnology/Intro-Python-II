8fbba83368091947143ebb176ca6ac52a706ee57
michaelagard <mdagard@gmail.com>
2019-01-13 00:00:30 -0500

    add player functionality

--- src/player.py
+++ src/player.py
@@ -1,9 +1,19 @@
-# Write a class to hold player information, e.g. what room they are in
-# currently.
-
 
 class Player:
+    # initial values set for Player class
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
-        self.inventory = ["torch"]
+        self.items = []
+
+    def __repr__(self):
+        if self.items == []:
+            return f"Name: {self.name}"
+        else:
+            return f"Name: {self.name}\nItems: {self.items} "
+
+    def look(self):
+        if self.current_room.items == []:
+            return f"{self.current_room.description}"
+        else:
+            return f"{self.current_room.description}\n {self.current_room.items}"
