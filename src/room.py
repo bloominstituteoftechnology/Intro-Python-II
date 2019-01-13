1761836be9ecc954e6cd4b811dfbdf4c685da028
michaelagard <mdagard@gmail.com>
2019-01-13 04:16:11 -0500

    renamed items to __items, add method to look in rooms

--- src/room.py
+++ src/room.py
@@ -1,12 +1,19 @@
+# Class to manage the rooms' title, descriptions, and items
 class Room:
+
     def __init__(self, title, description):
         self.title = title
         self.description = description
-        self.items = []
+        self.__items = []
 
     def __repr__(self):
         print(f"-" * 40)
         return (f"You are at the {self.title}.")
+    # Method to add an item to a room
 
     def add_item(self, item):
-        return self.items.append(item)
+        return self.__items.append(item)
+    # Method to list items in a room.
+
+    def list_items_in_room(self):
+        return self.__items
