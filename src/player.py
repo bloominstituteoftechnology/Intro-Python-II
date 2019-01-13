c63a8732b6a7de509e408732ef24c839b6942130
michaelagard <mdagard@gmail.com>
2019-01-13 04:15:47 -0500

    refined look method | renamed items to __items

--- src/player.py
+++ src/player.py
@@ -1,19 +1,16 @@
-
+# Class to manage a players name, current room, and items
 class Player:
-    # initial values set for Player class
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
-        self.items = []
+        self.__items = []
 
     def __repr__(self):
-        if self.items == []:
+        if self.__items == []:
             return f"Name: {self.name}"
         else:
-            return f"Name: {self.name}\nItems: {self.items} "
+            return f"Name: {self.name}\nItems: {self.__items}"
+    # Method to look in the player's current room for items
 
     def look(self):
-        if self.current_room.items == []:
-            return f"{self.current_room.description}"
-        else:
-            return f"{self.current_room.description}\n {self.current_room.items}"
+        return f"{self.current_room.description}\nItems: {self.current_room.list_items_in_room()}"
