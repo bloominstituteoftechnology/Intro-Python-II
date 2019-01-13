6ce551bcd6e08130b50300cf56cd9dbb46223092
michaelagard <mdagard@gmail.com>
2019-01-13 00:00:00 -0500

    add logic to move again

--- src/adv.py
+++ src/adv.py
@@ -2,10 +2,8 @@
 from room import Room
 from player import Player
 from item import Item
-
 # Import os for clear screen function
 import os
-
 # Clear screen function
 
 
@@ -17,18 +15,18 @@ def cls():
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons."),
-
-    'foyer':    Room("the Foyer", """Dim light filters in from the south. Dusty
+    'foyer':    Room("Foyer",
+                     """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("the Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("the Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("the Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -47,63 +45,66 @@ room['treasure'].s_to = room['narrow']
 
 name_bypass = 1
 game_state = 1
+player = ""
 
-# Make a new player object that is currently in the 'outside' room.
-player = Player("Test Name", room['outside'])
+dingus = Item("Dingus", "This really dings.")
 
-# Write a loop that:
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
-
-"""
-direction is the direction of the user input
-current_Room is the current room the player is in
-returns the new room the player moves to if the
-move was successful, or returns current room if
-the move was unsuccessful.
-"""
+room["outside"].add_item(dingus)
+# Method to attempt to changes rooms
 
 
 def try_direction(direction, current_room):
+        # appends direction attr to direction to be readable by the map
     direction_attribute = direction + "_to"
-
     # Check if direction has valid direction_attribute
     if hasattr(current_room, direction_attribute):
         # fetch the new room
         return getattr(current_room, direction_attribute)
     else:
-        print('Invalid direction.' + '\n' + "-" * 40)
-        return player.current_room
-
-
-while game_state is 1:
-    if name_bypass == 0:
-        player_name = input("Insert name: ")
-        player.name = player_name.capitalize()
-        name_bypass = 1
-    print(f'You are at the {player.current_room}')
-
-    # accepts input from user and converts to lowercase
-    player_input_option = input(
-        f'Which direction would you like to go? ').lower()
-    cls()
-    # check if input was q, exit, or quit, then terminates the program
-    if player_input_option == 'q' or player_input_option == 'exit' or player_input_option == 'quit':
         cls()
-        break
-    if player_input_option.split(' ', 1)[0] == "pickup":
-        print('Tried to pickup')
-    #
-    elif player_input_option:
-        player.current_room = try_direction(
-            player_input_option, player.current_room)
+        print("-" * 40 + f'\n{direction} is an invalid direction.')
+        return current_room
 
+
+# When game_state is 1, game will begin, 0 to stop the game
+while game_state is 1:
+    # Allows to bypass the name for testing
+    if player == "":
+        player = Player("Test Name", room['outside'])
+        cls()
+        if name_bypass == 0:
+            player_name = input("Insert name: ")
+            player.name = player_name.capitalize()
+            name_bypass = 1
+
+    # TEST PRINTS
+    print(f"{player.current_room}")
+    print(player)
+    print("-" * 40)
+    # accepts input from user and converts to lowercase
+    player_input = input(f'Enter a direction > ').lower()
+    cls()
+    # Split player_input into two arguments
+    if len(player_input.split(' ', 1)) > 2:
+        player_input_option1 = player_input.split(' ', 1)[0]
+        player_input_option2 = player_input.split(' ', 1)[1]
+    else:
+        player_input_option1 = player_input.split(' ', 1)[0]
+
+    # Check if input was q, exit, or quit, then terminates the program
+    if player_input_option1 == 'q' or player_input_option1 == 'exit' or player_input_option1 == 'quit':
+        cls()
+        game_state = 0
+
+    # Travel check
+    elif player_input_option1 in ("n", "s", "e", "w"):
+        cls()
+        player.current_room = try_direction(
+            player_input_option1, player.current_room)
+
+    elif player_input == "l":
+        cls()
+        print(player.look())
     else:
         cls()
         print("Command not found.")
