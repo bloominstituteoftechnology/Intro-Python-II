f92268d294344bad8431223c7634753286e7cffe
michaelagard <mdagard@gmail.com>
2019-01-13 20:57:39 -0500

    refactored code represent what it does better

--- src/item.py
+++ src/item.py
@@ -1,8 +1,17 @@
-# Class to manage items' names and descriptions
-class Item:
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
+from room import Room
+from player import Player
+
+
+class Item(object):
+    def __init__(self, item):
+        self.name = item[0]
+        self.description = item[1]
 
     def __repr__(self):
-        return f'{self.name}'
+        return f'{self.name}, {self.description}'
+
+    def get_name(self):
+        return self.name
+
+    def get_description(self):
+        return self.description
