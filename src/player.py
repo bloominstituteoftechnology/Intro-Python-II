96449e6a38bfd02b24d8338866c601666466c645
AM_Myrick <amyrick.820@gmail.com>
2019-01-15 16:10:19 -0500

    creates Player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, inventory):
+        self.name = name
+        self.inventory = inventory
+    def __str__(self):
+        return f"My name is {self.name}. I'm currently holding {self.inventory}."
