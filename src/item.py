19a266c7834a0a9d3bed04498d3b0244e2478dbd
jeffyjkang <jeffyjkang@gmail.com>
2019-02-12 12:52:31 -0800

    initial commit

--- src/item.py
+++ src/item.py
@@ -1,8 +1,8 @@
-# Implement a class to hold room information. This should have name and
+# Write a calss to hold item information. This should have name and
 # description attributes.
 
 
-class Room:
+class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
