b52f2988990a2f6c0680b081d4b515168561536f
jeffyjkang <jeffyjkang@gmail.com>
2019-02-12 15:53:19 -0800

    added items

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,40 @@
 from room import Room
+from item import Item
+from player import Player
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", ""),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", ""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", ""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", ""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", ""),
+}
+
+# Declare all items
+
+item = {
+    'sword': Item("excalibur", "pretty cool sword"),
+
+    "shield": Item("hylian shield", "pretty sturdy shield"),
+
+    "boots": Item("leather boots", "a pair of pretty fashionable boots"),
+
+    "armor": Item("tunic", "a pretty weird looking suit"),
+
+    "helmet": Item("crown", "pretty regal looking headpiece")
 }
 
 
@@ -39,6 +55,8 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player(room["outside"])
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +67,16 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+
+for r in room:
+    # print(f'room name: {room[r].name}, description: {room[r].description} \n')
+    print(room[r])
+
+# print(f'{room["outside"].name}')
+print(player)
+print(player.room.name)
+print(player.room.description)
+
+# print(room)
+# print(room["outside"].print_room)
--- src/room.py
+++ src/room.py
@@ -3,6 +3,19 @@
 
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
+        self.items = items
+
+    def __str__(self):
+        return f"room name: {self.name}, room description: {self.description}. \n"
+
+    # def print_room(self, name, description):
+    #     self.name = name
+    #     self.description = description
+    #     print(f'Room name: {name}, room description: {description}')
+
+
+# room = Room("hello", "something")
+# room.print_room("hello", "something")
