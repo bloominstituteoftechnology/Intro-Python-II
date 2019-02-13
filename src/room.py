ae1cce120a88787d85eee4137f35165d57c43778
Wanda <doubleuevans@gmail.com>
2019-02-13 15:58:13 -0600

    wanda_evans-introtopythonII

--- src/room.py
+++ src/room.py
@@ -1,2 +1,14 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+# from READme
+# Put the Room class in room.py based on what you see in adv.py.
+# : in adv.py I see a room object with keys of room names, and invoking
+# the Room class and within the parenthesis, a 2 quotes (a detailed room name)
+# and a short message.
+
+class Room:
+    def __init__(self, r_name, r_description):
+        self.r_name = r_name
+        self.r_name = r_description
+        
