86ada1dc3d8fbccced55abb95045b26e48314d25
Carlos Lantigua <clantigua954@gmail.com>
2019-02-13 16:41:47 -0500

    fixed what I broke, forgot to type an attribute in player out

--- src/player.py
+++ src/player.py
@@ -4,7 +4,7 @@
 
 class Player:
     def __init__(self, curr_room):
-        self.room = curr_room
+        self.curr_room = curr_room
 
         def __str__(self):
             return f"{self.name} is in {self.curr_room}"
