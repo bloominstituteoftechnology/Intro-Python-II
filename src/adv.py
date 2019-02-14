298749dbefdfc5ea284d29db1df59eadf6414362
Carlos Lantigua <clantigua954@gmail.com>
2019-02-14 10:27:58 -0500

    attempting to refactor to clean up room item stuff

--- src/adv.py
+++ src/adv.py
@@ -5,22 +5,26 @@ from item import Item
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'outside': Room("Outside Cave Entrance",
+                    "North of you, the cave mount beckons"),
+    'foyer': Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
-
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow': Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
-
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
+}
+
+item = {
+    'outside': Item('pistol', 'a black shiny pistol'),
+    'foyer': Item('beef jerky', 'wonder how long its been here'),
+    'overlook': Item('Nervana CD', 'strange that this is here'),
+    'narrow': Item('Soda', 'Good thing its not a "pop"'),
+    'treasure': Item('Monopoly Money', 'This is a terrible joke 😭')
 }
 
 # Link rooms together
@@ -35,14 +39,11 @@ room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
 # Link item to room??
-room['outside'].add_item(
-    Item('pistol', 'a black shiny pistol.. this could be useful'))
-room['foyer'].add_item(Item(
-    'beef jerky', 'no telling how long this has been here, better take it with you'))
-room['overlook'].add_item(
-    Item('Nervana CD', 'strange that this is here, but its a good cd'))
-room['narrow'].add_item(Item('Soda', 'Good thing its not a "pop"'))
-room['treasure'].add_item(Item('Monopoly Money', 'This is a terrible joke 😭'))
+room['outside'].add_item(Item(item['outside']))
+room['foyer'].add_item(Item(item['foyer']))
+room['overlook'].add_item(Item(item['overlook']))
+room['narrow'].add_item(Item([item['narrow']]))
+room['treasure'].add_item(Item([item['treasure']]))
 
 # Make a new player object that is currently in the 'outside' room.
 player = Player(room['outside'])
