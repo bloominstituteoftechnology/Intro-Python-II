747fb689482b234df16f2a86a358407731c8ebcc
TRIF3X <thewizardofcrypto@gmail.com>
2019-02-13 20:09:03 -0600

    item class

--- src/item.py
+++ src/item.py
@@ -2,3 +2,10 @@
 # The item should have name and description attributes.
 # Hint: the name should be one word for ease in parsing later.
 
+class Item:
+    def __init__(self, item, itemDes):
+        self.item = item
+        self.itemDes = itemDes
+
+    def __str__(self):
+        return f"{self.item}: {self.itemDes}"
