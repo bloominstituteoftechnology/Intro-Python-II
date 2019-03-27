fda5f59edc6296a78f6fa4184a951256e89f35cc
David Freitag <freitagdavid@outlook.com>
2019-03-27 15:57:35 -0400

    player: Initial simple implementation

--- src/player.py
+++ src/player.py
@@ -1,2 +1,17 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name="Player", room="outside", race="human", gender="male"):
+        self.name = name
+        self.room = room
+        self.race = race
+        self.gender = gender
+        self.inventory = []
+
+    def addItem(self, item):
+        self.inventory.append(item)
+
+    def setRoom(self, room):
+        self.room = room
