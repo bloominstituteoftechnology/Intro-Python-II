f01684a2de71b3e9704765151ef4e693b0115930
Marious <mlancelot88@gmail.com>
2019-05-03 14:16:17 -0500

    refactored `item.py` for acquisition and drop

--- src/item.py
+++ src/item.py
@@ -2,3 +2,12 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def __repr__(self):
+        return f"(self.name), (self.description)"
+
+    def on_get(self):
+        print("You acquired " + self.name)
+
+    def on_drop(self):
+        print("You dropped " + self.name)
