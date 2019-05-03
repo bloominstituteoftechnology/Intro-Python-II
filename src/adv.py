55d80493b8e1ffe579de5d8ff802b0d830760e7c
Marious <mlancelot88@gmail.com>
2019-05-03 14:18:57 -0500

    based on class notes, movement has been refactored

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
@@ -37,32 +38,68 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
+def adv_game():
+    # Make a new player object that is currently in the 'outside' room.
+    player = Player("Mari", room["outside"])
+    print(player)
 
-# Make a new player object that is currently in the 'outside' room.
-player = Player("Mari", "outside")
-roomKey = player.location
-roomName = room[player.location].name
-roomDesc = room[player.location].description
+    def try_direction(direction, location):
+        attribute = direction + "_to"
 
-# Write a loop that:
-#
-while True:
-    # * Prints the current room name
-    print(roomName)
-    # * Prints the current description (the textwrap module might be useful here).
-    print(roomDesc)
-    # * Waits for user input and decides what to do.
-    userInput = input("Choose a direction in which to move: n, s, e, w")
-    # If the user enters a cardinal direction, attempt to move to the room there.
-    if userInput == "n" and roomKey == 'outside':
-        roomKey = room[player.location].n_to
-        roomName = room[player.location].n_to.name
-        roomDesc = room[player.location].n_to.description
-    # If the user enters "q", quit the game.
-    elif userInput == "q":
-        print("Quitters never prosper!")
-        break
-    else:
-    # Print an error message if the movement isn't allowed.
-        print("Whoa partner! Cannot move there. Try again.")
-        continue
+        if hasattr(location, attribute):
+            return getattr(location, attribute)
+        else:
+            print("Wrong way!")
+            return location
+
+    # Write a loop that:
+    #
+    while True:
+        # * Prints the current room name
+        print(player.location.name)
+        # * Prints the current description (the textwrap module might be useful here).
+        print(player.location.description)
+        # * Displays Player Items
+        if len(player.location.items):
+            print(f"The items found in this room are: {player.location.items}")
+        else:
+            print("There is nothing to be found here.")
+        # * Waits for user input and decides what to do.
+        s = input("Choose a command: \n> ").lower().split()
+
+        # * Checks to see if one or two word command
+        if len(s) == 1:
+            # grab the first character of the first word
+            s = s[0][0]
+            if s == 'q':
+                print("You quit")
+                break
+
+            if s == 'i' or s[0] == "inventory":
+                if player.inventory == []:
+                    print("Your inventory is empty.")
+                else:
+                    print(f"your inventory has: {player.inventory}")
+
+            player.location = try_direction(s, player.location)
+        elif len(s) == 2:
+            # user passed us a two-word command
+            if s[0] == "get":
+                item_count = len(player.location.items)
+                for i, item in enumerate(player.location.items):
+                    if item.lower() == s[1]:
+                        player.inventory.append(player.location.items.pop(i))
+                        print(player.inventory)
+            elif s[0] == "drop":
+                item_count = len(player.inventory)
+                for i, item in enumerate(player.inventory):
+                    if item.lower() == s[1]:
+                        player.location.items.append(player.inventory.pop(i))
+                        print(f"Your inventory has: {player.inventory}")
+
+        else:
+            print("I don't understand that")
+            continue
+
+if __name__ == '__main__':
+    adv_game()
