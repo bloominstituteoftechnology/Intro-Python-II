d5bd51eaa824fa5223a9459bacf1dd0c275d3d80
cocoitali <cmburatto@gmail.com>
2019-05-13 21:21:15 -0600

    Built basic Player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+#Note to Self: add specific descriptors....
+#EXAMPLE CLASS, modify for actual project
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+
+    def __repr__(self):
+        return f"{self.name} is in the {self.current_room}"
