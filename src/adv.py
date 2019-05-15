4d681abef7c825bab970cd9dabb1df58a9d57bfe
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-05-14 22:06:07 -0400

    add 'movement' functionality'

---
+++ src/adv.py
@@ -0,0 +1,74 @@
+from room import Room
+from player import Player
+#
+# # Declare all the rooms
+#
+room = {
+    'outside':  Room("Outside Cave Entrance",
+                     "North of you, the cave mount beckons"),
+
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+passages run north and east."""),
+
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+into the darkness. Ahead to the north, a light flickers in
+the distance, but there is no way across the chasm."""),
+
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+to north. The smell of gold permeates the air."""),
+
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+chamber! Sadly, it has already been completely emptied by
+earlier adventurers. The only exit is to the south."""),
+}
+#
+#
+# # Link rooms together
+#
+room['outside'].n_to = room['foyer']
+room['foyer'].s_to = room['outside']
+room['foyer'].n_to = room['overlook']
+room['foyer'].e_to = room['narrow']
+room['overlook'].s_to = room['foyer']
+room['narrow'].w_to = room['foyer']
+room['narrow'].n_to = room['treasure']
+room['treasure'].s_to = room['narrow']
+
+#
+# Main
+#
+
+# Make a new player object that is currently in the 'outside' room.
+name = input("Please enter your character name: ")
+player = Player(name, room["outside"])
+print("\n")
+
+# Write a loop that:
+#
+# * Prints the current room name
+# * Prints the current description (the textwrap module might be useful here).
+# * Waits for user input and decides what to do.
+q = ""
+while q != "q":
+    if q != "You can't go that way!":
+        print(player.room)
+    q=input("Where would you like to move?")
+    print("\n")
+    try:
+        if q == "N" or q == "n" or q == "north" or q == "North":
+            player.room = player.room.n_to
+        if q == "S" or q == "s" or q == "south" or q == "South":
+            player.room = player.room.s_to
+        if q == "E" or q == "e" or q == "east" or q == "East":
+            player.room = player.room.e_to
+        if q == "W" or q == "w" or q == "west" or q == "West":
+            player.room = player.room.w_to
+    except:
+        q = "You can't go that way!"
+        print(q)
+
+#
+# If the user enters a cardinal direction, attempt to move to the room there.
+# Print an error message if the movement isn't allowed.
+#
+# If the user enters "q", quit the game.
