1d37d268c8a0f6e5640114f7970cf7c803ebed35
Colin Dismuke <colin.dismuke@gmail.com>
2019-05-14 20:53:47 -0500

    😄 Added emoji to prompts and descriptions

--- src/adv.py
+++ src/adv.py
@@ -10,14 +10,14 @@ room = {
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("Grand Overlook", """⛰️ A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("Treasure Chamber", """💰 You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -70,6 +70,7 @@ def player_input(player):
         player.move(command[0] + "_to")
         print(player.current_room)
     else:
+        print("💀 Please enter a valid direction (n, s, e, w).")
         return command
 
 # Main method for the game
