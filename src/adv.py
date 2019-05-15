1b80bbb9940c42ce6f2e0aa927fcc6d63d0b6acb
Michael Littleton <michaelglittleton@gmail.com>
2019-05-15 00:09:18 -0400

    Initial commit plus also everything from day one
    I really should learn to commit more

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -36,16 +37,67 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
-
 # Make a new player object that is currently in the 'outside' room.
+possDirections = {
+    "n": "North",
+    "s": "South",
+    "e": "East",
+    "w": "West",
+    "x": "Exit"
+}
+
+player = Player("Kilroy", room['outside'])
+direction = ''
+
+print("Welcome, " + player.name + "! You are have found yourself near a spooky mansion.")
 
 # Write a loop that:
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
+while direction != 'x':
+    inputOpts = ""
+    print(player.currentLoc)
+    for opt in possDirections:
+        inputOpts += '\n- %s (%s)' % (possDirections[opt], opt)
+
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
+    while direction != 'x':
+        direction = input(inputOpts + '\nWhich direction do you choose? :')
+        try:
+            youWent = "\nYou chose to move " + possDirections[direction]
+            if direction == "n":
+                player.currentLoc = player.currentLoc.n_to
+                print(youWent)
+                break
+            elif direction == "w":
+                player.currentLoc = player.currentLoc.w_to
+                print(youWent)
+                break
+            elif direction == "e":
+                player.currentLoc = player.currentLoc.e_to
+                print(youWent)
+                break
+            elif direction == "s":
+                player.currentLoc = player.currentLoc.s_to
+                print(youWent)
+                break
+            elif direction == "x":
+                print("\n\nThanks for playing!!")
+            else:
+                print("\n\n\nPlease enter a valid direction.")
+        except KeyError:
+            print('\nPlease choose a valid option: n, s, e, w, or x\n')
+            print(player.currentLoc)
+        except AttributeError:
+            print("\nRead the description carefully and choose again.\n")
+            print(player.currentLoc)
+
+print("No, really! Thanks for playing.")
+
+
 # If the user enters "q", quit the game.
