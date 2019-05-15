aaef64065d95fac70978235f0658024a8e1c5952
cocoitali <cmburatto@gmail.com>
2019-05-14 23:00:38 -0600

    Added REPL parser to adv.py

--- src/adv.py
+++ src/adv.py
@@ -40,28 +40,51 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 player = Player(room['outside']) #give entire outside instance
-print(player)
+ 
+def try_direction(direction, current_room):
+    #if/else logic could go here instead, but is less DRY
+    attribute = direction + '_to'
+
+    #see if the inputted direction is one we can move to w/ built in method
+    if hasattr(current_room, attribute):
+        return getattr(current_room, attribute) #fetch room w/ built in python methods
+    else:
+        print("This direction is unavailable!")
+        return current_room
 # Write a loop that:
+while True:
+    # * Prints the current room name
+    #-->look up room in dictionary...
+    print(player.current_room.name)
 
-# * Prints the current room name
-#-->look up room in dictionary...
-print(player.current_room.name)
+    # * Prints the current description (the textwrap module might be useful here).
+    print(player.current_room.description)
+    # * Waits for user input and decides what to do.
+    #d for direction
+    d = input("\n>").lower()[0]
 
-# * Prints the current description (the textwrap module might be useful here).
-print(player.current_room.description)
-# * Waits for user input and decides what to do.
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+    
 
-    #did user enter valid direction?
-    #Yes? -
-    #   is there anything IN that direction?
-    #       Yes? - move in that diredction, print new room info
-    #       No? - Print out message stating nothing in that direction
-    #   No? - Print an error message if the movement isn't allowed.
-    #continue promting for input
+            
+    # If the user enters a cardinal direction, attempt to move to the room there.
+    # Print an error message if the movement isn't allowed. SEE try_direction
+    #ALLOW : North, South, East, West, north, south, east, west, N, S, E, W, n, s , e, w
+
+    # If the user enters "q", quit the game.
+    if d =='q':
+        print("Thanks for playing!")
+        break
+
+    player.current_room = try_direction(d, player.current_room)
+
+    #NOTES FROM INSTRUCTOR HOUR/ELISSA
+        #did user enter valid direction?
+        #Yes? -
+        #   is there anything IN that direction?
+        #       Yes? - move in that diredction, print new room info
+        #       No? - Print out message stating nothing in that direction
+        #   No? - Print an error message if the movement isn't allowed.
+        #continue promting for input
 
 
 
