3fc63d2f529e4cb74770fa71b6f8d9ee8d51d735
cocoitali <cmburatto@gmail.com>
2019-05-14 21:28:10 -0600

    Cleaned up player and room classes to display basics required from README

--- src/player.py
+++ src/player.py
@@ -1,8 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
-#Note to Self: add specific descriptors....
-#EXAMPLE CLASS, modify for actual project
+
 class Player:
     def __init__(self, name, current_room):
         self.name = name
--- src/room.py
+++ src/room.py
@@ -9,36 +9,6 @@ class Room:
         self.s_to = s_to
         self.e_to = e_to
         self.w_to = w_to
-    def __repr__(self):
-        # name
-        # 1. + categories[0]
-        # 2. + categories[1]
-        output = ""
-        output += self.name + "\n"
-        i = 1
-        for d in self.description:
-            output += str(i) +". " + d + "\n"
-            i += 1
-        output += str(i) + ". Exit"
-        return output
+ 
+       
 
-#Example
-my_room = Room("Example Room", ["doors", "knobs", "walls", "table" ])
-print(my_room)
-
-selection = 0
-while int(selection) != len(my_room.descriptions) +1:
-    selection = input("Select a room: ")
-    #TRY/EXCEPT ERROR HANDLING
-    try:
-        selection = int(selection)
-        if selection == len(my_room.descriptions)+1:
-            print("Thanks for visiting!")
-        elif selection >= 1 and selection <= len(my_room.descriptions) +1:  #INPUTS ARE ALWAYS STRINGS
-            #print("The player selected " + str(selection))
-            print(my_room.descriptions[selection-1])
-        else:
-            print("Please select a valid room")
-    except ValueError:
-        print("Please enter your choice as a number.")
-        #allows user to reenter a new value
