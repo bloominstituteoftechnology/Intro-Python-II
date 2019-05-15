2145c684209ccf71b8ce0455c655d4ecbb0e5abd
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-05-14 21:07:02 -0400

    Add basic player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+
+    def __init__(self, name, room):
+        self.name = name
+        self.room = room
+
+    def __repr__(self):
+
+        output = ""
+        output += "Name: " + self.name + "\n"
+        output += "Room: " + self.room
+        return output
