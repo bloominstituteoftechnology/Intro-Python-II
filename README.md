895b6742bb278209c3213cbc5793f24fee83868f
Abigayle Franklin <abifranklin@gmail.com>
2019-05-15 14:58:30 -0500

    Rooms defined

--- README.md
+++ README.md
@@ -35,15 +35,15 @@ The `/src` directory contains the files `adv.py`, which is where the main logic 
 
 
 * Add a REPL parser to `adv.py` that accepts directional commands to move the player
-  * After each move, the REPL should print the name and description of the player's current room
-  * Valid commands are `n`, `s`, `e` and `w` which move the player North, South, East or West
+  X After each move, the REPL should print the name and description of the player's current room
+  X Valid commands are `n`, `s`, `e` and `w` which move the player North, South, East or West
   * The parser should print an error if the player tries to move where there is no room.
 
 * Put the Room class in `room.py` based on what you see in `adv.py`.
 
-  * The room should have `name` and `description` attributes.
+  X The room should have `name` and `description` attributes.
 
-  * The room should also have `n_to`, `s_to`, `e_to`, and `w_to` attributes
+  X The room should also have `n_to`, `s_to`, `e_to`, and `w_to` attributes
     which point to the room in that respective direction.
 
 * Put the Player class in `player.py`.
--- src/room.py
+++ src/room.py
@@ -1,15 +1,25 @@
-# Implement a class to hold room information. This should have name and
-# description attributes.
 class Room:
+    def __init__(self, name, description, n_to, s_to, e_to, w_to):
+        self.name = name
+        self.description = description
+        self.n_to = n_to
+        self.s_to = s_to
+        self.w_to = w_to
+        self.e_to = e_to
 
-    def __init__(self, name, description):
-        self.name: name
-        self.description: description
-    
     def __repr__(self):
-        output = ""
-        output += self.name     
-        return output
+        return self.name + " - " + self.description
 
-myRoom = Room("Bedroom", "Where I get my beauty sleep.")
-print(myRoom)
+
+East_Port = Room("East Portico", "Welcome to The Manor.", None, "Enterance Hall", None, None)
+Enterance_Hall = Room("Enterance Hall", "Please come in.", "East Portico", "Parlor", "Drawing Room", "Library")
+Parlor = Room("Parlor", "Gossip and tea served daily.", "Enterance Hall", "West Poritco", "Master Bedroom", "Dining Hall")
+West_Port = Room("West Portico", "Take in a view of the gardens.", "Parlor", None, None, None)
+Library = Room("Library", "All manners of Manor's business are conducted in the library.", None, "", "Enterance Hall", None)
+Dining_Hall = Room("Dining Hall", "Join us for a feast or just a bite to eat.", "Library", None, "Parlor", "Guest Room")
+Guest_Room = Room("Guest Room", "Stay awhile.", None, None, "Dining Hall", "North Terrace")
+North_Terrace = Room("North Terrace", "Lovely views of the nearby village.", None, None, "Guest Room", None)
+Drawing_Room = Room("Drawing Room", "Looking for a quiet spot to read or embroider? You've found the spot.", None, "Master Bedroom", None, "Enterance Hall")
+Master_Bedroom = Room("Master Bedroom", "Where the Lord and Lady of The Manor take their rest.", "Drawing Room", None, "Greenhouse", "Parlor")
+Greenhouse = Room("Greenhouse", "The finest tomatoes in the parish.", None, None, "Master Bedroom", "South Terrace")
+South_Terrace = Room("South Portico", "Vistas overlooking the pond.", None, None, None, "Greenhouse")
