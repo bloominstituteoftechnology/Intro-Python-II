b3badc060059fcf0941d1bcfff4d1c5d6e6befc2
cocoitali <cmburatto@gmail.com>
2019-05-16 19:57:32 -0600

    item.py : modified attr, prints attr

--- src/item.py
+++ src/item.py
@@ -1,7 +1,11 @@
 class Item:
-    def __init__(self, name, description):
+    def __init__(self, name, description, price):
         self.name = name
         self.description = description
+        self.price = price
 
-    def __repr__(self):
-        return f"{self.name}: {self.description}"
+    def __str__(self): #string representation for end users to be able to see
+        return self.name + "\t" + self.description + "\t$" + str(self.price)
+
+i = Item("sword", "A sharp item", 3)
+print(i)
