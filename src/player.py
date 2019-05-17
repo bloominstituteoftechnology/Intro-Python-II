f7c69455a9e14284b6b54771824f33c23d2143ac
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-05-16 21:59:42 -0400

    complete day 2 functionality

--- src/player.py
+++ src/player.py
@@ -2,13 +2,17 @@
 # currently.
 class Player:
 
-    def __init__(self, name, room):
+    def __init__(self, name, room, items):
         self.name = name
         self.room = room
+        self.items = items
 
     def __repr__(self):
 
         output = ""
         output += "Name: " + self.name + "\n"
-        output += "Room: " + self.room
+        output += "Room: " + self.room + "\n"
+        if len(self.items) > 0:
+            for i in range(0, len(self.items)):
+                output += str(int + 1) + ". " + self.items[i] + "\n"
         return output
