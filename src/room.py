93d536454228bed5a844318c4faf85fc6f2b3a64
cocoitali <cmburatto@gmail.com>
2019-05-16 19:56:20 -0600

    Room.py : modified, beginning to support list of items in attributes

--- src/room.py
+++ src/room.py
@@ -1,15 +1,18 @@
+from item import Item
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
 class Room:
-    def __init__(self, name, description): #, n_to, s_to, e_to, w_to
+    def __init__(self, name, description, items): 
         self.name = name
         self.description = description
-        #self.n_to = n_to
-        #self.s_to = s_to
-        #self.e_to = e_to
-        #self.w_to = w_to
-    def __repr__(self):
-        return f"{self.name}, {self.description}"
+        self.items = items
+       
+    def __str__(self):
+        output = self.name
+        for i in self.items:
+            output += "\n" str(i)
+
+        return output
        
 
