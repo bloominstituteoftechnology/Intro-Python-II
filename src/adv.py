0084d6a3f7ea6cdf3b868ab76976c325f29de74a
Javier Alavrez <javieralvarez82@live.com>
2019-05-18 01:43:37 -0700

    working on player creation

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,24 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
-
 room = {
     'outside':  Room("Outside Cave Entrance",
-"North of you, the cave mount beckons"),
+"North of you, the cave mount beckons", item='None'),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", item='old map'),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", item='flash light'),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", item='None'),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", item='Nothing bummer'),
 }
 
 
@@ -35,9 +35,8 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
-
 # Make a new player object that is currently in the 'outside' room.
-
+#
 # Write a loop that:
 #
 # * Prints the current room name
@@ -45,6 +44,9 @@ room['treasure'].s_to = room['narrow']
 # * Waits for user input and decides what to do.
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
+
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+Javier = Player(name="javier", desc="Coder Peasant", satchel="no items", location=room['outside'])
+
--- src/player.py
+++ src/player.py
@@ -1,12 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-class player:
+class Player:
     def __init__(self, name, desc, satchel, location):
         self.name = name
-        self.desc = desc
+        self.desc = name
         self.satchel = satchel
         self.location = location
 
 
     def __repr__(self):
-        return 
+        return '{self.__class__.__name__}({self.name}{self.satchel} {self.location})'.format(self=self)
