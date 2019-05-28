fa8a7463993257aed34024247bcc7455e482f9bb
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-05-28 16:59:14 -0400

    did some of the player onbject

--- src/player.py
+++ src/player.py
@@ -1,2 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player: 
+    def __init__(self, name, cur_room):
+        self.name = name
+        self.cur_room = cur_room
