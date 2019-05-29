f2c56ab66a4f498028a1a88363bea7be8722fc0d
Ivana Huckova <ivana.huckova@gmail.com>
2019-05-29 16:16:35 +0200

    Complete the game

--- src/adv.py
+++ src/adv.py
@@ -4,54 +4,126 @@ from player import Player
 # Declare all the rooms
 
 room = {
-    'winterfell':   Room("Winterfell", "Winterfell is the seat and the ancestral home of the royal House Stark. It is a very large castle located at the center of the North, from where the head of House Stark rules over his or her people."),
+    'winterfell':   Room("Winterfell", "Winterfell is the seat and the ancestral home of the royal House Stark. It is a very large castle located at the center of the North, from where the head of House Stark rules over his or her people.", "🐺"),
 
-    'riverrun':    Room("Riverrun", "Riverrun is the seat of House Tully, which was once occupied by House Frey. It is a large castle located in the central-western part of the Riverlands."),
+    'riverrun':    Room("Riverrun", "Riverrun is the seat of House Tully, which was once occupied by House Frey. It is a large castle located in the central-western part of the Riverlands.", "⛲️"),
 
-    'kings_landing': Room("Kings Landing", "King's Landing is the capital, and largest city, of the Seven Kingdoms. It enjoys a warm climate and life there is luxurious for those that can afford it."),
+    'kings_landing': Room("Kings Landing", "King's Landing is the capital, and largest city, of the Seven Kingdoms. It enjoys a warm climate and life there is luxurious for those that can afford it.", "👑"),
 
-    'braavos':   Room("Braavos", "Braavos is one of the Free Cities located to the east of Westeros."),
+    'eyrie':   Room("Eyrie", "The Eyrie is the principal stronghold of House Arryn. It is located in the Vale of Arryn near the east coast of Westeros. ", "🗻"),
 
-    'casterly_rock': Room("Casterly Rock", "Casterly Rock is the ancestral stronghold of House Lannister. It is located on the Western coast of Westeros on a rocky promontory overlooking the Sunset Sea."),
+    'casterly_rock': Room("Casterly Rock", "Casterly Rock is the ancestral stronghold of House Lannister. It is located on the Western coast of Westeros on a rocky promontory overlooking the Sunset Sea.", "👫"),
+
+    'pyke': Room("Pyke", "Pyke is the stronghold and seat of House Greyjoy, located on the island of the same name, which is one of the seven major Iron Islands. ", "⚓️"),
+
+    'highgarden': Room("Highgarden", "Highgarden was the seat of House Tyrell and is the regional capital of the Reach. Located on the banks of the river Mander, Highgarden sits astride the Roseroad, a major thoroughfare linking Oldtown and King's Landing.", "🏆"),
+
+    'stormsend': Room("Storm's End", "Storm's End is the ancestral seat of House Baratheon. Lord Gendry Baratheon is the Lord of Storm's End. Storm's End is a formidable fortress, located on the southeastern coast of Westeros overlooking Shipbreaker Bay.", " 🌊"),
+
+    'sunspear': Room("Sunspear", "Sunspear is the capital of Dorne Dorne, southernmost of the Seven Kingdoms, located in the far southeast of the continent on the Summer Sea.", "☀️"),
+
+
+
 }
 
 
 # Link rooms together
-
 room['winterfell'].n_to = room['riverrun']
+
+room['pyke'].n_to = room['casterly_rock']
+room['pyke'].e_to = room['riverrun']
+
+room['riverrun'].n_to = room['highgarden']
 room['riverrun'].s_to = room['winterfell']
-room['riverrun'].n_to = room['kings_landing']
-room['riverrun'].e_to = room['braavos']
-room['kings_landing'].s_to = room['riverrun']
-room['braavos'].w_to = room['riverrun']
-# room['braavos'].n_to = room['casterly_rock']
-# room['casterly_rock'].s_to = room['braavos']
+room['riverrun'].w_to = room['pyke']
+room['riverrun'].e_to = room['eyrie']
 
-#
-# Main
-#
+room['eyrie'].w_to = room['riverrun']
+room['eyrie'].n_to = room['kings_landing']
 
-# Make a new player object that is currently in the 'winterfell' room.
-player1 = Player("John", "winterfell")
 
-# Write a loop that:
+room['casterly_rock'].s_to = room['pyke']
+room['casterly_rock'].e_to = room['highgarden']
 
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+room['highgarden'].w_to = room['casterly_rock']
+room['highgarden'].e_to = room['kings_landing']
+room['highgarden'].s_to = room['riverrun']
+room['highgarden'].n_to = room['sunspear']
+
+room['kings_landing'].s_to = room['eyrie']
+room['kings_landing'].n_to = room['stormsend']
+room['kings_landing'].w_to = room['highgarden']
+
+room['sunspear'].s_to = room['highgarden']
+room['sunspear'].e_to = room['stormsend']
+
+room['stormsend'].w_to = room['sunspear']
+room['stormsend'].s_to = room['kings_landing']
+
+
+player = Player("Ivana", room["winterfell"])
 directions = ''
 
+print("\n==========================================================")
+print(f" 🌏   Hello {player.name} and welcome to TRAVEL AROUND WESTEROS")
+print("==========================================================\n")
+
+print(
+    f"You are currently in {player.current_room.name.upper()} {player.current_room.emoji} ")
+
 while directions != 'q':
-    print(
-        f"You, {player1.name} are currently in {room[player1.current_room].name} ")
-
     directions = input(
-        "Where would you like to go? Use north/south/west/east for navigation")
-
-    print(f"You have decided to go {directions} ")
+        "❔   Would you like to stay or go somewhere else? Use n/s/w/e for navigation: ")
+    try:
+        if directions == "n":
+            if player.current_room.s_to:
+                player.current_room = player.current_room.s_to
+                print("...walking...")
+                print(
+                    f"You have decided to go north and you moved to {player.current_room.name.upper()} {player.current_room.emoji}")
+                print(f"{player.current_room.describtion}")
+            else:
+                print(
+                    f"⬆️  There is nothing north of {player.current_room.name.upper()} {player.current_room.emoji}")
+        elif directions == "s":
+            if player.current_room.n_to:
+                player.current_room = player.current_room.n_to
+                print("...walking...")
+                print(
+                    f"You have decided to go south and you moved to {player.current_room.name.upper()} {player.current_room.emoji}")
+                print(f"{player.current_room.describtion}")
+            else:
+                print(
+                    f"⬇️  There is nothing south of {player.current_room.name.upper()} {player.current_room.emoji}")
+        elif directions == "e":
+            if player.current_room.w_to:
+                player.current_room = player.current_room.w_to
+                print("...walking...")
+                print(
+                    f"You have decided to go east and you moved to {player.current_room.name.upper()} {player.current_room.emoji}")
+                print(f"{player.current_room.describtion}")
+            else:
+                print(
+                    f"➡️  There is nothing east of {player.current_room.name.upper()} {player.current_room.emoji}")
+        elif directions == "w":
+            if player.current_room.e_to:
+                player.current_room = player.current_room.e_to
+                print("...walking...")
+                print(
+                    f"You have decided to go east and you moved to {player.current_room.name.upper()}")
+                print(f"{player.current_room.describtion}")
+            else:
+                print(
+                    f"⬅️  There is nothing west of {player.current_room.name.upper()}")
+        elif directions == "q":
+            print("\n==========================================================")
+            print(
+                f"     SEEMS LIKE YOU ENJOY YOUR LIFE IN {player.current_room.name.upper()} {player.current_room.emoji}")
+            print(
+                f"         GOOD CHOICE AND THANK YU FOR PLAYING!")
+            print("==========================================================\n")
+            break
+        else:
+            print("Please select a valid direction")
+    except ValueError:
+        print("Please enter the valid direction")
--- src/room.py
+++ src/room.py
@@ -3,9 +3,14 @@
 
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, describtion, emoji):
         self.name = name
-        self.description = description
+        self.describtion = describtion
+        self.emoji = emoji
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
 
     def __str__(self):
-        return f"{self.name} - {self.description}"
+        return f"{self.name} - {self.describtion}"
