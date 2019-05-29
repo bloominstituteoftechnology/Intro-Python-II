b62681cd881ba798af360fe9ac5c133bfe47a137
griffinshuth <griffinshuth@gmail.com>
2019-05-28 21:39:25 -0600

    Griffin Huth Adventure Game

--- src/player.py
+++ src/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+Class Player1
+
+def __init__( self, name, current_room):
+ 	self.name = name
+        self.current_room = current_room
+	
