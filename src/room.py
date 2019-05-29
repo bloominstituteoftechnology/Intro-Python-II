c0be33c383244d5c020e685ec4b9ae5ec533b845
Carlos Gutierrez <chgutier@gmail.com>
2019-05-29 17:53:07 -0500

    Initial code to create game

--- src/room.py
+++ src/room.py
@@ -1,2 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, title, description, n_to="", s_to="", e_to="", w_to=""):
+        self.title = title
+        self.description = description
+        self.n_to = n.to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
+
+    
+
