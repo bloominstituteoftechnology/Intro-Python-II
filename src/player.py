075b5409d80f2b1e87475f00c5e742bd422b9240
Megan Fontenot <meganfontenot517@yahoo.com>
2019-05-28 22:44:20 -0500

    Intial Commit

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+  """ Parent class for Playa Playa """
+  def __init__ (self, name, inside_room, inventory)
+    self.name = name 
+    self.inside_room = inside_room
+    self.inventory = inventory 
+
+
