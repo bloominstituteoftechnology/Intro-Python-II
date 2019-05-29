10705e4ff3fc7d437edd4470f5ba09f315496449
Kendra Williams <kmonique.williams@gmail.com>
2019-05-28 22:45:06 -0400

    created player class with attributes name and current room

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player():
+   name = ""
+   current_room: "outside"
+
+   def change_name(self, name):
+      self.name = name
+   def move_room(self, new_room):
+      self.current_room = new_room
