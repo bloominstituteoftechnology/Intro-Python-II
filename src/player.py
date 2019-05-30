130ab3172ea13f468fbada5023c44b78ddd5a5a0
AustinBCole <austin.cole.chileno@gmail.com>
2019-05-30 17:31:01 -0600

    Change Character and Spider classes to their own file.

--- src/player.py
+++ src/player.py
@@ -97,27 +97,4 @@ class Player:
         else:
             print("\nYou currently have no items.\n")
 
-class Character:
-    def __init__(self, health, current_room):
-        self.health = health
-        self.items = []
-        self.current_room = current_room
 
-    def travel(self):
-        print("started")
-        possible_exits = self.current_room.get_room_exits()
-        dark_exits = [exit for exit in possible_exits if self.current_room.get_room_in_direction(exit).is_light == False]
-        direction = random.choice(dark_exits)
-        next_room = self.current_room.get_room_in_direction(direction)
-        if next_room.present_player is not None:
-            player_light_source = [item for item in self.items if type(item) == LightSource]
-            room_light_source = [item for item in self.current_room.items if type(item) == LightSource]
-            if self.current_room.is_light or len(player_light_source) > 0 or    len(room_light_source) > 0:
-                return
-        self.current_room = next_room
-        print(f"{self.current_room.present_player}")
-
-class Spider(Character):
-    def __init__(self, health, current_room, is_poisonous):
-        super().__init__(health, current_room)
-        self.is_poisonous = is_poisonous
