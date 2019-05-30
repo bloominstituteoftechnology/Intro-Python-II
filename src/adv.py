7b9d7b9e6fc0b9c85a199a152cec11feecc646f7
Carlos Gutierrez <chgutier@gmail.com>
2019-05-30 00:14:11 -0500

    "Day 1 MVP"

--- src/adv.py
+++ src/adv.py
@@ -1,10 +1,11 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons."),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -35,10 +36,18 @@ room['treasure'].s_to = room['narrow']
 
 #
 # Main
-if __name__ == "__main__":
-    
+# if __name__ == "__main__":
 
 # Make a new player object that is currently in the 'outside' room.
+curr_player = Player("P1", room["outside"])
+
+# _______ Intro Message _______
+print("//////////////////////////////////")
+print("////// Welcome to THE MAZE //////")
+print("/////////////////////////////////\n")
+print(f"Your current location is the {curr_player.location.title}.")
+print("..."+curr_player.location.description+"..\n")
+print("To move around choose from [n]North, [s]South, [e]East, or [w]West.\nOr [q] to Quit from The Maze\n")
 
 # Write a loop that:
 #
@@ -50,3 +59,70 @@ if __name__ == "__main__":
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+# Begining of REPL (Read, Evaluate, Print, Loop)
+# will wait for response from user and according to it
+# proceed to process move and print out description of room
+# or print out message in case move fails
+# then wait for user input again
+command = input("Where would you like to go to begin?: ")
+# print('INPUT GIVEN BY USER:', command.strip(' '))
+
+commands = ['n', 's', 'e', 'w', 'q']
+
+# chosen_loc = None
+# if cmd =='n':
+#     chosen_loc = 'north'
+# elif cmd == 's':
+#     chosen_loc = 'south'
+# elif cmd == 'e':
+#     chosen_loc = 'east'
+# elif cmd == 'w':
+#     chosen_loc = 'west'
+
+while command in commands: # quit game if 'q' entered
+    if command == "q":
+        break
+
+    elif command == 'n':
+        print(f'You have chosen to go North.\n')
+        if curr_player.location.n_to == None:
+                #curr_player.location = curr_player.location.n_to
+                print("--> You are not able to go north based on your current location. <--\n")
+        else:
+            curr_player.location = curr_player.location.n_to
+            #print("Not able to go north based on your current location.\n")
+
+    elif command == 's':
+        print('You have chosen to go South.\n')
+        if curr_player.location.s_to == None:
+                #curr_player.location = curr_player.location.n_to
+                print("--> You are not able to go south based on your current location. <--\n")
+        else:
+            curr_player.location = curr_player.location.s_to
+            #print("Not able to go north based on your current location.\n")
+
+    elif command == 'e':
+        print('You have chosen to go East.\n')
+        if curr_player.location.e_to == None:
+                #curr_player.location = curr_player.location.n_to
+                print("--> You are not able to go east based on your current location. <--\n")
+        else:
+            curr_player.location = curr_player.location.e_to
+            #print("Not able to go north based on your current location.\n")
+
+    elif command == 'w':
+        print('You have chosen to go West.\n')
+        if curr_player.location.w_to == None:
+                #curr_player.location = curr_player.location.n_to
+                print("--> You are not able to go west based on your current location. <--\n")
+        else:
+            curr_player.location = curr_player.location.w_to
+            #print("Not able to go north based on your current location.\n")
+
+    print(f'Your current location is the {curr_player.location.title}.')
+    print("..."+curr_player.location.description+"..")
+    command = input("\nWhere would you like to go now?\n ([n]North, [s]South, [e]East, or [w]West.\nOr [q] to Quit): ")
+
+
+
--- src/room.py
+++ src/room.py
@@ -1,13 +1,15 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
-    def __init__(self, title, description, n_to="", s_to="", e_to="", w_to=""):
+    def __init__(self, title, description, n_to=None, s_to=None, e_to=None, w_to=None):
         self.title = title
         self.description = description
-        self.n_to = n.to
+        self.n_to = n_to
         self.s_to = s_to
         self.e_to = e_to
         self.w_to = w_to
 
+    def __str__(self):
+        return self.description
     
 
