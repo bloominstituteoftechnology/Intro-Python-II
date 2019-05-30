669919957d6ce77fe27785efed8bf79aa9d1050b
blokboy <proxxist@gmail.com>
2019-05-30 10:13:04 -0400

    Adding Player Class

--- src/adv.py
+++ src/adv.py
@@ -1,17 +1,26 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
+thief = Player('Kjadra, The Cunning', 1, 2, 'Human', 'Thief')
+brawler = Player('Yaegar, The Mighty', 3, 8, 'Orc', 'Brawler')
+
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", 
+                     thief, 
+                     ['Iron Sword', 'Iron Shield', '5 Gold Pieces', 'Wine']),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer", 
+                     """Dim light filters in from the south. Dusty passages run north and east.""",
+                     brawler,
+                     ['Silver Helmet', '15 Gold Pieces', 'Goblet'] ),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", 
+                     """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in
+                        the distance, but there is no way across the chasm.""",
+                    items=['The Skeleton Key', 'The Sword of 1000 Truths']),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
@@ -39,6 +48,7 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +59,44 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+def createCharacter():
+    races = ['', 'Night Elf', 'High Elf', 'Human', 'Orc', 'Jin', 'Draconian']
+    classes = ['', 'Shadow Priest', 'Bard', 'Warlord', 'Nighthawk', 'Mage', 'Sentinel', 'Bounty Hunter', 'Smuggler']
+
+    name = input("What is the name of your hero? ")
+    race = int(input("What is your race? 1 - Night Elf, 2 - High Elf, 3 - Human, 4 - Orc, 5 - Jin, 6 - Draconian "))
+    archetype = int(input("What is your class? 1 - Shadow Priest, 2 - Bard, 3 - Warlord, 4 - Nighthawk, 5 - Mage, 6 - Sentinel, 7 - Bounty Hunter, 8 - Smuggler "))
+
+    if race < 0 or race > 6: 
+        race = 3 #default to creating human 
+
+    if archetype < 0 or archetype > 8:
+        archetype = 3 # default to Warlord 
+
+    return Player(name, 3, races[race], classes[archetype], current_room=room['outside'])
+
+player = createCharacter()
+
+def getMove(player):
+    print(f'{player.current_room}')
+    print(f'Your available moves are: {player.getAvailableRooms()}')
+
+    legal_moves = player.getAvailableRooms()
+    move = ""
+
+    while move not in legal_moves.keys():
+        move = input('Which direction would you like to go? (N, S, E, W) ')
+
+    if move == 'N':
+        player.current_room = player.current_room.n_to
+    if move == 'S':
+        player.current_room = player.current_room.s_to
+    if move == 'E':
+        player.current_room = player.current_room.e_to
+    if move == 'W':
+        player.current_room = player.current_room.w_to
+
+print(player.current_room)
+getMove(player)
+print(player.current_room)
--- src/player.py
+++ src/player.py
@@ -1,2 +1,38 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player():
+    def __init__(self, name, dmg, race, archetype, lv=1, inv=[], current_room=''):
+        self.name = name
+        self.dmg = dmg
+        self.race = race 
+        self.archetype = archetype 
+        self.lv = lv 
+        self.inv = inv
+        self.current_room = current_room 
+
+    def __str__(self):
+        if self.current_room == '': #if they are an enemy
+            return f'They are {self.name}, a level {self.lv} {self.race} {self.archetype}.'
+        return f'You are {self.name}, a level {self.lv} {self.race} {self.archetype}.'
+
+    def getRoom(self):
+        return self.current_room 
+
+    def getAvailableRooms(self):
+        directions = {}
+  
+        if hasattr(self.current_room, 'n_to'):
+            directions['N'] = self.current_room.n_to.name
+
+        if hasattr(self.current_room, 'w_to'):
+            directions['W'] = self.current_room.w_to.name
+
+        if hasattr(self.current_room, 's_to'):
+            directions['S'] = self.current_room.s_to.name
+    
+        if hasattr(self.current_room, 'e_to'):
+            directions['E'] = self.current_room.e_to.name
+
+        return directions
+
+    
