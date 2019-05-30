697862ad17c51a18d01671b81791e8518fbc337f
Robert Billington <chillingtonbillington@gmail.com>
2019-05-30 11:39:44 -0500

    Created Player Class in player.py

--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+#name
+#current room
+
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+
+    def __repr__(self):
+        return f"{self.name} is in the {self.current_room}"    
