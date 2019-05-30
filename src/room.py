c6e7c3a35f5d3fc2f20da8f2c39ea83f5e045db8
Jor_Poon <jormpoon@gmail.com>
2019-05-30 16:51:01 -0400

    create the ability for room class to hold item in a list

--- src/room.py
+++ src/room.py
@@ -5,6 +5,10 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.items_stored = []
     
     def __str__(self):
         return f' At "{self.name} , "{self.description}"'
+
+    def placed_item(self, item):
+        self.items_stored.append(item)
