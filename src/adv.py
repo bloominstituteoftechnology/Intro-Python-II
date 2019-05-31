bf97d89ddd66ee703ed9f9e69390ee4306c35518
Emmanium <pradoe.ep@gmail.com>
2019-05-30 21:48:46 -0600

    added some code to every script to make this thing work on a basic level the user now has the basic menu to select where to go

--- src/adv.py
+++ src/adv.py
@@ -5,46 +5,50 @@ from item import Item
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons", "Nothing"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""",),
+    'studio':  Room("Studio", """Dim light filters in from the south. Dusty
+passages run north and east.""", "Katana"),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", "Nothing"),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", "Nothing"),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", "MIDI Keyboard"),
 }
 
-item = {
+items = {
     "Katana": Item("Katana", "Shiny and sharp, please don't poke people with the pointy end", "Swing, Slice"),
     "MIDI Keyboard": Item("MIDI Keyboard", "12 Keys Black and White, One Hole, Two connections", "Music Powa")
 }
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
+room['outside'].n_to = room['studio']
+room['studio'].s_to = room['outside']
+room['studio'].n_to = room['overlook']
+room['studio'].e_to = room['narrow']
+room['overlook'].s_to = room['studio']
+room['narrow'].w_to = room['studio']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-room["treasure"].items = [item["Katana"], item["MIDI Keyboard"]]
+room["treasure"].items = [items["Katana"], items["MIDI Keyboard"]]
 #
 # Main
 #
 
+currRoom = room["studio"]
+
+name = input("Enter your name soldier\n")
+
 # Make a new player object that is currently in the 'outside' room.
-player_one = Player('outside')
+player = Player(name, room["outside"], items)
 # Write a loop that:
 #
 # * Prints the current room name
@@ -56,3 +60,27 @@ player_one = Player('outside')
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+direction = ""
+
+while True:
+    print(
+        f"Current space\n You are Carrying: {player.items}\n This room has these items: {player.room.items}\n {player.room.description}")
+    directionChoice = input(
+        "Pick a direction to move: n, w, s, or e, or enter q to quit\n")
+
+    if directionChoice == "n" or directionChoice == "w" or directionChoice == "s" or directionChoice == "e" or directionChoice == "q":
+        if directionChoice == "n":
+            player.room == player.room.n_to
+        elif directionChoice == "w":
+            player.room == player.room.w_to
+        elif directionChoice == "s":
+            player.room == player.room.s_to
+        elif directionChoice == "e":
+            player.room == player.room.e_to
+        # Quits the game
+        elif directionChoice == "q":
+            print("Ended the game")
+            break
+        else:
+            print("Pick another way to walk friend")
--- src/player.py
+++ src/player.py
@@ -3,5 +3,10 @@
 
 
 class Player:
-    def __init__(self, cur_room):
-        self.cur_room = cur_room
+    def __init__(self, name, room, items):
+        self.name = name
+        self.room = room
+        self.items = items
+
+    def __str__(self):
+        return f"My name is {self.name} and I am carrying {self.items}."
