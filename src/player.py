d9d319b3246f2598123bc697ec670c0502c44582
vy3191 <venkyafhs@gmail.com>
2019-05-30 23:29:20 -0400

    Modified Player.py

--- src/player.py
+++ src/player.py
@@ -2,9 +2,13 @@
 # currently.
 
 class Player:
-  def __init__(self, name, rName):
+  def __init__(self, name, rName, items=None):
     self.name = name
     self.rName = rName
+    if items is None:
+      self.items = []
+    else:
+      self.items = items  
 
   def __str__(self):
     return f"Hi, {self.name} you are in {self.rName} room."
