3300e7e41a9de55e6065357948c21203b7961627
Jonathan Brock <jbrock1981@gmail.com>
2019-05-30 22:03:20 -0500

    initial commit, mostly completed

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
-
+from player import Player
+from item import Item
 # Declare all the rooms
 
 room = {
@@ -21,6 +22,11 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
+item = {
+    "Staff": Item("Staff", "For walking and fighting"),
+    "Health Potion": Item("Health Potion", "For preventing death")
+
+}
 
 # Link rooms together
 
@@ -33,12 +39,12 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+room["outside"].items = [str(item["Staff"]), str(item["Health Potion"])]
+
 #
 # Main
 #
-
 # Make a new player object that is currently in the 'outside' room.
-
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +55,32 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+currentRoom = room["outside"]
+
+name = input("Enter your player name choice\n")
+
+player = Player(name, room["outside"])
+
+while True:
+    print(f"you are in room: {player.room.name}\n carrying items: {player.items}\n and the room has items: {player.room.items}\n {player.room.description}")
+    moveChoice = input("choose a move: n, s, e or w, or enter q to quit\n")
+    print(f"you chose {moveChoice}\n")
+    if moveChoice == 'n' or moveChoice == 's' or moveChoice == 'w' or moveChoice == 'e' or moveChoice == 'q':
+        if moveChoice == "n":
+            player.room = player.room.n_to
+        elif moveChoice == "s":
+            player.room = player.room.s_to
+        elif moveChoice == "w":
+            player.room == player.room.w_to
+        elif moveChoice == "e":
+            player.room == player.room.e_to
+        elif moveChoice == "q":
+            print("exiting the game now, goodbye")
+            break
+        #this ability to grab items not working yet    
+        # elif moveChoice == 'get':
+        #     player.items.append(moveChoice)
+        #     print("Item grabbed, good job!")
+        else:
+            print("c'mon, enter a correct direction choice, man!")
