4e0a72a43bec122a44c4204a1317cc0784d1e9a7
vy3191 <venkyafhs@gmail.com>
2019-05-30 22:52:14 -0400

    Modified player.py

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+  def __init__(self, name, rName):
+    self.name = name
+    self.rName = rName
+
+  def __str__(self):
+    return f"Hi, {self.name} you are in {self.rName} room."
+
