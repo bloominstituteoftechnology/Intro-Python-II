22b55405948730aa18de7cc274c98e2883feea57
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-05-31 12:42:23 -0400

    completed MVP 1

--- src/adv.py
+++ src/adv.py
@@ -53,36 +53,86 @@ room['treasure'].s_to = room['narrow']
 #
 # If the user enters "q", quit the game.
 
-my_player = Player("Abdiel", room["outside"])
-possible_directions = ["n", "s", "e", "w", "q"]
+# my_player = Player("Abdiel", room["outside"])
+possible_directions = ["n", "s", "e", "w"]
 
 def get_direction():
-    direction = input("Which way would you like to go?").lower()
-    if direction in possible_directions:
-        return direction
-    else:
-        print(f"{direction} is not a valid input, please choose between 'n', 's', 'e', 'w', 'q'")
-        return "error"
-
-def attempt_move(cur_room):
-    direction = get_direction()
+    direction = input("Which way would you like to go? \n").lower()
 
     if direction == "q":
-        return False
-    elif direction == "error":
-        attempt_move(cur_room)
-    else:
+        return direction
+    elif direction in possible_directions:
         direction = f"{direction}_to"
-    
-    print(f"it is trying to run {direction}")
-    room_returned = getattr(cur_room, direction)
-    print(f"room returned is {room_returned}")
-    if room_returned == None:
-        print("There is no room in that direction")
-        return cur_room
-    else:
-        return room_returned
+        return direction
+    else :
+        print(f"{direction} is not a valid input, please choose between 'n', 's', 'e', 'w', 'q'\n" )
+        return None
 
-while(my_player.cur_room):
-    print(f"{my_player.name} is currently at {my_player.cur_room.name}")
-    my_player.cur_room = attempt_move(my_player.cur_room)
+def attempt_move(current_room, direction):
+    if direction == "q":
+        return False
+    elif direction == None:
+        return current_room
+    else:
+        if hasattr(current_room, direction):
+            attribute_returned = getattr(current_room, direction)
+            if attribute_returned == None:
+                print("There is no room in that direction \n")
+                return current_room
+            else:
+                return attribute_returned
+        else:
+            print("There is no room in that direction \n")
+            return current_room
+
+
+def begin_move():
+    player_name =  input("Before we start, let's get your name: \n")
+    my_player = Player(player_name, room['outside'])
+    print(f"Great, {my_player.name}, let's begin.\n")
+    print(f"We will start of at the {my_player.cur_room.name} where {my_player.cur_room.description}")
+    ask_if_ready = input("\nReady to begin? (Y/N)\n").lower()
+    if ask_if_ready == 'y':
+        while(my_player.cur_room):
+            direction = get_direction()
+            my_player.cur_room = attempt_move(my_player.cur_room, direction)
+            if my_player.cur_room == False:
+                pass
+            else:
+                print(f"\nYou are in the {my_player.cur_room.name} where {my_player.cur_room.description}\n")
+
+        print("\nThank you for playing! See ya soon")
+    else:
+        print("\nThank you for stopping by! See ya soon")
+
+begin_move()
+# def get_direction():
+#     direction = input("Which way would you like to go?").lower()
+#     if direction in possible_directions:
+#         return direction
+#     else:
+#         print(f"{direction} is not a valid input, please choose between 'n', 's', 'e', 'w', 'q'")
+#         return "error"
+
+# def attempt_move(cur_room):
+#     direction = get_direction()
+
+#     if direction == "q":
+#         return False
+#     elif direction == "error":
+#         attempt_move(cur_room)
+#     else:
+#         direction = f"{direction}_to"
+    
+#     print(f"it is trying to run {direction}")
+#     room_returned = getattr(cur_room, direction)
+#     print(f"room returned is {room_returned}")
+#     if room_returned == None:
+#         print("There is no room in that direction")
+#         return cur_room
+#     else:
+#         return room_returned
+
+# while(my_player.cur_room):
+#     print(f"{my_player.name} is currently at {my_player.cur_room.name}")
+#     my_player.cur_room = attempt_move(my_player.cur_room)
