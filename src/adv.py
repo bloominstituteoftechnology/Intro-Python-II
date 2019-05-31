affb03f471e4ec86b5742793e2acaacda12e2948
Jesse Anderson <janders9009@gmail.com>
2019-05-30 20:30:58 -0500

    player is able to interact with the game and move from room to room

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,6 @@
+import os
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -39,14 +41,60 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+player = Player('Player 1', 'outside')
+print(f"Welcome {player.name}")
+print("Enter q or Q to quit the game.")
+print('')
 # Write a loop that:
 #
+good_move = True 
+while True:
+    if good_move:
+        os.system('clear')
+    good_move = True
 # * Prints the current room name
+    print(f"Current Location: {room[player.cur_room]}")
 # * Prints the current description (the textwrap module might be useful here).
+    print(room[player.cur_room].description)
 # * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
+    action = input("Where would you like to go? ")
 #
 # If the user enters "q", quit the game.
+    if action.lower() == 'q':
+        os.system('clear')
+        print("Thank you for playing!\n")
+        exit()
+# If the user enters a cardinal direction, attempt to move to the room there.
+    elif action.lower() == 'n':
+        if not room[player.cur_room].n_to == None:
+            room[player.cur_room] = room[player.cur_room].n_to
+        else:
+            good_move = False
+            os.system('clear')
+            print(f"\nThere is no room to the North of {room[player.cur_room]}\n")
+    elif action.lower() == 'e':
+        if not room[player.cur_room].e_to == None:
+            room[player.cur_room] = room[player.cur_room].e_to
+        else:
+            good_move = False
+            os.system('clear')
+            print(f"\nThere is no room to the East of {room[player.cur_room]}\n")
+        # room[player.cur_room] = room[player.cur_room].e_to
+    elif action.lower() == 's': 
+        if not room[player.cur_room].s_to == None:
+            room[player.cur_room] = room[player.cur_room].s_to
+        else:
+            good_move = False
+            os.system('clear')
+            print(f"\nThere is no room to the South of {room[player.cur_room]}\n")
+    elif action.lower() == 'w':
+        if not room[player.cur_room].w_to == None:
+            room[player.cur_room] = room[player.cur_room].w_to
+        else:
+            good_move = False
+            os.system('clear')
+            print(f"\nThere is no room to the West of {room[player.cur_room]}\n")
+    else:
+# Print an error message if the movement isn't allowed.
+        print("\nPlease enter valid direction. (n, e, s, w)\n")
+#
