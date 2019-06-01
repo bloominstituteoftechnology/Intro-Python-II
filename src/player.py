3ae3cb510d85d94623f8b499f7160193ff430f99
Jesse Anderson <janders9009@gmail.com>
2019-06-01 09:51:25 -0500

    added functionality to the game

--- src/player.py
+++ src/player.py
@@ -5,6 +5,7 @@ class Player:
     def __init__(self, name, cur_room):
         self.name = name
         self.cur_room = cur_room
+        items = [None]
 
     def __str__(self):
         return f"Hello {self.name}. Your current location is: {self.cur_room}"
