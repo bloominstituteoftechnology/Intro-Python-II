51d809058f067a3cc2e450acedadf74dc63f5321
Megan Fontenot <meganfontenot517@yahoo.com>
2019-06-01 15:25:08 -0500

    Changed player class, made room class, added an intro fuction probably will not use it.

--- src/adv.py
+++ src/adv.py
@@ -1,41 +1,59 @@
-from room import Room
+import random
+
+# from room import Room
 
 # Declare all the rooms
 
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
-}
+# room = {
+#     "outside": Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+#     "foyer": Room(
+#         "Foyer",
+#         """Dim light filters in from the south. Dusty
+# passages run north and east.""",
+#     ),
+#     "overlook": Room(
+#         "Grand Overlook",
+#         """A steep cliff appears before you, falling
+# into the darkness. Ahead to the north, a light flickers in
+# the distance, but there is no way across the chasm.""",
+#     ),
+#     "narrow": Room(
+#         "Narrow Passage",
+#         """The narrow passage bends here from west
+# to north. The smell of gold permeates the air.""",
+#     ),
+#     "treasure": Room(
+#         "Treasure Chamber",
+#         """You've found the long-lost treasure
+# chamber! Sadly, it has already been completely emptied by
+# earlier adventurers. The only exit is to the south.""",
+#     ),
+# }
 
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+# room["outside"].n_to = room["foyer"]
+# room["foyer"].s_to = room["outside"]
+# room["foyer"].n_to = room["overlook"]
+# room["foyer"].e_to = room["narrow"]
+# room["overlook"].s_to = room["foyer"]
+# room["narrow"].w_to = room["foyer"]
+# room["narrow"].n_to = room["treasure"]
+# room["treasure"].s_to = room["narrow"]
 
-#
+# #
 # Main
 #
+
+
+def intro():
+    print(
+        """
+        You are the fiercely independent grandchild of an eccentric adventurer who vanished years earlier. Hoping to solve the mystery of your grandfather's disappearance, you must embark on a perilous journey to his last-known destination -- a fabled tomb on a mythical island that might be somewhere off the coast of Japan. The stakes couldn't be higher as you must rely on your sharp mind, blind faith and stubborn spirit to venture into the unknown.
+        """
+    )
+
 
 # Make a new player object that is currently in the 'outside' room.
 
@@ -49,3 +67,6 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+intro()
+
--- src/player.py
+++ src/player.py
@@ -1,11 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
-class Player:
-  """ Parent class for Playa Playa """
-  def __init__ (self, name, inside_room, inventory)
-    self.name = name 
-    self.inside_room = inside_room
-    self.inventory = inventory 
 
+class player:
+    def __init__(self, name, description, fanny_pac, current):
+        self.name = name
+        self.description = description
+        self.fanny_pac = fanny_pac
+        self.current = current
+
+    def __repr__(self):
+        return
 
