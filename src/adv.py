a8e04d80cc36b7bad34a5c183d168b61e9756fbb
louislee7 <7.louislee@gmail.com>
2019-06-03 15:08:19 -0700

    finished mvp

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -39,6 +40,8 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+newPlayer = Player('Louis', room['outside'])
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +52,20 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+playerInput = ''
+while str(playerInput) != 'q':
+    print(newPlayer.__str__())
+    playerInput = input(
+        "Where do you want to move next? Enter 'n, e, s, w' to move, or enter 'q' to quit.  ")
+    try:
+        if str(playerInput) == 'n':
+            newPlayer.room = newPlayer.room .n_to
+        elif str(playerInput) == 'e':
+            newPlayer.room = newPlayer.room .e_to
+        elif str(playerInput) == 's':
+            newPlayer.room = newPlayer.room .s_to
+        elif str(playerInput) == 'w':
+            newPlayer.room = newPlayer.room .w_to
+    except:
+        print("You cannot go this way!")
+print("You have exited the game")
