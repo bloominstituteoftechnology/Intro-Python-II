e7bbc469f69c5b73cdc9c5d398ab24478f838d49
Frulwinn <frulwinn@gmail.com>
2019-06-06 17:12:50 -0500

    added the item class

--- src/player.py
+++ src/player.py
@@ -3,14 +3,19 @@
 
 class Player:
 
-    def __init__(self, name, room):
+    def __init__(self, name, current_room):
         self.name = name
-        self.room = room
+        self.current_room = current_room
     
     def __str__(self):
-        return f'{self.name} is in {self.room}'
+        return f'{self.name} is in {self.current_room}'
 
     def get_name(self):
         return str(self.name)
-    def get_current_room(self):
+
+    def get_current_room(self, current_room):
         return str(self.current_room)
+
+    def move_direction(self, direction):
+        if direction == "n"
+            self.current_room.n_to
