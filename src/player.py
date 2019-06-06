cff9ecfe205807dcced93235c247384346c4b41b
Frulwinn <frulwinn@gmail.com>
2019-06-06 17:15:33 -0500

    added inventory to player

--- src/player.py
+++ src/player.py
@@ -6,14 +6,15 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
     
     def __str__(self):
         return f'{self.name} is in {self.current_room}'
-
+    def add_item(self, item):
+        self.inventory.append(item)
     def get_name(self):
         return str(self.name)
-
-    def get_current_room(self, current_room):
+    def get_current_room(self):
         return str(self.current_room)
 
     def move_direction(self, direction):
