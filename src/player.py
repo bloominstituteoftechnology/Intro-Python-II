6d3b5afb40e58d8c78faf08f9875b744da94f222
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-10 21:23:31 +0530

    created initial objects

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    name = ""
+    current_room = ""
+
+    def __init__ (self, name, current_room):
+        self.name = name
+        self.current_room = current_room
--- src/room.py
+++ src/room.py
@@ -1,2 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    name = ""
+    description = ""
+
+    def __init__ (self, name, description):
+        self.name = name
+        self.description = description
