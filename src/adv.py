5992b27a246dcf70d7bf1d1d680f097fdc058d5e
derek-shing <hpshing@gmail.com>
2019-07-11 10:19:58 -0700

    Day 2 MVP

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
@@ -23,6 +24,11 @@ earlier adventurers. The only exit is to the south."""),
 }
 
 
+items={
+    "gold1" : Item("gold", "1000 gold"),
+    "bag1" : Item("bag","bag")
+}
+
 # Link rooms together
 
 room['outside'].n_to = room['foyer']
@@ -34,6 +40,11 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+#Add items to room
+
+room['treasure'].add_item(items["gold1"])
+room['outside'].add_item(items["bag1"])
+
 #
 # Main
 #
@@ -41,6 +52,7 @@ room['treasure'].s_to = room['narrow']
 # Make a new player object that is currently in the 'outside' room.
 player1 = Player("player1",room["outside"])
 print(player1)
+player1.current_room.show_item()
 
 def move(player, direction):
     if direction=='n':
@@ -58,6 +70,43 @@ def move(player, direction):
         player.current_room=target
     print(player)
 
+valid_move =['n','s','w','e']
+
+def search_item(name):
+    l = []
+    for (key, value ) in items.items():
+        if value.name==name:
+            l.append(items[key])
+    #l = [i for (key,value) in items if i.value.name==name]
+    return l
+
+def get_item_function(name):
+    l = search_item(name) #get the item list by name
+    found = False
+    for i in l :          #check if any of the item in the current room
+        if i in player1.current_room.item_list:
+            found = True
+            break
+    if found==False:
+        print(f"{name} does not exist in this room")
+    else:
+        player1.get_item(i)
+        player1.current_room.remove_item(i)
+
+
+def drop_item_function(name):
+    l = search_item(name) #get the item list by name
+    found = False
+    for i in l :          #check if any of the item in the current room
+        if i in player1.inventory:
+            found = True
+            break
+    if found==False:
+        print(f"{player1.name} does not have {name}")
+    else:
+        player1.drop_item(i)
+        player1.current_room.add_item(i)
+
 
 # Write a loop that:
 #
@@ -72,7 +121,24 @@ def move(player, direction):
 
 
 while True:
-    cmd = input("Input a direction [n,s,e,w] or [q] to quit")
-    if cmd=='q':
-        break
-    move(player1,cmd)
+    cmd = input("Input a direction [n,s,e,w] or [q] to quit: ")
+    sentence = cmd.split()
+
+    if len(sentence)==1:
+        if cmd=='q':
+            break
+        elif cmd=="i":
+            player1.show_item()
+        if cmd in valid_move:
+            move(player1,cmd)
+        #player1.current_room.show_item()
+
+
+    elif len(sentence)==2:
+        if sentence[0]== "get":
+            get_item_function(sentence[1])
+        if sentence[0]== "drop":
+            drop_item_function(sentence[1])
+    #    player1.action(sentence[0], sentence[1])
+
+    player1.current_room.show_item()
--- src/room.py
+++ src/room.py
@@ -10,4 +10,16 @@ class Room:
         self.s_to = None
         self.w_to = None
         self.n_to = None
+        self.item_list=[]
 
+    def add_item(self,item):
+        self.item_list.append(item)
+
+    def remove_item(self,item):
+        self.item_list.remove(item)
+
+
+    def show_item(self):
+        print(f'This room has {len(self.item_list)} items:')
+        for item in self.item_list:
+            print(item.name)
