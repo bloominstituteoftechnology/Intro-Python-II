293017da98cd967042039936d155c076b94d3f13
Tristan Grovender <therealtristangrovender@gmail.com>
2019-08-08 17:37:34 -0500

    Day 1 MVP complete

--- src/player.py
+++ src/player.py
@@ -2,3 +2,11 @@
 # currently.
 
 # YOUR CODE GOES HERE!
+
+class Player():
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+
+    def __str__(self):
+        return f"Hello {self.name} you are in room {self.current_room.name}."
