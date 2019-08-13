7efb48232b196acbceee4363cd8e095f1595ed3f
iradsanchez <ira.d.sanchez@gmail.com>
2019-08-13 08:05:50 -0700

    Add Player and Room Classes and Create New Player Outside: >  Created classes for Player and Room in player.py and room.py. Then, I called Player() in adv.py, passing in outside. When the current_player is printed, it displays a string stating the player's room. I also added a while loop in adv.py that updates the user_input variable or quits the loop.

--- src/adv.py
+++ src/adv.py
@@ -1,26 +1,31 @@
+#from file import function
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'outside':
+    Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    'foyer':
+    Room(
+        "Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook':
+    Room(
+        "Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':
+    Room(
+        "Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure':
+    Room(
+        "Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
-
 
 # Link rooms together
 
@@ -39,6 +44,10 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+current_player = Player("outside")
+print(current_player)
+
+user_input = input("Pick a cardinal direction -- n, s, e, w. Press q to quit.")
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +58,18 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while user_input != "q":
+    if user_input == "n":
+        print(user_input)
+
+    elif user_input == "s":
+        print(user_input)
+    elif user_input == "e":
+        print(user_input)
+    elif user_input == "w":
+        print(user_input)
+    else:
+        print(
+            "Please choose a valid option: n, s, w, or e to move or q to quit."
+        )
--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, room="outside"):
+        self.room = room
+
+    def __str__(self):
+        return 'I am in {self.room}'.format(self=self)
