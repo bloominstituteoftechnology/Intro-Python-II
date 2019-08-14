cae338e0214e6e3b582a40c868ca0e0ae3807fa8
Ashley Owens <ash4741@gmail.com>
2019-08-14 00:06:21 -0400

    Action Input Method

--- src/player.py
+++ src/player.py
@@ -5,3 +5,22 @@ class Player:
 
   def __str__(self):
     return f"{self.name}, {self.current_room}"
+
+  def action_input(self, decision):
+    if decision == 'n':
+      if self.current_room.n_to is not None:
+          self.current_room = self.current_room.n_to
+    elif decision == 's':
+      if self.current_room.s_to is not None:
+        self.current_room = self.current_room.s_to
+    elif decision == 'e':
+      if self.current_room.e_to is not None:
+        self.current_room = self.current_room.e_to
+    elif decision == 'w':
+      if self.current_room.w_to is not None:
+        self.current_room = self.current_room.w_to
+    else:
+      print("Thanks for playing")
+
+  def display_room(self):
+    print(f'Current Room: {self.current_room.name}\n \n{self.current_room.description}\n\n')
