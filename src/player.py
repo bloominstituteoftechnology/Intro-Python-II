8554de42deb7d01ef23770ac87a0d26111691609
Arpita Sinha <sinha.arpita@gmail.com>
2019-08-15 21:50:12 -0700

    MVP done

--- src/player.py
+++ src/player.py
@@ -4,8 +4,20 @@ class Player:
     def __init__(self,name,current_room):
         self.name=name
         self.current_room = current_room
+        self.items=[]
 
+
+    def get_items(self):
+         return self.items
+
+
+    def add_item(self,item):
+        return self.items.append(item)
+
+
+    def delete_item(self,item):
+         return self.items.remove(item)
 
 
     def __repr__(self):
-        return "Name : " + self.name + "\n Current Room :  " + str(self.current_room)
+        return "Name : " + self.name + "\n Current Room :  " + str(self.current_room)+ "\n Your  Items: " + str(self.items)
