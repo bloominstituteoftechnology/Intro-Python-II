3a251a28471974bc612e39c52e3d6f8f2ff0869e
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-08-16 21:18:20 -0400

    2day mvp

--- src/player.py
+++ src/player.py
@@ -1,19 +1,23 @@
-# Write a class to hold player information, e.g. what room they are in
-# currently.
-from room import Room
 
-class Player():
+class Player:
+
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.items = []
 
-    # output = self.name + " is in room " + self.current_room
-    # print(output)
+    def get_items(self):
+        return self.items
 
-    def __str__(self):
-    #     output = ""
-        return self.name + " is in room " + self.current_room
-        # return  output
+    def add_item(self, item):
+        return self.items.append(item)
+
+    def remove_item(self, item):
+        return self.items.remove(item)
+
+    def __repr__(self):
+        return "Name : " + self.name + "\n Current Room :  " + str(self.current_room) + "\n Your  Items: " + str(
+            self.items)
 
 
 
--- src/room.py
+++ src/room.py
@@ -2,18 +2,28 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description, n_to = None, s_to = None, e_to = None, w_to = None):
+    def __init__(self, name, description, n_to=None, s_to=None, e_to=None, w_to=None, items=None):
         self.name = name
         self.description = description
         self.n_to = n_to
         self.s_to = s_to
         self.e_to = e_to
         self.w_to = w_to
+        self.items = []
 
-    # def set_to(self, new_to):
-    #     self._
-    # def __str__(self):
-    #     # output =
-    #     return self.name + " is in room " + self.description
+    def get_items(self):
+        return self.items
+
+    def add_item(self, item):
+        return self.items.append(item)
+
+    def delete_item(self, item):
+        return self.items.remove(item)
+
+    def __repr__(self):
+        return "Name : " + self.name + " \n Description :" + self.description + "\n Items in the room : " + str(
+            self.items)
+
+
 
 
