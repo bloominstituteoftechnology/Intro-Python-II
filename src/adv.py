bca0eefc07fb0f06feea65d1a0305282d20c9923
omar okla <omaro@me.com>
2019-09-04 18:54:46 -0400

    init commmmmmmmmit. testing

--- src/adv.py
+++ src/adv.py
@@ -21,6 +21,8 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
+print("test2")
+
 
 # Link rooms together
 
--- src/room.py
+++ src/room.py
@@ -1,2 +1,25 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def __str__(self):
+        return f"Name: {self.name}, Description: {self.description}"
+
+    def __repr__(self):
+        return f"{self.name}, {self.description}"
+
+
+myRoom = Room('test room', 'a room to test things')
+
+print(myRoom)
+
+print(repr(myRoom))
