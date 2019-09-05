a1a386a271c310277869a4c801cacfc443c99bff
ThaDocIzN <rodan72@msn.com>
2019-09-05 15:40:39 -0500

    player can move around

--- src/adv.py
+++ src/adv.py
@@ -1,27 +1,26 @@
-from room import Room
 
 # Declare all the rooms
+from src.room import Room
 from src.player import Player
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside': Room("Outside Cave Entrance",
+                    "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer': Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow': Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
-
 
 # Link rooms together
 
@@ -52,3 +51,38 @@ player = Player('Charles', room['outside'])
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+exitGame = False
+
+while not exitGame:
+    print(f"You are here {player.room.name}")
+    print(f"You are here {player.room.description}")
+
+    userInput = input("Where do you want to go?")
+
+    if userInput == "q":
+        print("Give me a sec..Done")
+        exitGame = True
+        break
+    elif userInput == "n":
+        if player.room.n_to is not None:
+            player.room = player.room.n_to
+        else:
+            print("No room here")
+    elif userInput == "e":
+        if player.room.e_to is not None:
+            player.room = player.room.e_to
+        else:
+            print("No room here")
+    elif userInput == "w":
+        if player.room.w_to is not None:
+            player.room = player.room.w_to
+        else:
+            print("No room here")
+    elif userInput == "s":
+        if player.room.s_to is not None:
+            player.room = player.room.s_to
+        else:
+            print("No room here")
+    else:
+        print("Not allowed")
