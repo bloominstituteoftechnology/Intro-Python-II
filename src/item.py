6103e3c960ec4e6a9f4f432f4a91c570fb1bc658
ThaDocIzN <rodan72@msn.com>
2019-09-05 13:47:37 -0500

    added item class

---
+++ src/item.py
@@ -0,0 +1,8 @@
+# Create a file called item.py and add an Item class in there.
+
+
+class Item:
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
