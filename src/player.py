88ae6ea63e35fc3e43ba456b0a921ef157a983e3
ThaDocIzN <rodan72@msn.com>
2019-09-05 16:46:52 -0500

    Add capability to add Items to the player's inventory

--- src/player.py
+++ src/player.py
@@ -7,3 +7,11 @@ class Player:
     def __init__(self, name, room):
         self.name = name
         self.room = room
+        self.items = []
+
+    def print_items(self):
+        if not self.items:
+            pass
+        else:
+            for i in self.items:
+                print(i.name)
