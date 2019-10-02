d0cbea180025b3a817cf16c4da2b303d5ef5bfec
Quinn Dougherty <quinn.dougherty92@gmail.com>
2019-10-02 11:43:09 -0400

    everything but the main loop

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
-
+from player import Player
+import sys
 # Declare all the rooms
 
 room = {
@@ -36,16 +37,17 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
+if __name__=='__main__':
 
-# Make a new player object that is currently in the 'outside' room.
-
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+    # Make a new player object that is currently in the 'outside' room.
+    player = Player("Yoneda", room['outside'])
+    # Write a loop that:
+    #
+    # * Prints the current room name
+    # * Prints the current description (the textwrap module might be useful here).
+    # * Waits for user input and decides what to do.
+    #
+    # If the user enters a cardinal direction, attempt to move to the room there.
+    # Print an error message if the movement isn't allowed.
+    #
+    # If the user enters "q", quit the game.
--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from typing import Optional
+from room import Room
+from dataclasses import dataclass
+
+@dataclass
+class Player:
+    name: str
+    current_room: Room
