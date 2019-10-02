62983b761190a34f819a2638cd913c2f1494b9b6
Mitchell Robles <mitchellr92@live.com>
2019-10-02 15:51:30 -0700

    Created room and player class and added directional functionality

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -21,7 +22,6 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
-
 # Link rooms together
 
 room['outside'].n_to = room['foyer']
@@ -39,11 +39,34 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+john = Player('John', room['outside'])
+
 # Write a loop that:
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
+
+while True:
+
+    print(john.current_room.name)
+    print(john.current_room.description)
+    myInput = input(f"Which direction do you want to go?: ")
+    if (myInput == 'n'):
+        john.current_room = john.current_room.n_to       
+    elif (myInput == 'e'):
+        john.current_room = john.current_room.e_to
+    elif (myInput == 's'):
+        john.current_room = john.current_room.s_to
+    elif (myInput == 'w'):
+        john.current_room = john.current_room.w_to
+    elif(myInput == 'q'):
+        print(f"Thanks for playing!")
+        break
+    else:
+        print("You can't go there!")
+    
+
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
