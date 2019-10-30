1add2ca0cb8c3250825f4a563b1e446d1f07541a
mmastin <matthew.mastin@gmail.com>
2019-10-30 18:48:21 -0400

    initial commit

--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player(self, room):
+    def __init__(self):
+        self.room = room
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
--- src/room.py
+++ src/room.py
@@ -1,2 +1,14 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room(object):
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+        
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
