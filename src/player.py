df8a59eb565c759df6db03f061b3fb267b6c1dc0
Nick-Ballenger <nickster214@gmail.com>
2019-12-04 10:27:52 -0700

    starting player stuffs

--- src/player.py
+++ src/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, startRoom):
+        self.currentRoom = startRoom
+        self.inventory = []
+
+    def add_item(self, item):
+        self.inventory.append(item)
