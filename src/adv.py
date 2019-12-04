8090b735f9543ded97705505344357168321b347
Suparada Saitalae <ann.suparada@gmail.com>
2019-12-04 15:02:55 -0600

    create Room

--- src/adv.py
+++ src/adv.py
@@ -32,7 +32,7 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
-
+print(room['outside'].n_to)
 #
 # Main
 #
