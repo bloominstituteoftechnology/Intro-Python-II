4c21ad6efb8cbf76891c98272f64cd9443ddec81
Eric Wuerfel <Eric@Erics-MacBook-Pro-5.local>
2019-12-04 16:40:58 -0500

    MVP for wednesday

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,7 @@
+import sys
+
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -23,7 +26,6 @@ earlier adventurers. The only exit is to the south."""),
 
 
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -33,9 +35,85 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+player = Player(room['outside'])
+
+# Functions
+def move():
+    # Get user input
+    user_move = input("Which direction? (N, E, S, W):  ")
+    user_move = user_move.lower()
+
+    # Check if move is a valid direction
+    valid_dirs = ["n", "e", "s", "w", "q"]
+    if user_move not in valid_dirs:
+        print("Sorry, that's not a valid direction. Please enter: 'N', 'E', 'S' or 'W'")
+        move()
+    else:
+        # Quit if "q"
+        if user_move=="q":
+            print("Bye!")
+            sys.exit(0)
+        
+        global player
+        # Check if there's a room in each direction
+
+        # North
+        try:
+            if user_move=="n":
+                if player.room.n_to == "nothing":
+                    print("Sorry, there's nothing there.")
+                    move()
+                else:
+                    new_location = player.room.n_to
+                    player = Player(new_location)
+                    print(player.room)
+                    move()
+
+            # South
+            if user_move=="s":
+                if player.room.s_to == "nothing":
+                    print("Sorry, there's nothing there.")
+                    move()
+                else:
+                    new_location = player.room.s_to
+                    player = Player(new_location)
+                    print(player.room)
+                    move()
+
+            # East
+            if user_move=="e":
+                if player.room.e_to == "nothing":
+                    print("Sorry, there's nothing there.")
+                    move()
+                else:
+                    new_location = player.room.e_to
+                    player = Player(new_location)
+                    print(player.room)
+                    move()
+
+            # West
+            if user_move=="w":
+                if player.room.w_to == "nothing":
+                    print("Sorry, there's nothing there.")
+                    move()
+                else:
+                    new_location = player.room.w_to
+                    player = Player(new_location)
+                    print(player.room)
+                    move()
+        except AttributeError:
+            print("Sorry, there's nothing there.")
+            move()
+
+    
+
 #
 # Main
 #
+
+def Main():
+    print(player.room)
+    move()
 
 # Make a new player object that is currently in the 'outside' room.
 
@@ -49,3 +127,5 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+Main()
--- src/room.py
+++ src/room.py
@@ -1,2 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description, n_to="nothing", e_to="nothing", s_to="nothing", w_to="nothing"):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f"{self.description}"
