22665fcacec88e35b4ade8317adea5c1fef98e68
Alberto-Funk <laptopreneur@gmail.com>
2019-12-05 09:51:05 -0800

    Day 2 MVP complete - Room methods: add, remove, view - player methods: add, remove, view, take, drop - Item class - split cmd for 2 possible cmds - error handling WIP

--- src/adv.py
+++ src/adv.py
@@ -1,7 +1,14 @@
 from room import Room
 from player import Player
+from item import Item
+
 
 # Declare all the rooms
+item = {
+    'light': Item("light"),
+    'enos_key': Item("enos_key"),
+    'chamber_key': Item("chamber_key")
+}
 
 room = {
     'outside':  Room("Outside Cave Entrance",
@@ -24,20 +31,38 @@ earlier adventurers. The only exit is to the south."""),
 
 
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
+
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
 room['foyer'].e_to = room['narrow']
+
 room['overlook'].s_to = room['foyer']
+
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
+
 room['treasure'].s_to = room['narrow']
 
 
-# Make a new player object that is currently in the 'outside' room.
+# Add items to rooms
+# for r in room:
+#     print(room[r].name)
+room['overlook'].items.append(item['light'])
+room['narrow'].items.append(item['chamber_key'])
 
-player = Player(room['outside'])
+
+# Set dark room
+room['narrow'].is_dark = True
+
+
+# Set locked room
+room['treasure'].is_locked = True
+
+
+# Make a new player object that is currently in the 'outside' room.
+name = input("Enter Name ->")
+player = Player(name, room['outside'])
 
 
 #
@@ -55,22 +80,54 @@ player = Player(room['outside'])
 #
 # If the user enters "q", quit the game.
 
-print(room["foyer"].n_to.name)
-print(player.curr_room)
+print("ROOM NAME", room["foyer"].n_to.name)
+print("PLAYER CURR ROOM", player.curr_room)
 
 while True:
     print("CURRENT LOCATION NAME", player.curr_room.name)
     print("CURRENT LOCATION DESC", player.curr_room.desc)
     cmd = input(" -> ")
-    if cmd == "q":
+    split_cmd = cmd.split(" ")
+    if split_cmd[0] == "q":
         break
-    elif cmd == "n":
+    elif split_cmd[0] == "n":
         player.move_n()
-    elif cmd == "s":
+    elif split_cmd[0] == "s":
         player.move_s()
-    elif cmd == "w":
+    elif split_cmd[0] == "w":
         player.move_w()
-    elif cmd == "e":
+    elif split_cmd[0] == "e":
         player.move_e()
+
+    elif split_cmd[0] == "view":
+        if len(split_cmd) == 2:
+            if split_cmd[1] == "items":
+                player.curr_room.view_items()
+            elif split_cmd[1] == "inventory":
+                player.view_inventory()
+            else:
+                print("Incorrect command")
+        else:
+            print("Missing command")
+
+    elif split_cmd[0] == "take":
+        if len(split_cmd) == 2:
+            player.take_item(split_cmd[1], player.curr_room)
+        else:
+            print("Missing command")
+
+    elif split_cmd[0] == "drop":
+        if len(split_cmd) == 2:
+            player.drop_item(split_cmd[1], player.curr_room)
+        else:
+            print("Missing command")
     else:
-        print("incorrect command")
+        print("incorrect commands")
+
+
+
+# data is everywhere
+# need to refactor, better handling of conditionals
+  # dark room, items in room, items in inventory, adding/removing items
+
+  
