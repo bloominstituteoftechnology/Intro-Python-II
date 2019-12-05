88dc06807d38cefd92ab7029f3cb87537e305894
Nathan Van Wyck <nathanvanwyck@Macintosh-00254bbd8f8a-2.local>
2019-12-05 18:35:04 -0500

    Completed Day 2 MVP

--- src/player.py
+++ src/player.py
@@ -3,6 +3,7 @@
 
 
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, items):
         self.name = name
         self.current_room = current_room
+        self.items = items
