e0cf8c108475107f075f0722f6f955b3ad37cb28
jose restrepo <arturo.restrepo@gmail.com>
2019-12-05 10:20:58 -0500

    Game adventure w REPL

--- src/player.py
+++ src/player.py
@@ -1,2 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+
+    def __init__(self, current_room):
+        self.current_room = current_room
+
+    def get_current_room(self):
+        return self.current_room
+
+    def set_current_room(self, room):
+        self.current_room = room
+
+    def __str__(self):
+        return f"Current Room: {self.current_room}"
