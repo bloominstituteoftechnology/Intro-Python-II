3e5619521d146e45a56f79981983f8b423ba9a07
Annique Nsabimana <anniquensabimana@gmail.com>
2019-12-04 20:35:14 -0600

    worked on classes

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+# Attributes
+# name
+# current_room
+
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
--- src/room.py
+++ src/room.py
@@ -1,2 +1,20 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+# which point to the room in that respective direction.
+
+# Attributes
+# name
+# description
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def __str__(self):
+        return f"\nname: {self.name}\ndescription: {self.description}\n"
