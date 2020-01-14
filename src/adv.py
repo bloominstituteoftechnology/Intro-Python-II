33f9d005e41da00b47590d7502c76a659c9d67a1
Brandon_Pampuch <brandonpampuch@gmail.com>
2020-01-14 10:44:57 -0600

    basic movement function

--- src/adv.py
+++ src/adv.py
@@ -1,29 +1,49 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons",
+                     'nothing',
+                     'nothing',
+                     'nothing',
+                     'nothing'
+                     ),
+
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""",
+                     'nothing',
+                     'nothing',
+                     'nothing',
+                     'nothing'
+                     ),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""",
+                     'nothing',
+                     'nothing',
+                     'nothing',
+                     'nothing'
+                     ),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", 'nothing',
+                     'nothing',
+                     'nothing',
+                     'nothing'
+                     ),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", 'nothing',
+                     'nothing',
+                     'nothing',
+                     'nothing'
+                     ),
 }
-
-
-# Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -33,19 +53,29 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
 # Main
-#
+current_player = Player("Brandon", "outside")
+current_room_in = current_player.current_room
 
-# Make a new player object that is currently in the 'outside' room.
+print("Welcome to your adventure!")
+print(room[current_room_in].name)
+print(room[current_room_in].description)
+msg = str(input(
+    "what direction will you go? Please press n for north s for south w for west or e for east or q for quit.\n"))
 
+current_player = Player("Brandon", "outside")
+current_room_in = ""
 # Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+current_player.current_room_in = current_player.current_room
+
+
+while not msg == "q":
+    # * Prints the current description
+    if msg == "n":
+        current_room_in = room[current_player.current_room].n_to.name
+        current_player.current_room = room[current_player.current_room].n_to.name.lower(
+        )
+        print(current_room_in)
+    # Print an error message if the movement isn't allowed
+    msg = str(input(
+        "what direction will you go? Please press n for north s for south w for west or e for east or q for quit.\n"))
--- src/room.py
+++ src/room.py
@@ -1,2 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init__(self, name, description, n_to, s_to, e_to, w_to):
+        self.description = description
+        self.name = name
+        self.n_to = n_to
+        self.s_to = s_to
+        self.w_to = w_to
+        self.e_to = e_to
