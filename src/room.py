e2e675dad64c5cb485fd24f98776f33a1f5ad323
Jason Sonnichsen <Harliquinn@gmail.com>
2020-01-15 15:22:56 -0500

    Room and Player classes created. Set player starting room.

--- src/room.py
+++ src/room.py
@@ -1,2 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    """Class to define rooms in the game"""
+
+    def __init__(self, name, description):
+        self.name = name
+        self.name = description
