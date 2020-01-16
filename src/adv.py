b838d3cf16ff9f65c0a5e7b09e10873c3b954011
Tyler <tylerwesleyquinn@gmail.com>
2020-01-16 11:41:16 -0500

    classes foundation

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,6 +39,9 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+
+new_player = Player('Billy', room['outside'])
+
 
 # Write a loop that:
 #
