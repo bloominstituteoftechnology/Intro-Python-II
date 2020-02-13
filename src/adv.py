6fa18cd77e739b8ff0c16a2e61bd1110ccdf3028
Gatrrr <davidgatrmacaulay@gmail.com>
2020-02-12 18:51:00 -0800

    Day 1 + Day 2 MVP Reached

--- src/adv.py
+++ src/adv.py
@@ -16,48 +16,28 @@ item = [
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons",
-                     None,
-                     None,
-                     None,
-                     None,
                      [item[0],
                       item[1]
                       ]),
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east.""",
-                     None,
-                     None,
-                     None,
-                     None,
                     [item[2],
                      item[3]
                      ]),
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm.""",
-                     None,
-                     None,
-                     None,
-                     None,
                      [item[3],
                       item[0]
                       ]),
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air.""",
-                     None,
-                     None,
-                     None,
-                     None,
                      [item[1],
                       item[3]
                       ]),
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south.""",
-                     None,
-                     None,
-                     None,
-                     None,
                      [item[1],
                       item[2]
                       ]),
@@ -88,11 +68,14 @@ room['treasure'].s_to = room['narrow']
 #
 # If the user enters "q", quit the game.
 
-player = Player("Not Steve", room ['outside'])
-
 def show_welcome_message():
     welcome_message = "Welcome to the game!"
     print(welcome_message)
+
+show_welcome_message()
+
+player = Player("Not Steve", room['outside'], [item[0]])
+print(f"Your player: {player.player_name}, currently located at: {player.current_room.name}, Inside your inventory currently is: {player.inventory[0].item_name}")
 
 def get_user_choice():
     choice = input("[n] north [s] south [e] east [w] west [q] quit\n")
@@ -103,27 +86,27 @@ choice_options = {
     "s": "south",
     "e": "east",
     "w": "west",
-    "q": "quit"
+    "q": "quit",
+    "get": "get item",
+    "drop": "drop item",
+    "i": "inventory",
+    "inventory": "inventory"
 }
-show_welcome_message()
+
 
 while True:
     current_room = player.current_room
     print(f"You are currently in {current_room.name}")
     print(f"{current_room.description}")
-    move = input("Select N, S, E, or W >>> ")
+    move = input("Select N, S, E, or W to move to the next room!\nType search to search a room!\nType get then the item name to get the item\nType drop then the item name to drop the item >>> ")
     if move == "n":
         if current_room.n_to is not None:
             player.current_room = current_room.n_to
-            print(f"You picked up {current_room.items[0]}")
-            print(f"You picked up {current_room.items[1]}")
         else:
             print("You hit a dead end!  Try again.")
     elif move == "s":
         if current_room.s_to is not None:
             player.current_room = current_room.s_to
-            print(f"You picked up {current_room.items[0]}")
-            print(f"You picked up {current_room.items[1]}")
         else:
             print("You hit a dead end!  Try again.")
     elif move == "e":
@@ -136,6 +119,42 @@ while True:
             player.current_room = current_room.w_to
         else:
             print("You hit a dead end!  Try again.")
+
+    elif "get" in move:
+        item = move[4:]
+
+        for x in range(len(current_room.items)):
+            if item == current_room.items[x].item_name:
+                player.inventory.append(current_room.items[x])
+                print(f"You have picked up {current_room.items[x]}")
+                del current_room.items[x]
+                break
+            else:
+                print("Item not in room")
+
+        if len(current_room.items) == 0:
+            print("Item not in the room")
+
+    elif "drop" in move:
+        item = move[5:]
+
+        for x in range(len(player.inventory)):
+            if item == player.inventory[x].item_name:
+                current_room.items.append(player.inventory[x])
+                print(f"You have dropped {player.inventory[x]}")
+                del player.inventoryp[x]
+                break
+            else:
+                print("Don't have that item to drop!")
+
+    elif move == "i" or move == "inventory":
+        for x in range(len(player.inventory)):
+            print(player.inventory[x])
+
+    elif move == "search":
+        for x in range(len(current_room.items)):
+            print(f"{current_room.items}")
+
     elif move == "q":
         print("Game has quit")
         exit()
--- src/room.py
+++ src/room.py
@@ -1,5 +1,5 @@
 class Room:
-    def __init__(self, name, description, n_to, s_to, e_to, w_to, items):
+    def __init__(self, name, description, items, n_to = None, s_to = None, e_to = None, w_to = None, ):
         self.name = name
         self.description = description
         self.n_to = n_to
