07e84cfdd84608821d142d0751d579c4122b8362
sweyome <sweyome1@gmail.com>
2020-02-18 13:34:37 -0500

    class for room

--- src/room.py
+++ src/room.py
@@ -1,2 +1,20 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room():
+    ''' class for room'''
+    def __init__(self, room, description):
+        self.room = room 
+        self.description = description
+
+class Direction(Room):
+    def __init__(self, n_to, s_to, e_to, w_to, room, description):
+        super().__init__(room, description)
+        self.n_to = n_to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
+
+    def __str__(self):
+        return f'{self.n_to} put you in the {self.room}'
+        
