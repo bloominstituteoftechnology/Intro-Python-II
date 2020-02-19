e5b8a89a0c2ec09e8e173813957199c251e1fbf6
sweyome <sweyome1@gmail.com>
2020-02-18 19:51:09 -0500

    item and player class done

---
+++ src/item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f'Item Name: {self.name}\nItem Description: {self.description}'
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, new_name):
+        self.name = new_name
--- src/room.py
+++ src/room.py
@@ -1,20 +1,42 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
-
-class Room():
-    ''' class for room'''
-    def __init__(self, room, description):
-        self.room = room 
+class Room:
+    def __init__(self, name, description):
+        self.name = name
         self.description = description
-
-class Direction(Room):
-    def __init__(self, n_to, s_to, e_to, w_to, room, description):
-        super().__init__(room, description)
-        self.n_to = n_to
-        self.s_to = s_to
-        self.e_to = e_to
-        self.w_to = w_to
-
-    def __str__(self):
-        return f'{self.n_to} put you in the {self.room}'
+        self.n_to = None
+        self.s_to = None
+        self.w_to = None
+        self.e_to = None
+        self.players = []
+        self.items = []
         
+    def __str__(self):
+        display_str = ""
+        display_str += f"\n{self.name}\n"
+        display_str += f"\n{self.description}\n"
+        display_str += f"\n{self.get_exits_string()}\n"
+        return display_str
+        
+    def get_room_in_direction(self, direction):
+        if hasattr(self, f"{direction}_to"):
+            return getattr(self, f"{direction}_to")
+        return None
+        
+    def get_exits(self):
+        exits = []
+        if self.n_to:
+            exits.append("n")
+        if self.s_to:
+            exits.append("s")
+        if self.w_to:
+            exits.append("w")
+        if self.e_to:
+            exits.append("e")
+        return exits
+    
+    def get_exits_string(self):
+        return f"Exits: {', '.join(self.get_exits())}"
+    
+    def get_room_items(self):
+        return f"Items {', '.join(self.items)}"
