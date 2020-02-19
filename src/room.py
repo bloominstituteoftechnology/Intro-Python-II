372cffd61dd6b38fd39f72ea66dc3c57bf6307fa
heuzin <matheusqs.arq@gmail.com>
2020-02-18 19:45:23 -0500

    Created loop for rooms directions

--- src/room.py
+++ src/room.py
@@ -1,2 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, direction=[]):
+        self.name = name
+        self.direction = direction
+
+    def __str__(self):
+        # print out something different if len(items)
+
+        output = f'{self.name} has: ' 
+        for i in self.direction:
+            output += f'\n - {i}'
+
+        return output
