821dd267da7a01bef6001840ab8f4b1a61d7f04b
Elizabeth Ter Sahakyan <liztersahakyan@gmail.com>
2020-03-11 16:04:15 -0700

    started writing room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,18 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room():
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def n_to():
+    
+
+    def s_to():
+
+    
+    def e_to():
+
+    
+    def w_to():
