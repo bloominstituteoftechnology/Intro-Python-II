e70ac3162da2e9ca15c13d585e866ab4f173bb31
Nckflannery <Nckflannery@users.noreply.github.com>
2020-03-11 16:00:08 -0700

    Remove items

--- src/items.py
+++ src/items.py
@@ -25,10 +25,10 @@ class Basic(Item):
         super().__init__(name=name, description=description, value=value)
         self.useful = useful
 
-dagger = Weapon('Stabby', 'A short dagger.', 5, 2)
-sword = Weapon('Slash', 'A large sword.', 12, 4)
-needle = Weapon('Pokey', 'A long, needle-like sword. (stick them with the pointy end)', 25, 9)
-candle = Basic('Candle', 'Just a candle', 1, False)
-shoes = Basic('Shoes', 'Pair of Jordans. What are those doing here?', 20, True)
-book = Book('Book', 'An old book, most of the pages are faded', 3, 'Unknown')
-great_book = Book('Cryptonomicon', 'A fantastic book, seriously read it', 20, 'Neal Stephenson')
+# dagger = Weapon('Stabby', 'A short dagger.', 5, 2)
+# sword = Weapon('Slash', 'A large sword.', 12, 4)
+# needle = Weapon('Pokey', 'A long, needle-like sword. (stick them with the pointy end)', 25, 9)
+# candle = Basic('Candle', 'Just a candle', 1, False)
+# shoes = Basic('Shoes', 'Pair of Jordans. What are those doing here?', 20, True)
+# book = Book('Book', 'An old book, most of the pages are faded', 3, 'Unknown')
+# great_book = Book('Cryptonomicon', 'A fantastic book, seriously read it', 20, 'Neal Stephenson')
