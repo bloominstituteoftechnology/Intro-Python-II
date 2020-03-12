c3a6cf5cbc92dd4d44bd4d685b81b13876dd1528
Ale <alearanibar@gmail.com>
2020-03-12 16:29:13 -0700

    work in progress

--- src/player.py
+++ src/player.py
@@ -1,2 +1,29 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, current_room, inventory=[]):
+        self.name = name
+        self.current_room = current_room
+        self.inventory = inventory
+    def __str__(self):
+        return (f"{self.name} is in the {self.current_room}.")
+
+    def travel(self, direction):
+        if hasattr(self.current_room, f'{direction}_to'):
+            current_room =getattr(self.current_room, f'{direction}_to')
+            self.current_room = current_room
+            print(f"You have entered the {current_room}.\n")
+            print(current_room.description + "\n")
+        else:
+            print("Wrong way!\n")
+    def add_item(self, item):
+        self.inventory.append(item)
+        print(f'{item} is yours now!')
+    def item_inventory(self):
+        if len(self.inventory) == 0:
+            print('You have no items in your inventory')
+        else:
+            print("Your Inventory")
+            for item in self.inventory:
+                print(f'{item.name} {item.description} ')
--- src/room.py
+++ src/room.py
@@ -1,2 +1,33 @@
-# Implement a class to hold room information. This should have name and
+# Implement a class to hold room information. This should have title and
 # description attributes.
+from player import Player
+
+class Room:
+    """
+    room class
+    """
+    def __init__(self, name, description, items=[]):
+        self.name = name
+        self.description = description
+        self.items = []
+        if len(self.items) > 0:
+            for item in self.items:
+                self.items.append(item)
+        else:
+            self.items = items
+
+    def __str__(self):
+        return f' {self.name}\n\n{self.description}'
+
+    def room_inventory(self):
+        if self.items == []:
+            print('no items here')
+        elif type(self.items) is list :
+            for item in self.items:
+                print(f'there is a {item.name} ')
+        else:
+            print(f'here there is a {self.items.name} ')
+
+    def take_item(self, item):
+        self.items.remove(item)
+        Player.add_item(item)
