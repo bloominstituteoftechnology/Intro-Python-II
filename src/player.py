3357df25212d697a4992c65bf5ec7c3f7f9c9817
nijanes97 <nickjanes97@gmail.com>
2020-03-12 17:02:56 -0700

    finished player, item, and room

--- src/player.py
+++ src/player.py
@@ -1,2 +1,28 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, current_room, inventory):
+        self.name = name
+        self.current_room = current_room
+        self.inventory = inventory
+    def travel(self, direction):
+        if getattr(self.current_room, f"{direction}_to") is not None:
+            self.current_room = getattr(self.current_room, f"{direction}_to")
+            print(self.current_room)
+        else:
+            print("There is no room there.")
+    def drop_item(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            print(f"{item} dropped.")
+            return True
+        else:
+            print("You don't have that item.")
+            return False
+    def pickup_item(self, item):
+        self.inventory.append(item)
+        print(f"{item} added to inventory.")
+    def show_inventory(self):
+        for i in self.inventory:
+            print(i.name)
--- src/room.py
+++ src/room.py
@@ -1,2 +1,41 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description, items):
+        self.name = name
+        self.description = description
+        self.items = items
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+    def __str__(self):
+        return_str = "\n--------\n"
+        return_str += self.name
+        return_str += "\n"
+        return_str += self.description
+        return_str += "\n"
+        return_str += ', '.join(self.items)
+        return_str += "\n--------\n"
+        return return_str
+    def get_exits_string(self):
+        exits = []
+        if(self.n_to is not None):
+            exits.append("n")
+        if(self.s_to is not None):
+            exits.append("s")
+        if(self.w_to is not None):
+            exits.append("w")
+        if(self.e_to is not None):
+            exits.append("e")
+        return exits
+    def add_item(self, item):
+        self.items.append(item)
+    def remove_item(self, item):
+        if self.items in item:
+            self.items.remove(item)
+            return True
+        else:
+            print(f"{item} is not in room.")
+            return False
