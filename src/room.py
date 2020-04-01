b27a745c018718376bc219b9bc7543599569f532
Dan Morse <djmthedev@gmail.com>
2020-03-31 19:33:09 -0700

    added items to Room

--- src/room.py
+++ src/room.py
@@ -2,13 +2,19 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description, n_to=None, s_to=None, e_to=None, w_to=None):
+    def __init__(self, name, description, items=[], n_to=None, s_to=None, e_to=None, w_to=None):
         self.name = name
         self.description = description
+        self.items = items
         self.n_to = n_to
         self.s_to = s_to
         self.e_to = e_to
         self.w_to = w_to
 
     def __str__(self):
-        return f"You are in the {self.name} room. {self.description}"
+        output = f"You are in the {self.name} room. {self.description} "
+        if self.items != []:
+            output += "You see the following items:"
+            for item in self.items:
+                output += f"\n\t-{item}"
+        return output
