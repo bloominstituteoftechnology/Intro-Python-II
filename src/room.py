96a7a36bf8ef36125da984e81d9b61be3855ca97
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-08 17:31:02 -0400

    Room: write __init__, __str__, __repr__

--- src/room.py
+++ src/room.py
@@ -4,4 +4,32 @@
 
 
 class Room:
-    pass
+
+    def __init__(self, name, description):
+
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+
+        class_name = self.__class__.__name__
+        name = str(self.name)
+        description = str(self.description)
+
+        return ('\n'.join((
+            f'{class_name} {{',
+            f'    name: {name}',
+            f'    description: {description}',
+            f'}}',
+        )))
+        # -- I really don't like the hanging non-indentation in multi-line strings...
+
+    def __repr__(self):
+
+        class_name = self.__class__.__name__
+        name = repr(self.name)
+        description = repr(self.description)
+
+        return(
+            f'{class_name}({name}, {description})'
+        )
