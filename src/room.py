4596fe07c45a5faa21514f2adee71a147f782900
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-09 12:30:04 -0400

    Room: write basic support to init adjacent rooms

--- src/room.py
+++ src/room.py
@@ -7,14 +7,29 @@ from printable import printable
 
 class Room:
 
-    def __init__(self, name, description):
+    directions = ('n', 'e', 's', 'w')
+    connections = tuple(f'{dir}_to' for dir in directions)
+
+    def __init__(self, name, description=None, **kwargs):
 
         self.name = name
         self.description = description
 
+        # parse kwargs for keys in connections
+        for move_key in self.connections:
+
+            move_value = None
+
+            if move_key in kwargs.keys():
+                move_value = kwargs[move_key]
+
+            setattr(self, move_key, move_value)
+
+        return
+
     def __str__(self):
 
-        attr_keys = ('name', 'description')
+        attr_keys = ('name', 'description', *self.connections)
 
         return printable.to_str(self, attr_keys)
 
