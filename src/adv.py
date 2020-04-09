7263280bbe91d9ece2f5449bbf95ea59d92254ab
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-04-09 16:24:09 -0400

    moving and items

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
-
+from player import Player
+from item import Item
 # Declare all the rooms
 
 room = {
@@ -33,12 +34,32 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+# Add items
+item = {
+    'torch': Item("Torch", "This is perfect to light the dark cave!"),
+    'shield': Item("Shield", "You can defend yourself with this."),
+    'sword': Item("Sword", "Legend says it wields great power."),
+    'bottle': Item("Bottle", "Great to hold things in.")
+}
+
+room['outside'].addItem(item['torch'])
+room['outside'].addItem(item['shield'])
+room['overlook'].addItem(item['sword'])
+room['treasure'].addItem(item['bottle'])
+
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+print("========================================")
+playerOne = Player('Aaron', room['outside'])
+print(playerOne.current_room)
+print("====================")
+print("Items: \n")
+for roomItem in playerOne.current_room.items:
+    print(roomItem)
+print("====================")
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +70,84 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+print("What do you want to do?")
+command = input(
+    "[n] north  [s] south  [e] east  [w] west  [q] quit \n[take item-name]  [drop item-name]\n[i] [inventory] Inventory\n\n")
+
+while command != 'q':
+    print("========================================")
+    if len(command.split()) == 2:
+
+        handleItem = command.split()
+        print(handleItem)
+
+        if handleItem[0] == 'take':
+            try:
+                selectedItem = item[handleItem[1].lower()]
+                if selectedItem in playerOne.current_room.items:
+                    playerOne.takeItem(selectedItem)
+                    playerOne.current_room.removeItem(selectedItem)
+                else:
+                    print("Invalid item")
+            except:
+                print("Invalid item")
+
+        elif handleItem[0] == 'drop':
+            try:
+                selectedItem = item[handleItem[1].lower()]
+                if selectedItem in playerOne.inventory:
+                    playerOne.current_room.addItem(selectedItem)
+                    playerOne.dropItem(selectedItem)
+                else:
+                    print("Invalid item")
+            except:
+                print("Invalid item")
+
+    elif command == 'n':
+        if hasattr(playerOne.current_room, 'n_to'):
+            print("moving north ...")
+            playerOne.current_room = playerOne.current_room.n_to
+        else:
+            print("can't move any further north")
+
+    elif command == 's':
+        if hasattr(playerOne.current_room, 's_to'):
+            print("moving south ...")
+            playerOne.current_room = playerOne.current_room.s_to
+        else:
+            print("can't move any further south")
+
+    elif command == 'e':
+        if hasattr(playerOne.current_room, 'e_to'):
+            print("moving east ...")
+            playerOne.current_room = playerOne.current_room.e_to
+        else:
+            print("can't move any further east")
+
+    elif command == 'w':
+        if hasattr(playerOne.current_room, 'w_to'):
+            print("moving west ...")
+            playerOne.current_room = playerOne.current_room.w_to
+        else:
+            print("can't move any further west")
+
+    elif command == 'i' or command == 'inventory':
+        print("Inventory: \n")
+        for inventoryItem in playerOne.inventory:
+            print(inventoryItem)
+
+    else:
+        print("invalid command")
+
+    print("========================================")
+    print(playerOne.current_room)
+    print("====================")
+    print("Items: \n")
+    for roomItem in playerOne.current_room.items:
+        print(roomItem)
+    print("====================")
+
+    print("\nWhat next?")
+    command = input(
+        "[n] north  [s] south  [e] east  [w] west  [q] quit \n[take item-name]  [drop item-name]\n[i] [inventory] Inventory\n\n")
--- src/player.py
+++ src/player.py
@@ -1,2 +1,19 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+
+    def __init__(self, name, current_room, inventory=None):
+        self.name = name
+        self.current_room = current_room
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+
+    def takeItem(self, item):
+        self.inventory.append(item)
+
+    def dropItem(self, item):
+        self.inventory.remove(item)
