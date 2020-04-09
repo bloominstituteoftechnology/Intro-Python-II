0a8342e26fd34f67d6b0d284688088c5eebdee4c
Dournbrood <tanner1220002@gmail.com>
2020-04-09 17:07:08 -0400

    MVP completion day 2

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+from item import Item
 import textwrap
 # Declare all the rooms
 
@@ -53,17 +54,77 @@ dournbrood = Player(room["outside"])
 #
 # If the user enters "q", quit the game.
 
+# Declare all the items
+
+item = {
+    "sword": Item('Sword', 'a plain-looking sword known as the Sword of Resolute Destruction "Hakai\'s Embrace". \nThis item will instantly kill any enemy it comes into contact with. No traces of the body or being will be left behind.'),
+    "shield": Item('Shield', 'a plain-looking shield known as the Shield of Universal Entropy "Hakai\'s Dissolution". \nThis item blocks all incoming damage to the bearer, regardless of its position.')
+}
+
+# Assign items to rooms
+
+room["outside"].addItem(item["sword"])
+room["outside"].addItem(item["shield"])
+
+# Create commands list
+commands = {
+    "move": [
+        "move",
+        "walk",
+        "run",
+        "climb",
+        "swim",
+        "swing",
+        "crawl",
+        "jump"
+    ],
+    "pickup": [
+        "get",
+        "take"
+    ],
+    "place": [
+        "drop"
+    ]
+}
+
+# Other global variables
+lastCommand = None
+
 while 1:
-    print(dournbrood.room.name)
-    print(dournbrood.room.description)
-    command = input()
-    if command == "q":
+    if lastCommand == None or lastCommand in commands["move"]:
+        dournbrood.room.printStats()
+
+    if lastCommand in commands["pickup"] or lastCommand in commands["place"]:
+        dournbrood.room.printInventory()
+
+    print("Your command: ", end="")
+    commandArgs = str.lower(input()).split(" ")
+
+    lastCommand = commandArgs[0]
+
+    if str(commandArgs[0]) == "q":
         quit()
-    currentRoom = dournbrood.room
 
-    if currentRoom.canMoveTo(str(command)):
-        currentRoom = currentRoom.getRoom(str(command))
+    elif str(commandArgs[0]) in commands["move"]:
+        if commandArgs.__len__() <= 1:
+            print("Please specify a cardinal direction.")
+        else:
+            for direction in commandArgs[1::1]:
+                dournbrood.moveTo(direction)
+
+    elif str(commandArgs[0]) in commands["pickup"]:
+        if commandArgs.__len__() <= 1:
+            print("Please specify item(s) to pick up!")
+        else:
+            for item in commandArgs[1::1]:
+                dournbrood.takeItem(item)
+
+    elif str(commandArgs[0]) in commands["place"]:
+        if commandArgs.__len__() <= 1:
+            print("Please specify item(s) to drop!")
+        else:
+            for item in commandArgs[1::1]:
+                dournbrood.dropItem(item)
+
     else:
-        print("Cannot move in that direction.")
-
-    dournbrood.room = currentRoom
+        print(f"Command '{commandArgs[0]}' not found!")
--- src/room.py
+++ src/room.py
@@ -3,9 +3,16 @@
 
 
 class Room():
-    def __init__(self, name, description):
+    def __init__(self, name="default name", description="default description", n_to=None, e_to=None, s_to=None, w_to=None, inventory=None):
         self.name = name
         self.description = description
+        self.n_to = n_to
+        self.e_to = e_to
+        self.s_to = s_to
+        self.w_to = w_to
+        self.inventory = inventory
+        if self.inventory == None:
+            self.inventory = []
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
@@ -13,15 +20,22 @@ class Room():
     def __repr__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
 
-    def canMoveTo(self, direction):
-        dirStr = f"{direction}_to"
-        for key in self.__dict__.keys():
-            if dirStr == key:
-                return True
-        return False
+    def removeItem(self, itemToRemove):
+        if itemToRemove in self.inventory:
+            self.inventory.remove(itemToRemove)
 
-    def getRoom(self, direction):
-        dirStr = f"{direction}_to"
-        for key in self.__dict__.keys():
-            if dirStr == key:
-                return self.__dict__[key]
+    def addItem(self, newItem):
+        self.inventory.append(newItem)
+
+    def printInventory(self):
+        if self.inventory.__len__() <= 0:
+            pass
+        else:
+            print("You take a moment to scan the room for objects.\nYou notice: ")
+            # print(self.inventory[0].name)
+            for item in self.inventory:
+                print(f'\n{item.name}, {item.description}')
+
+    def printStats(self):
+        print(f"You are in the {self.name}. \n\"{self.description}\"")
+        self.printInventory()
