cde2b1f0373efe9fb4c415a665cf09f9525f6409
maxjamb <maxwelljamesbeard@gmail.com>
2020-04-09 15:56:00 +0100

    init

--- examples/guessing_game.py
+++ examples/guessing_game.py
@@ -1,28 +1,36 @@
-import random
+# import random
 
-def guessing_game():
-  print("Guess the number!")
+# def guessing_game():
+#   print("Guess the number!")
 
-  secret_number = random.randrange(101)
+#   secret_number = random.randrange(101)
 
-  while True:
-    guess = input("Input your guess: ")
+#   while True:
+#     guess = input("Input your guess: ")
     
-    try:
-      guess = int(guess)
-    except ValueError:
-      print("Please enter an integer.")
-      continue
+#     try:
+#       guess = int(guess)
+#     except ValueError:
+#       print("Please enter an integer.")
+#       continue
 
-    print(f"You guessed: {guess}")
+#     print(f"You guessed: {guess}")
 
-    if guess == secret_number:
-      print("You win!")
-      break
-    elif guess < secret_number:
-      print("Too small!")
-    else:
-      print("Too big!")
+#     if guess == secret_number:
+#       print("You win!")
+#       break
+#     elif guess < secret_number:
+#       print("Too small!")
+#     else:
+#       print("Too big!")
 
-if __name__ == '__main__':
-  guessing_game()
+# if __name__ == '__main__':
+#   guessing_game()
+
+
+class thisIsClass:
+    pass
+
+x = thisIsClass()
+y = thisIsClass()
+print(x)
