43bd4eb7f0a9c49dea710ed87ce36159169a775f
Trevor Martin <trevor.j.martin@gmail.com>
2020-05-06 18:20:10 -0400

    test loading rooms with items

--- src/adv.py
+++ src/adv.py
@@ -1,15 +1,20 @@
-
+#!/bin/env python3
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
+h2g2 = Item(name="book",
+            description='''An electronic tablet branded with the words "don't panic".''')
+lightsword = Item(name="lightsword",
+                  description="is that a lightsword?")
 
-room = {
+rooms = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", [lightsword]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", [h2g2]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
@@ -26,14 +31,15 @@ earlier adventurers. The only exit is to the south."""),
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+rooms['outside'].n_to = rooms['foyer']
+rooms['foyer'].s_to = rooms['outside']
+rooms['foyer'].n_to = rooms['overlook']
+rooms['foyer'].e_to = rooms['narrow']
+rooms['overlook'].s_to = rooms['foyer']
+rooms['narrow'].w_to = rooms['foyer']
+rooms['narrow'].n_to = rooms['treasure']
+rooms['treasure'].s_to = rooms['narrow']
+
 
 #
 # Main
@@ -55,8 +61,10 @@ def repl(name):
 
     If the user enters "q", the game will quit.
     """
-    player1 = Player(room=room['outside'], name=name)
-    cmd = "start"
+    startAt = rooms['outside']  # < Player > starts here
+    player1 = Player(room=startAt, name=name)  # create new < Player >
+    cmd = "start"  # the first command for < Player > to evaluate.
+
     while not (cmd in ["q", "quit"]):
         # EVALUATE
         player1.evaluate(cmd)
@@ -64,7 +72,9 @@ def repl(name):
         print(player1.status())
         print(player1.look())
         # READ
+        # next command to be evaluated by < Player >
         cmd = input("what now ? ").lower()
+
     print(f'\nThanks for playing, goodbye')
 
 
