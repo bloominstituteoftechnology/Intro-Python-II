11fae3d57f06c0a4252779caf5663f298b9f5a8b
Brianna <briannakeune@gmail.com>
2020-05-06 12:02:42 -0500

    added a nicer method to print room and player

--- src/player.py
+++ src/player.py
@@ -6,3 +6,6 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+
+    def __str__(self):
+        return f"Player info:\nname: {self.name}\ncurrent room is: [\n{self.current_room}\n]"
