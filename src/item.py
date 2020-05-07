446bba990f89aa1010bbf7342b0f445012f29e4c
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-05-07 13:13:07 -0700

    finish items init

---
+++ src/item.py
@@ -0,0 +1,6 @@
+# this is currently identical to Room, decide if its worth 
+
+class Item:
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
