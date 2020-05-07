3de32c1f19dc780faa7edbf84820d1b876cddefd
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-05-07 15:54:53 -0700

    fixed player not having inventory

--- src/player.py
+++ src/player.py
@@ -3,7 +3,7 @@
 from inventory_holder import Inventory_Holder
 
 class Player(Inventory_Holder):
-    def __init__(self, name, location):
+    def __init__(self, name, location, inventory):
         self.name = name
         self.location = location
         self.inventory = inventory 
