f196d91fb8f49634dd609e766e64169f022fe50e
Lambda_School_Loaner_222 <lambda_school_loaner_222@Lambda-School-Loaner-222s-MacBook-Pro.local>
2020-05-07 16:50:55 -0700

    day 2 mvp

--- src/player.py
+++ src/player.py
@@ -5,8 +5,15 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
 
     def __str__(self):
-        return f"name: {self.name}, current room: {self.current_room}"
+        return f"name: {self.name}, current room: {self.current_room}, inventory: {self.inventory}"
 
-    
+    def pickup_item(self, item):
+        self.inventory.append(item)
+        self.current_room.items.remove(item)
+
+    def drop_item(self, item):
+        self.inventory.remove(item)
+        self.current_room.items.append(item)
