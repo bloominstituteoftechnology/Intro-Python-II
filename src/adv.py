281780ea5b09b298344ae68ecfa71d38e4885dcf
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2020-05-07 17:43:01 -0400

    yannick-wessidjam

--- src/adv.py
+++ src/adv.py
@@ -39,6 +39,9 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = room['outside']:
+    for room in player
+    print(player)
 # Write a loop that:
 #
 # * Prints the current room name
