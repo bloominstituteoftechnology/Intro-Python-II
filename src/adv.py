003d4b5241e2cea0a3ef03c2d21974e6899f44df
ElisabethShah <ElisabethShah@users.noreply.github.com>
2020-05-07 20:55:11 -0700

    Day 1 MVP

--- src/adv.py
+++ src/adv.py
@@ -1,43 +1,53 @@
 from room import Room
+from player import Player
+
+# Define the default prompt.
+
+prompt = '> '
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room('Outside Cave Entrance',
+                     'North of you, the cave mouth beckons.'),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room('Foyer',
+                     ('Dim light filters in from the south. Dusty passages '
+                      'run north and east.')),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room('Grand Overlook',
+                     ('A steep cliff appears before you, falling into the '
+                      'darkness. Ahead to the north, a light flickers in the '
+                      'distance, but there is no way across the chasm.')),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room('Narrow Passage',
+                     ('The narrow passage bends here from west to north. The '
+                      'smell of gold permeates the air.')),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room('Treasure Chamber',
+                     'You\'ve found the long-lost treasure chamber! Sadly, it '
+                     'has already been completely emptied by earlier '
+                     'adventurers. The only exit is to the south.'),
 }
 
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room['outside'].exits['n'] = room['foyer']
+room['foyer'].exits['s'] = room['outside']
+room['foyer'].exits['n'] = room['overlook']
+room['foyer'].exits['e'] = room['narrow']
+room['overlook'].exits['s'] = room['foyer']
+room['narrow'].exits['w'] = room['foyer']
+room['narrow'].exits['n'] = room['treasure']
+room['treasure'].exits['s'] = room['narrow']
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player(room['outside'])
 
 # Write a loop that:
 #
@@ -49,3 +59,10 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+move = input(f'{player.room}\n\n{prompt}')
+while move != 'q':
+    try:
+        player.room = player.room.exits[move]
+        move = input(f'{player.room}\n\n{prompt}')
+    except KeyError:
+        move = input(f'There\'s no obvious way to do that.\n\n{prompt}')
