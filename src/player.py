9e62784b197e9ae6aef9170cb49d9b7121b4f762
unknown <dj.zaragoza@icloud.com>
2020-05-09 11:21:35 -0700

    first commit building class player

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+   def __init__(self, player_name, curr_location, items_found-[]):
+      self.player_name = player_name
+      self.curr_location = curr_location
+      self.items_found = items_found
+      
+   def get_inventory(self):
+      return self.items_found
+   
+   def __str__(self):
+      return str(self.__dict__)   
