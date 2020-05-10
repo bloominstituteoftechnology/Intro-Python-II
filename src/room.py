21ee344e8032ea0b56dbf3ad73f96fe3844c9524
unknown <dj.zaragoza@icloud.com>
2020-05-09 21:50:07 -0700

    finished room.py file

--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+   def __init__(self, name, room_desc, rm_items=[]):
+      self.name = name
+      self.room_desc = room_desc
+      self.rm_items = rm_items
+      
+   def __str__(self):
+      return str(self.__dict__   
