a45fdf5af1b7c2112ef3109c32b0fd8363cf4f7a
anthonyvigz <anthonyvigz@gmail.com>
2020-05-13 14:47:47 -0400

    setting up player moves

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,12 +39,25 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+print('Welcome to the game! Enter a name for your player...')
+name = input('>>>')
+player1 = Player(name, room['outside'])
+
+playerInput = ''
+while playerInput != 'Q' or 'q':
+    print(player1)
+    print('Choose your next move: [N] North [S] South [E] East [W] West [Q] Quit')
+    playerInput = input('>>').upper()
+
+    if playerInput == 'N' or 'S' or 'E' or 'W':
+        print('hey nice')
 
 # Write a loop that:
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
+
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
