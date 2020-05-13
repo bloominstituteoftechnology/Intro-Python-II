bc26ce7c0fabc0231148b2ee8241706247f29410
AceIsHuman <menoman1995@yahoo.com>
2020-05-12 22:56:11 -0500

    create room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+import textwrap
+class Room:
+  def __init__(self, name, description):
+    self.name = name
+    self.description = description
+
+  def __str__(self):
+    wrap = textwrap.TextWrapper(width = 40)
+
+    wrap.wrap(text = self.name + "\n\n" + self.description + "\n\n")
