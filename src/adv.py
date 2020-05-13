43104cbb79f2baa5060fdd559bbcf6fff7b7d918
Corey <seschwan@yahoo.com>
2020-05-12 22:00:52 -0600

    Day 1 Complete

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -36,15 +37,37 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
+name = input('\nWelcome to Adventure game, what shall we call you...? \n\n')
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player(name, room['outside'])
+
+print(f'\nWelcome {player.name} to the mysterious adventure lands... You are currently located {player.current_room} \n\n')
+print(f'\nTo play the game you may select a direction using N, S, E, W, and to quit the game use Q')
+
 # Write a loop that:
-#
+while True:
+
 # * Prints the current room name
+
 # * Prints the current description (the textwrap module might be useful here).
+
 # * Waits for user input and decides what to do.
-#
+    selection = input("\nEnter a direction or q to escape:\n")
+
+    user_selection = selection.lower().split(" ")
+    print(user_selection)
+
+    if len(user_selection) == 1 and selection == "q":
+        print("We shall meet again...")
+        break
+    elif selection == "n" or selection == "s" or selection == "e" or selection == "w":
+            player.move(selection)
+            print(f'You are now located {player.current_room}\n')
+    else:
+        print(f'{selection} is not a valid command, try using N, S, E, W... or Q to quit.\n')
+
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player():
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+
+    def move(self, direction):
+        if getattr(self.current_room, f'{direction}_to') is not None:
+            self.current_room = getattr(self.current_room, f'{direction}_to')
+        else:
+            print('\nYou just hit your head on the wall... \n')
