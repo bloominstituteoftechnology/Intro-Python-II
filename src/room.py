c49f30a928ca39f826d21f05530e983aad55c769
marilynle <marilynle@users.noreply.github.com>
2020-05-15 02:16:49 -0500

    'add_assignment_stretch_goals'

--- src/room.py
+++ src/room.py
@@ -1,2 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    """Base class for Items"""
+    def __init__ (self, name, description, items):
+        self.name = name
+        self.description = description
+        self.items = items
+
+
+
+        
+
+   
