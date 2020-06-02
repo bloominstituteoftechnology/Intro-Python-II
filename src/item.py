a21b14608dca7da1cbeca0543b9c0684e876140e
Jon Bash <jonbash@protonmail.com>
2020-06-02 13:53:48 -0700

    add Item

---
+++ src/item.py
@@ -0,0 +1,5 @@
+class Item:
+    '''An object to be found in a room & picked up by the player'''
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
