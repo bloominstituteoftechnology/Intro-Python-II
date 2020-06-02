02f975b00374ea6ced52fb2e4e0fa3c9ff5a89b1
Jon Bash <jonbash@protonmail.com>
2020-06-02 15:38:05 -0700

    add item.on_take/on_drop

--- src/item.py
+++ src/item.py
@@ -3,3 +3,11 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def on_take(self):
+        '''Called when item is picked up'''
+        print(f"<You picked up the {self.name}.>")
+
+    def on_drop(self):
+        '''Called when item is dropped'''
+        print(f"<You dropped the {self.name} on the floor.>")
--- src/player.py
+++ src/player.py
@@ -1,9 +1,13 @@
+from room import Room
+from item import Item
+
+
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
 class Player:
     '''The player; holds current room'''
-    def __init__(self, initial_room):
+    def __init__(self, initial_room: Room):
         self.current_room = initial_room
         self.items_list = []
 
@@ -11,16 +15,18 @@ class Player:
         '''Change player's current room to the one provided'''
         self.current_room = room
 
-    def take_item(self, item):
+    def take_item(self, item: Item):
         '''Adds item from current room to inventory;
         throws exception if room does not contain item'''
         assert self.current_room.items_list.__contains__(item)
         self.current_room.items_list.remove(item)
         self.items_list.append(item)
+        item.on_take()
 
-    def drop_item(self, item):
+    def drop_item(self, item: Item):
         '''Drops item from inventory;
         throws exception if room does not contain item'''
         assert self.items_list.__contains__(item)
         self.items_list.remove(item)
         self.current_room.items_list.append(item)
+        item.on_drop()
