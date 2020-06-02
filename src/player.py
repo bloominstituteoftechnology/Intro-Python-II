3338feb21683001ded86352b70fe2773643d54f5
Jon Bash <jonbash@protonmail.com>
2020-06-02 11:14:12 -0700

    set up player

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    '''The player; holds current room'''
+    def __init__(self, initial_room):
+        self.current_room = initial_room
+
+    def enter_room(self, room):
+        self.current_room = room
