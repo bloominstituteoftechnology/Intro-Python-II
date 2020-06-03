5d722104876a7a81a518e63fe776f9b7fd3587fa
Tobi Tselios <tobi.tselios@gmail.com>
2020-06-03 22:14:17 +0100

    Modified Room class and created a Player class

--- src/room.py
+++ src/room.py
@@ -2,9 +2,10 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, player = None):
         self.name = name 
         self.description = description
+        self.player = player
 
     def __str__(self):
         return f"In the {self.name}, {str(self.description).lower()}"
