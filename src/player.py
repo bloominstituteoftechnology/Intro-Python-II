15fa3d618e7cc5ecb175be9b6046e1ce4d8e0e9a
CC <ciarabeitel@gmail.com>
2020-06-03 16:11:59 -0400

    Add player and room classes

--- src/player.py
+++ src/player.py
@@ -1,2 +1,6 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player(self):
+    def __init__(self, inRoom):
+        self.inRoom = inRoom
--- src/room.py
+++ src/room.py
@@ -1,2 +1,7 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room(self):
+    def __init__(self, name, decription):
+        self.name = name
+        self.description = decription
