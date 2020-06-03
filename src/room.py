006beff972489a60c08334a82fb5b7a0d0724685
Brianna Keune <briannakeune@gmail.com>
2020-06-03 14:46:16 -0500

    room class setup

--- src/room.py
+++ src/room.py
@@ -1,2 +1,15 @@
-# Implement a class to hold room information. This should have name and
-# description attributes.
+'''
+Creates a room object that has a name and a description
+'''
+
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return 'The room is called: {self.name}.\n{self.description}'.format(self=self)
+
+    def __repr__(self):
+        return 'Room({self.name}, {self.description})'.format(self=self)
