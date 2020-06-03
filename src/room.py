526ec7c71b20ae14b81e64b79f3d7f0e53c9bcbc
Hector <hectoremil21@hotmail.com>
2020-06-02 21:42:18 -0400

    Custom getter/setter for name (practice) and items_in_room method

--- src/room.py
+++ src/room.py
@@ -2,11 +2,24 @@
 # description attributes.
 
 class Room():
-
     def __init__(self, name, description):
-        self.name = str(name)
+        self.__name = str(name)
         self.description = str(description)
+        self.items = []
         self.n_to = None
         self.s_to = None
         self.w_to = None
         self.e_to = None
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, n):
+        self.__name = n
+
+    def items_in_room(self):
+        print(f"There are {self.items.count()} in this room:")
+        for i, item in enumerate(self.items, 1):
+            print(f"\t{i}.) {item}")
+
+    name = property(get_name, set_name)
