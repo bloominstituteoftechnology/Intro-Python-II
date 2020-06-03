9ae0d2cdc56e34bb6f90dc46a7cee63ddb63a346
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-03 20:53:28 +0300

    change adv file

--- src/adv.py
+++ src/adv.py
@@ -68,28 +68,24 @@ while player_choice != 'q':
             print("Cannot move in that direction.")
     elif player_choice == 's':
         if  (player1.current_room, 's_to'):
-            # player1.current_room = player1.current_room.s_to
+            player1.current_room = player1.current_room.s_to
             print(f"Current location: { player1.current_room.name }")
             print(player1.current_room.description)
         else:
             print("Cannot move in that direction.")
     elif player_choice == 'e':
         if  (player1.current_room, 'e_to'):
-            # player1.current_room = player1.current_room.e_to
             print(f"Current location: { player1.current_room.name }")
             print(player1.current_room.description)
         else:
             print("Cannot move in that direction.")
     elif player_choice == 'w':
         if  (player1.current_room, 'w_to'):
-            player1.current_room = player1.current_room.w_to
-            # print(f"Current location: { player1.current_room.name }")
-            # print(player1.current_room.description)
+            print(f"Current location: { player1.current_room.name }")
+            print(player1.current_room.description)
         else:
             print("Cannot move in that direction.")
     elif player_choice == 'q':
         print("Thanks for playing. Exiting game.")
     else:
         print("Not a valid direction.")
-
-
