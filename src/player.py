52a97150c3ff39449bf5e0e179a51aa50d6502ee
CC <ciarabeitel@gmail.com>
2020-06-03 16:31:27 -0400

    Import player class, select outside from room dict

--- src/player.py
+++ src/player.py
@@ -1,6 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
-class Player(self):
-    def __init__(self, inRoom):
-        self.inRoom = inRoom
+class Player():
+    def __init__(self, player_name, in_room):
+        self.player_name = player_name
+        self.in_room = in_room
+    
+    def __str__(self):
+        return f'{self.player_name} is in {self.in_room}'
--- src/room.py
+++ src/room.py
@@ -1,7 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
-class Room(self):
-    def __init__(self, name, decription):
+class Room():
+    def __init__(self, name, description):
         self.name = name
-        self.description = decription
+        self.description = description
+
+    def __str__(self):
+        return f'{self.name}: {self.description}'
