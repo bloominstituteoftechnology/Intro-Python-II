c01bf18871d1af532542507a1458dd222e135ab6
Tyler de Friess <tyler.defriess@gmail.com>
2020-06-04 17:53:37 -0400

    Built basic structure of Player, Room classes. Began constructing REPL

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -24,20 +25,21 @@ earlier adventurers. The only exit is to the south."""),
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room['outside'].connections["n"] = room['foyer']
+room['foyer'].connections["s"] = room['outside']
+room['foyer'].connections["n"] = room['overlook']
+room['foyer'].connections["e"] = room['narrow']
+room['overlook'].connections["s"] = room['foyer']
+room['narrow'].connections["w"] = room['foyer']
+room['narrow'].connections["n"] = room['treasure']
+room['treasure'].connections["s"] = room['narrow']
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player("Tyler", room["outside"])
 
 # Write a loop that:
 #
@@ -49,3 +51,15 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+print(player.name)
+
+user_is_playing = True
+while user_is_playing:
+    print(player.current_room.name)
+    print(player.current_room.description)
+
+    user_input = input("Which direction would you like to go? (n/e/s/w): ")
+
+    user_is_playing = False
+    
--- src/room.py
+++ src/room.py
@@ -1,2 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description, n_to=None, e_to=None, s_to=None, w_to=None):
+        self.name = name
+        self.description = description
+        self.connections = {
+            "n": n_to,
+            "e": e_to,
+            "s": s_to,
+            "w": w_to
+        }
