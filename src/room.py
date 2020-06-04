caae93ee76441d97d289addca636584538730cbe
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-04 22:27:13 +0300

    refactor minor changes at room directory

--- src/room.py
+++ src/room.py
@@ -8,13 +8,17 @@ class Room:
     def __init__(self, name, description, n_to = None, e_to = None, s_to = None, w_to =None, items = None):
         self.name = name
         self.description = description
-        self.n_to = n_to
-        self.e_to = e_to
-        self.s_to = s_to
-        self.w_to = w_to
-        self.items = ["Magic Axe"]
+        self.n_to = None
+        self.e_to = None
+        self.s_to = None
+        self.w_to = None
+        self.items = []
     def __str__(self):
-        return f"Room name :{self.name}, Room description: {self.description}"
+        plain = f"Room name :{self.name}, Room description: {self.description}"
+
+        for i in self.items:
+            plain = f"Available Items: {i}. {i.description}"
+        return plain
 
    
 
