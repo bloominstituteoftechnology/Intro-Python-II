66ab038cb867eff259227dc056937addc4f358db
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-04 22:23:32 +0300

    refactor and clean code player directory

--- src/player.py
+++ src/player.py
@@ -8,18 +8,9 @@ class Player:
         self.inventory = []
 
     def __str__(self):
-        return f"name: {self.name}\n{self.current_room}"
+       plain = f"name: {self.name}\nInventory :"
+        for i in self.items:
+            plain += f"{i}"
+        return plain
 
-    def move(self, direction):
-        newRoom = self.current_room.get_room
-        if newRoom is not None:
-            self.current_room = newRoom
-            print(self.current_room)
-        else:
-            print("No way to go")
-
-    def pick_up(self, item):
-        for item in self.inventory:
-            if item.name == item.name:
-                return item
-            return None
+   
