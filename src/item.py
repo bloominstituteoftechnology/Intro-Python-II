4cecd0434e9cd26b6c68b633e958886609866a8f
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-04 15:10:18 +0300

    add str method to item file

--- src/item.py
+++ src/item.py
@@ -5,3 +5,5 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+    def __str__(self):
+        return (f"{self.name}, item description: {self.description})"
--- src/room.py
+++ src/room.py
@@ -1,6 +1,8 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+from item import Item
+
 class Room:
     """A class to hold room information and description attributes."""
     def __init__(self, name, description, n_to = None, e_to = None, s_to = None, w_to =None, items = None):
@@ -13,3 +15,5 @@ class Room:
         self.items = items
     def __str__(self):
         return f"Room name :{self.name}, Room description: {self.description}"
+
+    
