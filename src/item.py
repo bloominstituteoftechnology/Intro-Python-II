bacf1054a599867dd6ca54e63d008c02ef8d5acb
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-04 22:21:22 +0300

    refactor code at item directory

--- src/item.py
+++ src/item.py
@@ -5,17 +5,7 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
     def __str__(self):
-        return (f"{self.name}, item description: {self.description}")
-
-    def __repr__(self):
-        return self.name
-
-    def pick_item(self):
-        print(f'\n***You have picked up {self.name} to your inventory***')
-
-    def drop_item(self):
-        print(f'\n***You have dropped {self.name} from your inventory***')
-
-
+        return f"{self.name}\n{self.description}"
     
