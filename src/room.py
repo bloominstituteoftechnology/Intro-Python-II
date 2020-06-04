691805ddc2d51c430e937958a0b9828a591a9252
Jon Haase <haase1020@gmail.com>
2020-06-04 11:42:13 -0400

    update room file

--- src/room.py
+++ src/room.py
@@ -2,13 +2,13 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description, n_to=None, s_to=None, e_to=None, w_to=None, items=None):
+    def __init__(self, name, description, items=None):
         self.name = name
         self.description = description
-        self.n_to = n_to
-        self.s_to = s_to
-        self.e_to = e_to
-        self.w_to = w_to
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
         self.items = [] if items is None else items
 
     def add_item(self, item):
