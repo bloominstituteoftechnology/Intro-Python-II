40687596b2b44b1b0ae636d22e6d98cbdee39554
John Daly <jcdaly97@gmail.com>
2020-06-04 11:46:03 -0700

    hit a couple stretch goals

--- src/player.py
+++ src/player.py
@@ -1,2 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player: 
+    def __init__(self, name, current_room):
+      self.name = name
+      self.current_room = current_room
+      self.inventory = []
+    def __str__(self):
+      return f"<Player '{self.name}', is in room {self.current_room}>"
+    
+    def showInventory(self):
+        if len(self.inventory) > 0:
+            for item in self.inventory:
+                print(item.name)
+        else:
+            print('You have nothing in your inventory')
--- src/room.py
+++ src/room.py
@@ -1,2 +1,23 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, name, description, isDark, loot = None):
+        self.name = name
+        self.description = description
+        self.isDark = isDark
+        if loot is None:
+            self.loot = []
+        else:
+            self.loot = loot
+
+    def __str__(self):
+        return f"{self.name}: {self.description}"
+
+    def searchRoom(self):
+        print('You rummaged around and found... ')
+        if len(self.loot) > 0:
+            for item in self.loot:
+                print(item.name)
+        else:
+            print('...nothing')
+   
