df845642047f6e28bc9e8b3177c267640e143937
HeWhoCartsRoses <carteradrewrose@gmail.com>
2020-06-04 09:44:17 -0600

    adding loop then finished

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, room, items, moves):
+        self.room = room
+        self.items = items
+        self.moves = moves
+
+    def __str__(self):
+        print('bruh requiem')
--- src/room.py
+++ src/room.py
@@ -1,2 +1,9 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, name, desc):
+        self.desc = desc
+        self.name = name
+
+    def __src__(self):
+        print('{self.name}. {self.desc}').format(self=self)
