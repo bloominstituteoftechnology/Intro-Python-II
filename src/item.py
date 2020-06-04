b3d134a1a33a87417648bc44f54e7134c9bd285c
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-04 17:54:40 +0300

    working changes in item and room file

--- src/item.py
+++ src/item.py
@@ -8,4 +8,14 @@ class Item:
     def __str__(self):
         return (f"{self.name}, item description: {self.description}")
 
+    def __repr__(self):
+        return self.name
+
+    def pick_item(self):
+        print(f'\n***You have picked up {self.name} to your inventory***')
+
+    def drop_item(self):
+        print(f'\n***You have dropped {self.name} from your inventory***')
+
+
     
--- src/room.py
+++ src/room.py
@@ -12,26 +12,10 @@ class Room:
         self.e_to = e_to
         self.s_to = s_to
         self.w_to = w_to
-        self.items = []
+        self.items = ["Magic Axe"]
     def __str__(self):
         return f"Room name :{self.name}, Room description: {self.description}"
 
-    def pick_item(self, item_name):
-        for item in self.items:
-            if item.name == item_name:
-                return item
-        return None
-
-    def move_room(self, direction):
-        if direction == "n":
-            return self.n_to
-        elif direction == "s":
-            return self.s_to
-        elif direction == "e":
-            return self.e_to
-        elif direction == "w":
-            return self.w_to
-        else:
-            return None
+   
 
     
