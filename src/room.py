fad113f79a7db22e2ca4e6fe73ca758ec89a65f1
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-06-04 08:34:46 +0100

    Room class tweaked

--- src/room.py
+++ src/room.py
@@ -1,9 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, player = None):
         self.name = name 
         self.description = description
+        self.player = player
 
     def __str__(self):
          return f"In the {self.name}, {str(self.description).lower()}"
