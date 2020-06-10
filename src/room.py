25e269c161331a981d32e63da127997f3aa08854
rfamilara <techknohauz.com>
2020-06-09 21:31:02 -0500

    Completed Day 1 MVP: -adv.py -player.py -room.py

--- src/room.py
+++ src/room.py
@@ -1,2 +1,18 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+
+        return f'room: {self.name}, description: {self.description}'
+
+    name = str
+    description = str
+    n_to = str
+    s_to = str
+    e_to = str
+    w_to = str 
