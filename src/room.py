afaef629a51e0bf34e1c41fe7937c8f9f39f498a
Michael Lowe <mrlowememphis@yahoo.com>
2020-06-10 21:23:19 -0500

    room refactored

--- src/room.py
+++ src/room.py
@@ -2,13 +2,13 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description, n_to, s_to, e_to, w_to):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.n_to = n_to
-        self.s_to = s_to
-        self.e_to = e_to
-        self.w_to = w_to
+        self.n_to = None
+        self.s_to = None
+        self.w_to = None
+        self.e_to = None
         
         
         
