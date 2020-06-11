922cb8c9c425ce8f9e82281a11d6bee379dd5b48
Michael Lowe <mrlowememphis@yahoo.com>
2020-06-10 22:13:35 -0500

    item.py refactored

--- src/item.py
+++ src/item.py
@@ -1,4 +1,7 @@
 class Item:
-    def __init__(self, name, description):
+    def __init__(self, name, value):
         self.name = name
-        self.description = description
+        self.value = value
+
+    def __str__(self):
+        return f"{self.name}, Value: {self.value}" 
