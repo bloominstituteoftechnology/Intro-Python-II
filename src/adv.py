59aa45412ad805548fd10cba46f9fa1ebb40029e
JC8747 <jonathan.chiaverini@gmail.com>
2020-06-11 20:23:58 -0400

    MVP, Part II

--- src/adv.py
+++ src/adv.py
@@ -1,20 +1,20 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside': Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer': Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow': Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
@@ -22,17 +22,25 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
+items = {
+    'torch': Item('torch', 'a simple torch'),
+    'sword': Item('sword', 'such a very sharp sword')
+}
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room['outside'].n_to = 'foyer'
+room['foyer'].s_to = 'outside'
+room['foyer'].n_to = 'overlook'
+room['foyer'].e_to = 'narrow'
+room['overlook'].s_to = 'foyer'
+room['narrow'].w_to = 'foyer'
+room['narrow'].n_to = 'treasure'
+room['treasure'].s_to = 'narrow'
+
+# Place room items
+
+room['foyer'].items = [items['torch'], items['sword']]
 
 #
 # Main
@@ -86,7 +94,7 @@ while player_input != 'q':
                 jack.get_item(items[input_arr[1]])
                 print(f"You picked up the {input_arr[1]}")
                 player_input = input("Please enter a command:")
-            else: 
+            else:
                 print(f"There is no {input_arr[1]} in this room ya goober")
                 player_input = input("Please enter a command:")
         elif input_arr[0] == 'drop':
@@ -94,7 +102,7 @@ while player_input != 'q':
                 room[jack.current_room].receive_item(items[input_arr[1]])
                 print(f"You dropped the {input_arr[1]}")
                 player_input = input("Please enter a command:")
-            else: 
+            else:
                 print(f"You don't have a {input_arr[1]} to drop!")
                 player_input = input("Please enter a command:")
         else:
--- src/room.py
+++ src/room.py
@@ -2,13 +2,28 @@
 # description attributes.
 
 class Room():
-    def __init__(self, name, description):
-        self.name=name
-        self.description = description
-        self.n_to=None
-        self.e_to=None
-        self.s_to=None
-        self.w_to=None
+  def __init__(self, name, description):
+    self.name = name
+    self.description = description
+    self.n_to = None
+    self.s_to = None
+    self.e_to = None
+    self.w_to = None
+    self.items = []
 
-def __str__(self):
-    return f'Current Location: {self.name} - {self.description}'
+  def print_items(self):
+    for i in self.items:
+      print(i)
+
+  def receive_item(self, item):
+    self.items.append(item)
+
+  def got_item(self, item_name):
+    for i in self.items:
+      if i.name == item_name:
+        self.items.remove(i)
+        return True
+    return False
+
+  def __str__(self):
+      return f"Your Location: {self.name} - {self.description}"
