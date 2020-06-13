91ba048ca9087627c44196a564ec885782cabc30
Your name <a.m.verdine@gmail.com>
2020-06-13 01:16:47 -0400

    Build Player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,17 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, guild, weapon, ability ):
+        self.name = name
+        self.guild = guild
+        self.weapon = weapon
+        self.ability = ability
+
+    def __str__(self):
+        output = f"Welcome {self.name}!"
+        return output
+        
+
+player_1 = Player("Noraa Enidrev", "Dwarves Guild", "Battleaxe", "Armor Regeneration")
+
+print(player_1)
