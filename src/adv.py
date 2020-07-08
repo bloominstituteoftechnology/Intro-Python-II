8a561c9693c2f77fd015a8a4d32c34a0ea625f1f
wesley.moody <wgm3190@gmail.com>
2020-07-08 14:54:08 -0500

    tweaking language

--- src/adv.py
+++ src/adv.py
@@ -21,7 +21,7 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
-# print(room['outside'])
+print(room['outside'])
 
 # Link rooms together
 
