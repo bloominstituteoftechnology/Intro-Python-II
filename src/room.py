b53234e02b264caa1d226d245631b33188210e26
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-07-08 16:01:37 -0400

    Adds directional pointers to Room class

--- src/room.py
+++ src/room.py
@@ -5,6 +5,11 @@
 # which point to the room in that respective direction.
 
 class Room:
+    n_to = "north"
+    s_to = "south"
+    e_to = "east"
+    w_to = "west"
+
     def __init__(self, name, description):
         self.name = name
         self.description = description
@@ -12,23 +17,27 @@ class Room:
     def __str__(self):
         return f"LOCATION: {self.name} \nINFO: {self.description}"
 
-# room = {
-#     'outside':  Room("Outside Cave Entrance",
-#                      "North of you, the cave mount beckons"),
 
-#     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-# passages run north and east."""),
 
-#     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-# into the darkness. Ahead to the north, a light flickers in
-# the distance, but there is no way across the chasm."""),
+if __name__ == "__main__":
+    room = {
+        'outside':  Room("Outside Cave Entrance",
+                        "North of you, the cave mount beckons"),
 
-#     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-# to north. The smell of gold permeates the air."""),
+        'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    passages run north and east."""),
 
-#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-# chamber! Sadly, it has already been completely emptied by
-# earlier adventurers. The only exit is to the south."""),
-# }
+        'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    into the darkness. Ahead to the north, a light flickers in
+    the distance, but there is no way across the chasm."""),
 
-# print(room['treasure'])
+        'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    to north. The smell of gold permeates the air."""),
+
+        'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    chamber! Sadly, it has already been completely emptied by
+    earlier adventurers. The only exit is to the south."""),
+    }
+
+    print(room['treasure'])
+    breakpoint()
