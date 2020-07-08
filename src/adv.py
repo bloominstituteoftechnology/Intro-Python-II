b5ccb30b04aa76070afaff5d11feac099b8630a8
Elysia Gabe <elysiagabe@gmail.com>
2020-07-08 14:48:47 -0700

    Fills out Player & Room classes and creates command parser in adv.py. Day 1 MVP met.

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,6 @@
+import sys
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -49,3 +51,63 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+is_playing = False
+
+# Welcome message: 
+print("Welcome to Adventure Quest!")
+# Ask if user wants to play game:
+player_start = input("Do you want to go an adventure to find some hidden treasure (Y = yes, N = no)?: ")
+
+if player_start.upper() == 'N':
+    print("That's too bad. Well, if you change your mind, we'll be around. Tata for now!")
+    sys.exit()
+elif player_start.upper() == 'Y':
+    is_playing = True
+    print("Yay! That's great news. We're going to be rich. Before we get started, if at anytime you need to go home, enter 'Q' to quit the game. Now let's get started.")
+#TODO: invalid response (not y or n)
+
+# Collect player's name and instantiate player...by default, player starts in 'outside' room
+player_name = input("First things first. Tell me, what is your name? ")
+current_player = Player(player_name, room['outside'])
+
+print("It's nice to meet you ", current_player.name.capitalize(), "!", current_player.current_room)
+
+# Main game loop: 
+while is_playing: 
+    # print(current_player.current_room)
+    action = input("Where would you like to go next? (N = north, E = east, S = south, W = west): ")
+
+    if action.upper() == 'Q': 
+        is_playing = False
+        print("Goodbye! Hope to see you again some time soon.")
+    
+    elif action.upper() == 'N':
+        if (hasattr(current_player.current_room, 'n_to')):
+            current_player.current_room = current_player.current_room.n_to
+            print(current_player.current_room)
+        else: 
+            print("Nothing's there. Let's go somewhere else.")
+
+    elif action.upper() == 'E':
+        if (hasattr(current_player.current_room, 'e_to')):
+            current_player.current_room = current_player.current_room.e_to
+            print(current_player.current_room)
+        else: 
+            print("You must not go that way! Try again.")
+
+    elif action.upper() == 'S':
+        if (hasattr(current_player.current_room, 's_to')):
+            current_player.current_room = current_player.current_room.s_to
+            print(current_player.current_room)
+        else: 
+            print("Doesn't look like anything's that way.")        
+
+    elif action.upper() == 'W':
+        if (hasattr(current_player.current_room, 'w_to')):
+            print(current_player.current_room.w_to)
+        else: 
+            print("Looks like a wall with no doors. Head in a different direction.")
+
+    else: 
+        print("That's not an option. Please enter a valid direction. Remember, N = north, E = east, S = south, W = west.")
--- src/room.py
+++ src/room.py
@@ -1,2 +1,19 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room: 
+    def __init__(self, name, description):
+        self.name = name
+        self.desc = description
+
+    def __str__(self):
+        return f"You are currently located in the {self.name}. {self.desc}."
+
+# b = Room('bathroom', 'stinky and for pooping and stuff')
+# b.n_to = 'closet'
+# # b.e_to = 'kitchen'
+
+
+# if hasattr(b, 'e_to'): 
+#     print('room to the east exists:', b.e_to)
+# else: 
+#     print('room tot he east does NOT exist')
