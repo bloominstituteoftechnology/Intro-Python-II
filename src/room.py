d158c7531d94d19b2ea614f3ab3c34485556df6f
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-07-08 16:01:18 -0700

    Implementing room

--- src/room.py
+++ src/room.py
@@ -1,2 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
+
+        self.n_to: Room = None
+        
