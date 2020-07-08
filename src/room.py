e045dbf76f4e5a80892ff9dc187b7c78530a5275
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-07-08 15:43:30 -0400

    Create Room class with name and description attributes

--- src/room.py
+++ src/room.py
@@ -1,2 +1,34 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+# The room should also have n_to, s_to, e_to, and w_to attributes 
+# which point to the room in that respective direction.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f"LOCATION: {self.name} \nINFO: {self.description}"
+
+# room = {
+#     'outside':  Room("Outside Cave Entrance",
+#                      "North of you, the cave mount beckons"),
+
+#     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+# passages run north and east."""),
+
+#     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+# into the darkness. Ahead to the north, a light flickers in
+# the distance, but there is no way across the chasm."""),
+
+#     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+# to north. The smell of gold permeates the air."""),
+
+#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+# chamber! Sadly, it has already been completely emptied by
+# earlier adventurers. The only exit is to the south."""),
+# }
+
+# print(room['treasure'])
