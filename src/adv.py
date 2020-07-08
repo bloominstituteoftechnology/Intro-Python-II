660d5f50e19cb3ac345db097d3bd48f0789e75cf
unknown <coopervos1@gmail.com>
2020-07-08 17:31:30 -0500

    upto grabbing items

--- src/adv.py
+++ src/adv.py
@@ -1,29 +1,44 @@
 from room import Room
+from player import Player
+from item import Item
+import time
+
+# function to return key for any value 
+def get_key(val): 
+    for key, value in room.items(): 
+         if val == value: 
+             return key 
+  
+    return "key doesn't exist"
+
+# Declare all the items
+item1 = Item('sword', 'The blade is sharp. I best be careful with this. Hopefully I won\'t need to use it.')
+item2 = Item('shield', 'An old wooden shield with some wierd symbols engraved on it.')
+item3 = Item('bow', 'This is useless unless I can find some arrows...')
+item4 = Item('arrows', 'Sharp! Now if only I had a bow...')
+item5 = Item('hookshot', 'Maybe I can cross that chasm with this...')
 
 # Declare all the rooms
-
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", []),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", [item2]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", [item1]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", [item3]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", [item4, item5]),
 }
 
-
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -33,19 +48,51 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
 # Main
-#
+name = input('--------------------------------------------\nWhat is your adventurer\'s name?\n--------------------------------------------\n')
+player = Player(name, 'outside', [])
+user_input = "p"
 
-# Make a new player object that is currently in the 'outside' room.
+while user_input != "q":
+    print("--------------------------------------------\nLOCATION:\nThe", room[player.current_room].name)
+    print(room[player.current_room].description)
+    if len(room[player.current_room].items) > 0:
+        print('\nNEARBY ITEMS:')
+        for item in room[player.current_room].items:
+            print('A', item.name, '-', item.description)
+    print('\nPick an action for', player.name)
 
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+    user_input = input("\n\n        [n] North\n[w] West         [e] East\n        [s] South\n\n\n[i] Inventory\n[g] Grab an item\n[q] Quit\n\n")
+
+    if user_input == 'n':
+        try:
+            player.current_room = get_key(room[player.current_room].n_to)
+        except:
+            print('There is nothing to be gained by going in this direction...')
+            time.sleep(1.5)
+    if user_input == 'e':
+        try:
+            player.current_room = get_key(room[player.current_room].e_to)
+        except:
+            print('There is nothing to be gained by going in this direction...')
+            time.sleep(1.5)
+    if user_input == 's':
+        try:
+            player.current_room = get_key(room[player.current_room].s_to)
+        except:
+            print('There is nothing to be gained by going in this direction...')
+            time.sleep(1.5)
+    if user_input == 'w':
+        try:
+            player.current_room = get_key(room[player.current_room].w_to)
+        except:
+            print('There is nothing to be gained by going in this direction...')
+            time.sleep(1.5)
+    if user_input == 'i':
+        for item in player.items:    
+            print(item.name, ' ', item.description)
+        time.sleep(1.5)
+    if user_input == 'g':
+        for item in room[player.current_room].items:
+            player.items.append(item)
+            room[player.current_room].items.remove(item)
--- src/room.py
+++ src/room.py
@@ -1,2 +1,13 @@
-# Implement a class to hold room information. This should have name and
-# description attributes.
+class Room:
+    def __init__(self, name, description, items):
+        self.name = name
+        self.description = description
+        self.items = items
+
+    """
+    Specs say the following:
+    The room should also have n_to, s_to, e_to, and w_to attributes which point to the room in that respective direction.
+
+    I'm confused on this. I was given starter code that connected the rooms correctly already. Am I supposed to be transferring that
+    starting code into the Room class?
+    """
