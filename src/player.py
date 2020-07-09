49723cafa42597524c6dd7098105f27fa8f96bfb
Ben <bbjberg91@gmail.com>
2020-07-08 21:05:30 -0400

    room player and nav done

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+#assigned name and room properties as well as method to return name and location.
+class Player:
+    def __init__(self, name, room):
+        self.name = name
+        self.room = room
+
+    def __str__(self):
+        return f'{self.name}, is in {self.room}'
+        
--- src/room.py
+++ src/room.py
@@ -1,2 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+# assigned name and desc as well as set up directional connections 
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None 
+    def __str__(self):
+            return f'{self.name}, {self.description}'
