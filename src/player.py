ea26ea7ca7fc6e9797e47027bed9d7e228e22084
Poenomaly <djbenson741@gmail.com>
2020-07-09 15:23:14 -0600

    Complete day 2 MVP for Adventure Game

--- src/player.py
+++ src/player.py
@@ -6,6 +6,7 @@
 
 class Player:
 
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, inventory=[]):
         self.name = name
         self.current_room = current_room
+        self.inventory = inventory
--- src/room.py
+++ src/room.py
@@ -8,30 +8,12 @@
 # rooms to include: outside, foyer, overlook, narrow, treasure
 class Room:
 
-    def __init__(self, name, description):
+    def __init__(self, name, description, n_to=None, s_to=None, e_to=None, w_to=None, items=None):
 
         self.name = name
         self.description = description
-
-    def outside(self, n_to='foyer'):
-
         self.n_to = n_to
-
-    def foyer(self, s_to='outside', n_to='overlook', e_to='narrow'):
-
         self.s_to = s_to
-        self.n_to = n_to
         self.e_to = e_to
-
-    def overlook(self, s_to='foyer'):
-
-        self.s_to = s_to
-
-    def narrow(self, w_to='foyer', n_to='treasure'):
-
         self.w_to = w_to
-        self.n_to = n_to
-
-    def treasure(self, s_to='narrow'):
-
-        self.s_to = s_to
+        self.items = items
