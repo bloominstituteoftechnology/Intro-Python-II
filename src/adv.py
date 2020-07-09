e78bed0a4eb45480cef011ee3d65be635a9fdaae
Ramses Gasque <cedro.gasque@wsu.edu>
2020-07-09 09:41:53 -0700

    finishing up

--- src/adv.py
+++ src/adv.py
@@ -1,44 +1,32 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
-room = {
+rooms = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", {'n': 'foyer'}),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", {'n': 'overlook', 'e': 'narrow', 's': 'outside'}),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", {'s': 'foyer'}),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", {'n': 'treasure', 'w': 'foyer'}),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", {'s': 'narrow'}),
 }
-
-
-# Link rooms together
-
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
 
 #
 # Main
 #
-
+fix_d=lambda d:"North"if d=="n"else"South"if d=="s"else"West"if d=="w"else"East"
 # Make a new player object that is currently in the 'outside' room.
-
+player = Player('outside')
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +37,19 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+loop = True
+while(loop):
+    room = rooms[player.room]
+    print(room)
+    valid = False
+    while (not valid):
+        inp = input("What would you like to do?\n")
+        if inp == "q":
+            loop = False
+            valid = True
+        elif inp in room.exits:
+            player.set_room(room.exits[inp])
+            valid = True
+        else:
+            print(f"\aYou attempt to walk {fix_d(inp)} and slam your face directly into a wall.")
+            print("Maybe you shouldn't do that next time? I'd hate for your adventure to end prematurely.")
--- src/room.py
+++ src/room.py
@@ -1,2 +1,17 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from textwrap import TextWrapper
+textwrap = TextWrapper()
+class Room:
+    def __init__(self, name, description, exits):
+        self.name = name
+        self.description = description
+        self.exits = exits
+
+    def exit_to(direction):
+        return True if direction in self.exits else False
+
+    def __str__(self):
+        return f"""{self.name}
+        {textwrap.fill(self.description)}
+        There are exits to the {", ".join(self.exits.keys()).upper()}."""
