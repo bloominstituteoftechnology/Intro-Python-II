88e6262ddec5b66651f378f895204e3886e7d925
Avery Quinn <avery.quinn1493@gmail.com>
2020-07-08 22:47:50 -0700

    player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,18 @@
+# python player.py
+
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player():
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+
+    def __repr__(self):
+        return f"Name: {self.name}, Current Room: {self.current_room}"
+
+
+if __name__ == "__main__":
+    player_1 = Player("Adam", "Kitchen")
+    breakpoint()
+    print(player_1)
