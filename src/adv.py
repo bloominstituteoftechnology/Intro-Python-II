f70671edd17f16be6615d75c57280b2100634c74
cjogles <jackson.ogles28@gmail.com>
2020-07-09 14:39:13 -0600

    added another room for stretch goal

--- src/adv.py
+++ src/adv.py
@@ -1,13 +1,14 @@
+# Execute this file to play adventure game!
+
 import textwrap
 from room import Room
 from player import Player
 from item import Item
 
 # Declare all the rooms
-
 room = {
     'outside':  Room("Outside Cave Entrance",
-"North of you, the cave mount beckons", [Item("sword", "long sharp sword crafted by orcs"), Item("potion", "potion granting the ability to become invisible")]),
+                     "North of you, the cave mount beckons", [Item("sword", "long sharp sword crafted by orcs"), Item("potion", "potion granting the ability to become invisible")]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east.""", [Item("talisman", "magical artifact granting the owner psychic abilities"), Item("spear", "10 foot long spear")]),
@@ -18,15 +19,15 @@ the distance, but there is no way across the chasm.""", [Item("relic", "magical 
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air.""", [Item("turkey leg", "food that grants 100 percent healing"), Item("rope", "use this to climb down cliff")]),
+    
+    'dungeon':  Room("Dark Dungeon", """The cold dang air smells of sweat and evil! You might want to get out of here quick.""", [Item("vampire", "this vampire wants to be your friend")]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south.""", [Item("rubies", "treasure!"), Item("gold", "treasure!"), Item("emeralds", "treasure!")]),
 }
 
-
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -34,31 +35,17 @@ room['foyer'].e_to = room['narrow']
 room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
+room['narrow'].s_to = room['dungeon']
+room['dungeon'].n_to = room['narrow']
 room['treasure'].s_to = room['narrow']
-
-#
-# Main
-#
-
-# Make a new player object that is currently in the 'outside' room.
-
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
 
 def adventure_awaits():
 
     new_name = input("\nEnter Your NAME! Adventure Awaits...\n-->  ")
     current_player = Player(new_name, room["outside"], [])
     playing = True
-    print(f"\nHello {current_player.name}!\nExplore rooms and obtain treasure! \nAdventure Awaits!\n")
+    print(
+        f"\nHello {current_player.name}!\nExplore rooms and obtain treasure! \nAdventure Awaits!\n")
 
     while (playing):
         wrapper = textwrap.TextWrapper(width=70)
@@ -81,7 +68,8 @@ def adventure_awaits():
         print("----------------------------")
 
         # waits for user input to decide what to do next
-        action = input("\n\nWhat shall the hero do next??? \ntype 'help' for a hint! \n-->  ")
+        action = input(
+            "\n\nWhat shall the hero do next??? \ntype 'help' for a hint! \n-->  ")
         num_args = action.split()
 
         # loop logic here
@@ -109,12 +97,12 @@ def adventure_awaits():
             elif (action == "q"):
                 playing = False
             elif (action == "i"):
-                print(f"{current_player.name} items:" )
+                print(f"{current_player.name} items:")
                 for each_item in current_player.inventory:
                     print(f"{each_item.name} --- {each_item.description}")
             else:
                 wrapper = textwrap.TextWrapper(width=70)
-                error=wrapper.wrap(text="The game takes a one letter command, or a two word command. For a one letter command, possible commands include: n = north, s = south, w = west, e = east, i = display player inventory, q = quit game. For two letter commands you can do the following: take 'item-name', get 'item-name' or drop 'item-name' to add or remove items from player inventory and rooms.")
+                error = wrapper.wrap(text="The game takes a one letter command, or a two word command. For a one letter command, possible commands include: n = north, s = south, w = west, e = east, i = display player inventory, q = quit game. For two letter commands you can do the following: take 'item-name', get 'item-name' or drop 'item-name' to add or remove items from player inventory and rooms.")
                 for each_line in error:
                     print(each_line)
 
@@ -124,9 +112,11 @@ def adventure_awaits():
                 for count, item in enumerate(current_player.room.items):
                     if (num_args[1] == item.name):
                         item_present = True
-                        current_player.inventory.append(current_player.room.items[count])
+                        current_player.inventory.append(
+                            current_player.room.items[count])
                         current_player.room.items[count].on_take()
-                        current_player.room.items.remove(current_player.room.items[count])
+                        current_player.room.items.remove(
+                            current_player.room.items[count])
                     else:
                         pass
                 if (item_present):
@@ -138,9 +128,11 @@ def adventure_awaits():
                 for count, item in enumerate(current_player.inventory):
                     if (num_args[1] == item.name):
                         item_present = True
-                        current_player.room.items.append(current_player.inventory[count])
+                        current_player.room.items.append(
+                            current_player.inventory[count])
                         current_player.inventory[count].on_drop()
-                        current_player.inventory.remove(current_player.inventory[count])
+                        current_player.inventory.remove(
+                            current_player.inventory[count])
                     else:
                         pass
                 if (item_present):
