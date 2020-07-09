a75d87fef2afc8d494abd559ddff2571d11d994b
Mark Poggi <markp@mac.com>
2020-07-09 09:07:25 -0700

    Python II assigment complete

--- src/player.py
+++ src/player.py
@@ -1,2 +1,20 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+        self.items = []
+
+    def getItem(self, item):
+        return self.items.append(item)
+
+    def dropItem(self, item):
+        return self.items.remove(item)      
+
+    def __repr__(self):
+        return(f"You are {self.current_room}")
+
+        
+
