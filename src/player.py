b00556da04895b0cd6fa5a89c4dba708586c8dc2
Avery Quinn <avery.quinn1493@gmail.com>
2020-07-09 16:06:36 -0700

    inventory

--- src/player.py
+++ src/player.py
@@ -1,18 +1,31 @@
 # python player.py
 
-# Write a class to hold player information, e.g. what room they are in
-# currently.
-
 class Player():
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, inventory = []):
         self.name = name
         self.current_room = current_room
+        self.inventory = inventory
 
     def __repr__(self):
-        return f"Name: {self.name}, Current Room: {self.current_room}"
+        return f"{self.name.upper()}, you are currently at the {self.current_room}."
 
+    
+    def on_take(self, item):
+        item = str(item)
+        if not item in self.inventory:
+            self.inventory.append(item)
+            print(f"You have picked up {item}!")
+    
+    def on_drop(self, item):
+        item = str(item)
+        self.inventory.remove(item)
+
+    def move(self):
+        # TODO
+        pass
+
+        
 
 if __name__ == "__main__":
     player_1 = Player("Adam", "Kitchen")
-    breakpoint()
     print(player_1)
