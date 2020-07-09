17aa7f6cabb93073b8ba74d76d9651fb9243388a
Teague <teaguehannam@gmail.com>
2020-07-09 12:51:18 -0600

    Day 1 MVP

--- src/adv.py
+++ src/adv.py
@@ -1,29 +1,22 @@
 from room import Room
-
-# Declare all the rooms
+from player import Player
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance","North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. 
+    Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west to north. 
+    The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! 
+    Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 }
 
-
-# Link rooms together
-
+# Map
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -32,20 +25,43 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
+wrongWay = "\n\nThere is no room in that direction!\n\n"
 
-#
-# Main
-#
+# Start Game
+playGame = True
+player = Player(room['outside'])
 
-# Make a new player object that is currently in the 'outside' room.
+while playGame:
 
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+    print(f'\nCurrent location: {player.room.name}')
+    print(player.room.description, "\n")
+    direction = input('Which way would you like to go?\nn for North, e for East, s for South, w for West, q to quit\n')
+
+    try:
+        if direction == 'n':
+            player.room = player.room.n_to
+    except:
+        print(wrongWay)
+
+    try:
+        if direction == 'e':
+            player.room = player.room.e_to
+    except:
+        print(wrongWay)
+
+    try:
+        if direction == 's':
+            player.room = player.room.s_to
+    except:
+        print(wrongWay)
+
+    try:
+        if direction == 'w':
+            player.room = player.room.w_to
+    except:
+        print(wrongWay)
+
+    if direction == 'q':
+        print("\n\nGame over!")
+        playGame = False
+
