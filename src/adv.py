b0a5b788e744e7f630e148ce727a5556d1b99a4b
Avery Quinn <avery.quinn1493@gmail.com>
2020-07-09 16:07:06 -0700

    take and drop items

--- src/adv.py
+++ src/adv.py
@@ -1,10 +1,8 @@
-# python adv.py
-
-from room import Room
+from room import * 
 from player import Player
+from item import Item
 
 # Declare all the rooms
-
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
@@ -30,9 +28,7 @@ Grab a snack to keep up your energy."""),
 these waters are very dangerous for swimmers."""),
 }
 
-
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['outside'].s_to = room['lake']
 room['foyer'].s_to = room['outside']
@@ -47,36 +43,38 @@ room['kitchen'].e_to = room['foyer']
 room['lake'].n_to = room['outside']
 
 #
-# Main
+### Main ###
 #
 
-# Make a new player object that is currently in the 'outside' room.
+# New player object; currently in the 'outside' room
 name = input("Hey! What's your name...?\n>>>")
 new_player = Player(name, room["outside"])
 print(f"              WELCOME TO THE PARTY, {new_player.name.upper()}!")
 print("------------------------------------------------------")
 print("------------------------------------------------------")
 
-# Write a loop that:
+# Loop:
 while True:
-    # * Prints the current room name
-    # * Prints the current description (the textwrap module might be useful here).
+    # Current room, description, and visible items
+    print(new_player.__repr__())
     current_room = new_player.current_room
-    print(f"Current room: {current_room.name}")
-    print(f"Description: {current_room.description}")
+    if len(items[current_room.name]) == 0:
+        print("Visable Items: None")
+    else:
+        print(f"Visable Items: {items[current_room.name]}")
     
-    # * Waits for user input and decides what to do.
+    # User input
     print("---------------------------")
-    print("       Which direction would you like to go?")
-    direction = input("[n] North  [s] South   [e] East   [w] West  [q] Quit\n>>>")
+    print("Which direction would you like to go?")
+    direction = input("[n] North  [s] South   [e] East   [w] West\nOther Actions\n[i] Inventory   [take item_name] Take   [drop item_name] Drop   [q] Quit\n>>>")
 
     # If the user enters a cardinal direction, attempt to move to the room there.
     # Print an error message if the movement isn't allowed.
+    current_room = new_player.current_room
     if direction == "n":
         print("You selected North.".upper())
         if current_room.n_to is not None:
             new_player.current_room = current_room.n_to
-            print(f"You are now entering the {new_player.current_room.name}!!")
             print("---------------------------")
         else:
             print(":( Im sorry, you've reached a dead end...")  
@@ -85,7 +83,6 @@ while True:
         print("You selected South.".upper())
         if current_room.s_to is not None:
             new_player.current_room = current_room.s_to
-            print(f"You are now entering the {new_player.current_room.name}!!")
             print("---------------------------")
         else:
             print(":( Im sorry, you've reached a dead end...") 
@@ -94,7 +91,6 @@ while True:
         print("You selected East.".upper())
         if current_room.e_to is not None:
             new_player.current_room = current_room.e_to
-            print(f"You are now entering the {new_player.current_room.name}!!")
             print("---------------------------")
         else:
             print(":( Im sorry, you've reached a dead end...") 
@@ -103,17 +99,43 @@ while True:
         print("You seleected West.".upper())
         if current_room.w_to is not None:
             new_player.current_room = current_room.w_to
-            print(f"You are now entering the {new_player.current_room.name}!!")
             print("---------------------------")
         else:
             print(":( Im sorry, you've reached a dead end...".upper()) 
             print("---------------------------")
+    elif direction == "i":
+        print("INVENTORY LIST:", new_player.inventory)
+        print("---------------------------")
+    elif direction.startswith("take"):
+        if len(items[current_room.name]) == 0:
+            print("ACTION NOT AVAILABLE: No Items In Room")
+            print("---------------------------") 
+        else:
+            direction = direction.split(" ")
+            if len(direction) == 2 and direction[1] in items[current_room.name]:
+                new_player.on_take(direction[1])
+                items[current_room.name].remove(direction[1])
+                print("INVENTORY LIST:", new_player.inventory)
+                print("---------------------------")
+            else:
+                print("THAT ITEM IS NOT AVAILABLE")
+    elif direction.startswith("drop"):
+        if len(new_player.inventory) == 0:
+            print("ACTION NOT AVAILABLE: No Items In Inventory")
+            print("---------------------------")
+        else:
+            direction = direction.split(" ")
+            if len(direction) == 2 and direction[1] in new_player.inventory:
+                new_player.on_drop(direction[1])
+                items[current_room.name].append(direction[1])
+                print("INVENTORY LIST:", new_player.inventory)
+                print("---------------------------")
+            else:
+                print("ITEM NOT IN INVENTORY")
     elif direction == "q":
         # If the user enters "q", quit the game.
         print("Ciao Bella! o.0")
-        break
     else:
-        print("Not a valid input")
+        print("** SORRY, THAT'S NOT A VALID INPUT. **")
         
-    
     
