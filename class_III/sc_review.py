9a445448d78b0d136fded4516b26573eaae108d1
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-07-10 11:06:05 -0400

    SC re4view

--- class_III/sc_review.py
+++ class_III/sc_review.py
@@ -21,35 +21,45 @@ humans = [
     Human("David", 31),
 ]
 
+# breakpoint()
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [i for i in humans if i.name[0] == "D"]
 print(a)
+print("-----" * 5)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [i for i in humans if i.name[-1] == "e"]
 print(b)
+print("-----" * 5)
+
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
 c = []
 print(c)
+print("-----" * 5)
+
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
 d = [(i.age + 10) for i in humans]
 print(d)
+print("-----" * 5)
+
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
 e = [f"{i.name}-{i.age}" for i in humans]
 print(e)
+print("-----" * 5)
+
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
@@ -57,6 +67,8 @@ print(e)
 print("Names and ages between 27 and 32:")
 f = []
 print(f)
+print("-----" * 5)
+
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
@@ -64,9 +76,12 @@ print(f)
 print("All names uppercase:")
 g = []
 print(g)
+print("-----" * 5)
+
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
 h = []
 print(h)
+print("-----" * 5)
