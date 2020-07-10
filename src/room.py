2701a2b8a0939914b74a99c259d11cfc74d4ab0e
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-07-10 10:00:18 -0400

    Implemented direction attributes in Room class

--- src/room.py
+++ src/room.py
@@ -2,6 +2,6 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, n_to, s_to, e_to, w_to):
         self.name = name
         self.description = description
