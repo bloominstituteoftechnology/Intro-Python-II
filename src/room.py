a71678c6e2f1ef2388bf4bd89c0fecd26097e72a
mpHarm88 <mpHarm88@users.noreply.github.com>
2020-07-13 22:22:51 -0700

    add room and player .py

--- src/room.py
+++ src/room.py
@@ -1,2 +1,21 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(
+        self, name, 
+        description, 
+        # n_to="Its, Blocked", 
+        # e_to="Its, Blocked",
+        # s_to="Its, Blocked", 
+        # w_to="Its, Blocked"
+        ):
+        self.name = name
+        self.description = description
+        # self.n_to = n_to
+        # self.e_to = e_to
+        # self.s_to = s_to
+        # self.w_to = w_to
+
+    def __str__(self):
+        return f"Room Name: {self.name}\nDescription: {self.description}"
