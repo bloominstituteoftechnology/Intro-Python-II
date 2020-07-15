3cd34242fc940351f1cf2e7393a3cb7321ef6dbc
Toniddarden1 <Toniddarden@gmail.com>
2020-07-15 00:23:50 -0400

    first half mvp complete

--- src/adv.py
+++ src/adv.py
@@ -36,9 +36,16 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
-
+from player import Player
 # Make a new player object that is currently in the 'outside' room.
+# create obj 
+# bracket in dict room 
+# player_room = starter room 
 
+player_name = input("Choose your name: ")
+player_room = room["outside"]
+player = Player(player_name, player_room)
+print(player)
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +56,50 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+while True: 
+    current_room = player.current_room
+    print(f'You are located at {current_room.name} that is {current_room.description}')
+    print("where to next?")
+    enter = input('Enter n, s , e, w and q to quit the game: ')
+    
+    if enter == 'q':
+            print(f'Thanks for playing, {player_name}. See you again soon!')
+    if enter == 'n': 
+        # The hasattr() method returns true if an object has the given named attribute and false if it does not.
+        if hasattr(current_room, 'n_to'):
+            print("going North")
+            player.current_room = current_room.n_to
+        else: 
+            print(f'{player_name} enter another location, this area is closed down ')
+    elif enter == 's':
+        if hasattr(current_room, 's_to'):
+            print(f'{player_name} you are going South my friend')
+            player.current_room = current_room.s_to
+        else: 
+            print(f'{player_name} enter another location, this area is closed down')
+    elif enter == 'e':
+        if hasattr(current_room, 'e_to'):
+            print(f'{player_name} you are moving East!')
+            player.current_room = current_room.e_to
+        else: 
+            print(f'{player_name} enter another location, this area is closed down')
+    elif enter == 'w': 
+        if hasattr(current_room, 'w_to'):
+            print(f'{player_name} going out West')
+            player.current_room = current_room.w_to
+        else: 
+            print(f'{player_name} enter another location, this area is closed down')
+    else: 
+            print('Please enter a valid location')
+            break
+   
+        
+    
+   
+    
+        
+
+
+
+
+
--- src/room.py
+++ src/room.py
@@ -1,2 +1,9 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room: 
+        # instance attributes... will vary 
+        def __init__(self, name, description):
+            self.name = name
+            self.description = description
+      
