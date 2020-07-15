12cc8aaede26956bab4bcab732c7e3dd6ee90da9
Crystal Knight <crystalknightcodes@gmail.com>
2020-07-14 22:40:47 -0400

    Tweaked the text to be more readable.  Added the ability for the user to enter their own name.  Now uses the user's name in the feedback statements.

--- src/adv.py
+++ src/adv.py
@@ -4,37 +4,37 @@ from player import Player
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
+    'outside':  Room("outside cave entrance",
                      "North of you, the cave mouth beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'entrance':    Room("entrance", """Dim light filters in from the south. Dusty
 passages run north, east and west."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'cliff': Room("steep cliff", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'passage1':   Room("Narrow Passage", """A narrow passage bends here from west
+    'passage1':   Room("narrow passage", """A narrow passage bends here from west
 to north."""),
 
-    'chamber': Room("Treasure Chamber", """You've found a small room with musty old chest in it. The only exit is to the south."""),
+    'chamber': Room("empty chamber", """You've found a small room with musty old chest in it. The only exit is to the south."""),
 
-    'passage2':   Room("Wide Passage", """A wide passage stretches out before you.  You can't see the end of it. You think you see some faint light in the east."""),
+    'passage2':   Room("wide passage", """A wide passage stretches out before you. You can't see the end of it. You think you see some faint light in the east."""),
 }
 
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['overlook'].s_to = room['foyer']
+room['outside'].n_to = room['entrance']
+room['entrance'].s_to = room['outside']
+room['entrance'].n_to = room['cliff']
+room['cliff'].s_to = room['entrance']
 room['passage1'].n_to = room['chamber']
 room['chamber'].s_to = room['passage1']
-room['foyer'].e_to = room['passage1']
-room['passage1'].w_to = room['foyer']
-room['passage2'].e_to = room['foyer']
-room['foyer'].w_to = room['passage2']
+room['entrance'].e_to = room['passage1']
+room['passage1'].w_to = room['entrance']
+room['passage2'].e_to = room['entrance']
+room['entrance'].w_to = room['passage2']
 room['passage2'].w_to = room['passage2']
 
 
@@ -55,21 +55,21 @@ room['passage2'].w_to = room['passage2']
 #
 # If the user enters "q", quit the game.
 
-player_name = "Calli"
+player_name = input("\nPlease enter your name\n")
 starting_room = room["outside"]
 player = Player(player_name, starting_room)
 
 while True:
     current_room = player.current_room
 
-    print(f'You are now at the {current_room.name}\n')
+    print(f'You are now at the {current_room.name}.\n')
     print(current_room.description)
     print("\nWhich direction would you like to move?")
     
     cmd = input("Enter 'n', 's', 'e', or 'w' ('q' to quit): ")
 
     if cmd == 'q':
-        print("\nSee you again soon!\n")
+        print(f'\nThanks for playing, {player_name}. See you again soon!\n')
         break
     
     if cmd == 'n':
@@ -77,24 +77,24 @@ while True:
             print("\nWalking north...\n")
             player.current_room = current_room.n_to
         else:
-            print("You cannot walk north from here. Please try another direction.")
+            print(f'\nSorry, {player_name}, you cannot walk north from here. Please try another direction.\n')
     elif cmd == 's':
         if hasattr(current_room, 's_to'):
             print("\nWalking south...\n")
             player.current_room = current_room.s_to
         else:
-            print("You cannot walk south from here. Please try another direction.")
+            print(f'\nSorry, {player_name}, you cannot walk south from here. Please try another direction.\n')
     elif cmd == 'e':
         if hasattr(current_room, 'e_to'):
             print("\nWalking east...\n")
             player.current_room = current_room.e_to
         else:
-            print("You cannot walk east from here. Please try another direction.")
+            print(f'\nSorry, {player_name}, you cannot walk east from here. Please try another direction.\n')
     elif cmd == 'w':
         if hasattr(current_room, 'w_to'):
             print("\nWalking west...\n")
             player.current_room = current_room.w_to
         else:
-            print("\nYou cannot walk west from here. Please try another direction.\n")
+            print(f'\nSorry, {player_name}, you cannot walk west from here. Please try another direction.\n')
     else:
         print('\nInvalid input, please try again.\n')
