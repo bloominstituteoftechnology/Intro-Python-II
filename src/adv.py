26160382bb02ec58a7b2ba28c19a85b83724ac81
Jose <pjose14.jp38@yahoo.com>
2020-07-14 21:43:14 -0500

    files room.py and player.py are done. working on creating funtion.

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,7 @@
 from room import Room
+from player import Player
+
+import textwrap
 
 # Declare all the rooms
 
@@ -38,6 +41,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player("Jose", room["outside"])
 
 # Write a loop that:
 #
@@ -49,3 +53,32 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+user_is_playing = True
+#user will then get a welcome message 
+print("Welcome to the Adventure Game 2020")
+
+#loop starts here
+while user_is_palying:
+    #print current room and description
+    print(player.curret_room)
+    for line in textwrap.wrap(player.current_room.description, width=200):
+        print(line)
+    #take user input
+    user_cmd = input(
+        "[n] North   [s] South  [e] East  [w] West  [q] Quit\n").lower()
+    print(f"You chose {user_cmd}")
+    
+    #user choose a cardinal direction
+    #if user inputs q, quit the game 
+    if user_cmd == "q":
+        print("Bye, come back again")
+        exit()
+        
+    #if user inputs 4 directions 
+    
+    #if the direction is available/not none then got to that room
+    
+    #else the option is not available/none then throw error and back to use input 
+    
+    #any other input will throw error
