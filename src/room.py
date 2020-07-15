62e02a7a8276ccf7853a1f9f0d7839fde8477165
David Wright <davidtwright89@gmail.com>
2020-07-15 03:35:15 -0400

    Added an items attribute to the Room class

--- src/room.py
+++ src/room.py
@@ -1,10 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+from item import Item
+
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=[]):
         self.name = name
         self.description = description
-    
-    def __str__(self):
-        return f"Room: {self.name}, description: {self.description}"
+        self.items = items
