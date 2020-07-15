2539708f51a05b3fb600d129f22a617f1b756fca
RyanSWest <west56309@gmail.com>
2020-07-14 20:39:42 -0500

    1st commit

--- src/player.py
+++ src/player.py
@@ -1,2 +1,27 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, current_room):
+
+        self.name = name
+        self.room = current_room
+        self.item = []
+
+    def get_item(self, item):
+        self.item.append(item)
+
+    def remove_item(self, item):
+        return self.item.remove(item)
+
+    def print_item(self, item):
+        if len(self.item) >0:
+            print('{self.name} has these items:')
+            for i in self.item:
+                print(i)
+        
+        else: 
+            print ('No items in your inventory.')
+
+    def __str__(self):
+      s =  f'{self.name} is located in{self.current_room}'
+      return s
--- src/room.py
+++ src/room.py
@@ -1,2 +1,18 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+from item import Item
+
+
+
+class Room:
+    def __init__(self, name, description, n_to = 0, e_to=0,s_to=0,w_to=0):
+    
+
+        self.name = name
+        self.description = description
+        self.n_to = n_to
+        self.e_to = e_to
+        self.s_to = s_to
+        self.w_to = w_to
+        self.item =[]
