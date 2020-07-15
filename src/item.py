39965c455dc05612b9802d2f70e19968d8b4e726
David Wright <davidtwright89@gmail.com>
2020-07-15 03:34:06 -0400

    Added an Item class

---
+++ src/item.py
@@ -0,0 +1,13 @@
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+    
+    def __str__(self):
+        return f"{self.name}: {self.description}"
+    
+    def on_take(self):
+        print(f"\nYou have picked up the {self.name} and added it to your inventory.\n")
+    
+    def on_drop(self):
+        print(f"\nYou dropped the {self.name}.\n")
