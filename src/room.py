86c2cc7a9627f687338edb779be6c045cb5ba76c
David Wright <davidtwright89@gmail.com>
2020-07-15 03:41:25 -0400

    Removed an unnecessary import statement from room.py

--- src/room.py
+++ src/room.py
@@ -1,8 +1,6 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
-from item import Item
-
 class Room:
     def __init__(self, name, description, items=[]):
         self.name = name
