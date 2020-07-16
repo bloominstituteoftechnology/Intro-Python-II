bb0020c15492a4464bfe9a1e3f5cca2c761052f4
Crystal Knight <crystalknightcodes@gmail.com>
2020-07-15 23:53:59 -0400

    Created Item class.

---
+++ src/item.py
@@ -0,0 +1,24 @@
+class Item:
+    def __init__(self, name, description, iscontainer, isopen, islocked, isheavy):
+        self.name = name
+        self.description = description
+        self.iscontainer = iscontainer
+        self.isopen = isopen
+        self.islocked = islocked
+        self.isheavy = isheavy
+    
+    def __str__(self):
+        return f"{self.name}: {self.description}"
+    
+    def on_take(self):
+        if self.isheavy == True:
+            print(f"\nThe {self.name} is too heavy to pick up.\n")
+        else:
+            print(f"\nYou pick up the {self.name}.\n")
+    
+    def on_drop(self):
+        print(f"\nYou drop the {self.name}.\n")
+
+    def is_in_container(self, value, container):
+        self.value = False
+        self.container = container
