2c572f27d775b30833b254be2fc0bbbdae819896
Crystal Knight <crystalknightcodes@gmail.com>
2020-07-15 23:54:58 -0400

    Expanded the Player class to be able to interact with items.

--- src/player.py
+++ src/player.py
@@ -2,11 +2,79 @@
 # currently.
 
 from room import Room
+from item import Item
+
+direction_names = {
+    'n': 'North',
+    's': 'South',
+    'e': 'East',
+    'w': 'West'
+    }
 
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, inventory=[]):
         self.name = name
         self.current_room = current_room
+        self.inventory = inventory
 
     def __str__(self):
-        return f"{self.name} is currently in room: {self.current_room.name}."
+        num_items = len(self.inventory)
+
+        if len(num_items) > 0:
+            s = "\n    "
+            item_descriptions = s + s.join(str(item) for item in self.inventory)
+        else:
+            item_descriptions = "none"
+        
+        return f"""Player:
+            Name: {self.name}
+            Current room: {self.current_room.name}
+            Items: {item_descriptions}
+        """
+
+    def try_direction(self, cmd):
+        attribute = cmd + '_to'
+        if hasattr(self.current_room, attribute):
+            self.current_room = getattr(self.current_room, attribute)
+            self.print_location_status()
+        else:
+            print(f"\nYou cannot move {direction_names[cmd]} from here. Please try another direction.\n")
+    
+    def print_location_status(self):
+        print(f'\nYou are now at the {self.current_room.name}.')
+        print(f"{self.current_room.description}")
+
+        num_items = len(self.current_room.items)
+
+        if num_items > 0:
+            s = "\n    "
+            item_descriptions = s + s.join(str(item) for item in self.current_room.items)
+            item_plural = "item" if num_items == 1 else "items"
+            print(f"There are {num_items} {item_plural} here:{item_descriptions}\n")
+        else:
+            print("There is nothing of interest here.\n")
+    
+    def try_add_item_to_inventory(self, item_name):
+        for item in self.current_room.items:
+            if item.name == item_name:
+                if item.isheavy == False:
+                    self.inventory.append(item)
+                    self.current_room.items.remove(item)
+                    item.on_take()
+                    break
+                else:
+                    item.on_take()
+                    break
+            else:
+                print(f"\nYou can't find the \"{item_name}\".\n")
+
+    def try_drop_item_from_inventory(self, item_name):
+        for item in self.inventory:
+            if item.name == item_name:
+                self.current_room.items.append(item)
+                self.inventory.remove(item)
+                item.on_drop()
+                print(f"The {item_name} now lays on the ground.\n")
+                break
+        else:
+            print(f"\nYou can't find the \"{item_name}\".\n")
