07566fadfc6fa036ca0cd1ff93da36d1ca570162
rubyclaguna <rubyclaguna@gmail.com>
2020-07-15 17:03:03 -0700

    mvp 1

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -39,6 +40,9 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player("Ruby", room['outside'])
+
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +53,17 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+directions = ['n', 's', 'e', 'w']
+
+print(player.room)
+
+while True:
+    cmd = input("Please enter a direction : ")
+    if cmd in directions:
+        player.move(cmd)
+    elif cmd == 'q':
+        print("Thanks for Playing! :)")
+        break
+    else:
+        print("Oops! Choose a valid direction.")
--- src/player.py
+++ src/player.py
@@ -1,2 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player: 
+    def __init__(self, name, room):
+        self.name = name 
+        self.room = room 
+    
+    def move(self, direction): 
+        if direction in ['n', 's', 'e', 'w']: 
+            next_room = self.room.get_direction(direction)
+            if next_room is not None:
+                self.room = next_room
+                print(self.room)
+            else: 
+                print("Oops! Can't move in that direction.")
