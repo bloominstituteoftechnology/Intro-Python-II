928df3e0bf85e34660948744c45e70944df04515
Crystal Knight <crystalknightcodes@gmail.com>
2020-07-15 23:55:30 -0400

    Simplified Room class.

--- src/room.py
+++ src/room.py
@@ -2,9 +2,7 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=[]):
         self.name = name
         self.description = description
-    
-    def __str__(self):
-        return f"Room: {self.name}, description: {self.description}"
+        self.items = items
