102a5556b3465d52f025d42666cc36f7c9f8da8b
Crystal Knight <crystalknightcodes@gmail.com>
2020-07-15 23:56:09 -0400

    Fixed some formatting.  Added rooms for future use.  Added items to rooms.

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,51 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("outside cave entrance",
-                     "North of you, the cave mouth beckons"),
+    'outside': Room("cave entrance", "North of you, the cave mouth beckons."),
 
-    'entrance':    Room("entrance", """Dim light filters in from the south. Dusty
-passages run north, east and west."""),
+    'entrance': Room("entrance", """Dim light filters in from the south. Dusty passages run north, east and west."""),
 
-    'cliff': Room("steep cliff", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'cliff': Room("steep cliff", """A steep cliff appears before you, falling into the darkness. 
+    Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
 
-    'passage1':   Room("narrow passage", """A narrow passage bends here from west
-to north."""),
+    'passage1': Room("narrow passage", """A narrow passage bends here from west to north."""),
 
-    'chamber': Room("empty chamber", """You've found a small room with musty old chest in it. The only exit is to the south."""),
+    'chamber': Room("chamber", """You've found a small alcove. The only exit is to the south."""),
 
-    'passage2':   Room("wide passage", """A wide passage stretches out before you. You can't see the end of it. You think you see some faint light in the east."""),
+    'passage2': Room("wide passage", """A wide passage stretches out before you. You can't see the end of it. 
+    You think you see some faint light in the east."""),
+    # Future rooms, accessible only by hookshot.
+    'chasm': Room("other side of the chasm", """You've made it over the chasm! But there is no way to get back across. 
+    A faint light flickers in the passage to the north. Hopefully it leads to a way out."""),
+
+    'passage3': Room("dimly lit passage", """A musty smell fills the air in this damp passage.  
+    There is a dim light coming from the north, or you can walk into the darkness to the south."""),
+
+    'cavern': Room("large cavern", """The passage leads you into a large cavern with a small lake.  
+    There is a hole in the ceiling here.  Light is streaming through the hole, making the lake glimmer.""")
+}
+
+#Declare all the items
+
+item = {
+    'sword': Item("rusty sword", """A rusty sword lies here.""", False, False, True, False),
+
+    'coin': Item("gold coin", """There is a small gold coin here. Might be valuable.""", False, False, True, False),
+
+    'key': Item("key", """A dingy key lies here. Might be useful.""", False, False, True, False),
+
+    'rations': Item("rations", """Some old military rations are here. Hopefully they haven't expired.""", False, False, True, False),
+
+    'hookshot': Item("hookshot", """A spring-loaded, trigger-pulled hook attached to
+        lengthy chains. It can can attack enemies at a distance, 
+        retrieve remote items, and attach onto certain surfaces 
+        (like wood) to pull you across large distances.""", False, False, True, False),
+
+    'chest': Item("chest", """A dusty old chest lies in the corner here.""", True, False, True, True),
 }
 
 
@@ -36,6 +62,16 @@ room['passage1'].w_to = room['entrance']
 room['passage2'].e_to = room['entrance']
 room['entrance'].w_to = room['passage2']
 room['passage2'].w_to = room['passage2']
+# Chasm will be north of the cliff, once the hookshot is functional.
+room['chasm'].n_to = room['passage3']
+room['passage3'].s_to = room['chasm']
+room['passage3'].n_to = room['cavern']
+room['cavern'].s_to = room['passage3']
+
+# Add items to room
+room['cliff'].items = [item['key']]
+room['chamber'].items = [item['chest']]
+
 
 
 #
@@ -55,46 +91,43 @@ room['passage2'].w_to = room['passage2']
 #
 # If the user enters "q", quit the game.
 
-player_name = input("\nPlease enter your name\n")
-starting_room = room["outside"]
-player = Player(player_name, starting_room)
+def print_commands():
+    print("""Valid commands:
+    \'n\', \'s\', \'e\', or \'w\'   Move North, South, East, or West.
+    \'take <item>\'           Pickup an item, where <item> is the item name.
+    \'drop <item>\'           Drop an item, where <item> is the item name.
+    \'q\'                     Quit\n""")
 
+# Begin Program
+possible_directions = ['n', 's', 'e', 'w']
+player_name = input("\nPlease enter your name.\n")
+print(f"\nHi {player_name}.  Good luck on your adventure.")
+player = Player(player_name, room['outside'])
+player.print_location_status()
+print_commands()
+
+# REPL 
 while True:
-    current_room = player.current_room
+    cmd = input("What would you like to do?\n").strip().lower().split()   
+    num_words = len(cmd)
 
-    print(f'You are now at the {current_room.name}.\n')
-    print(current_room.description)
-    print("\nWhich direction would you like to move?")
-    
-    cmd = input("Enter 'n', 's', 'e', or 'w' ('q' to quit): ")
-
-    if cmd == 'q':
-        print(f'\nThanks for playing, {player_name}. See you again soon!\n')
-        break
-    
-    if cmd == 'n':
-        if hasattr(current_room, 'n_to'):
-            print("\nWalking north...\n")
-            player.current_room = current_room.n_to
+    if num_words == 1:
+        cmd = cmd[0]
+        if cmd == 'q':
+            print(f"\nSee you soon, {player_name}.\n")
+            break
+        if cmd in possible_directions:
+            player.try_direction(cmd)
+            continue
+    elif num_words == 2:
+        verb = cmd[0]
+        item_name = cmd[1]
+        if verb == 'get' or verb == 'take':
+            player.try_add_item_to_inventory(item_name)
+            continue
+        elif verb == 'drop':
+            player.try_drop_item_from_inventory(item_name)
+            continue
         else:
-            print(f'\nSorry, {player_name}, you cannot walk north from here. Please try another direction.\n')
-    elif cmd == 's':
-        if hasattr(current_room, 's_to'):
-            print("\nWalking south...\n")
-            player.current_room = current_room.s_to
-        else:
-            print(f'\nSorry, {player_name}, you cannot walk south from here. Please try another direction.\n')
-    elif cmd == 'e':
-        if hasattr(current_room, 'e_to'):
-            print("\nWalking east...\n")
-            player.current_room = current_room.e_to
-        else:
-            print(f'\nSorry, {player_name}, you cannot walk east from here. Please try another direction.\n')
-    elif cmd == 'w':
-        if hasattr(current_room, 'w_to'):
-            print("\nWalking west...\n")
-            player.current_room = current_room.w_to
-        else:
-            print(f'\nSorry, {player_name}, you cannot walk west from here. Please try another direction.\n')
-    else:
-        print('\nInvalid input, please try again.\n')
+            print("\nInvalid input, please try again.")
+    print_commands()
