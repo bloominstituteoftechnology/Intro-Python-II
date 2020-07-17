b9376b0b6bfdf9c340b370d208a8c3bd89460bf8
Veto Ramirez <vetorobert3@gmail.com>
2020-07-16 17:44:18 -0700

    Saving

--- src/adv.py
+++ src/adv.py
@@ -38,6 +38,11 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+class Player:
+  def __init__(self, name, location):
+  #attributes
+  self.name = name
+  self.location = location
 
 # Write a loop that:
 #
