5268e0bb9e4556b45acaff4edd8b3461bb04b87f
Crystal Knight <crystalknightcodes@gmail.com>
2020-07-16 23:06:55 -0400

    Tweaked some formatting.  Added function to look at inventory.

--- src/player.py
+++ src/player.py
@@ -41,7 +41,7 @@ class Player:
             print(f"\nYou cannot move {direction_names[cmd]} from here. Please try another direction.\n")
     
     def print_location_status(self):
-        print(f'\nYou are now at the {self.current_room.name}.')
+        print(f'You are now at the {self.current_room.name}.')
         print(f"{self.current_room.description}")
 
         num_items = len(self.current_room.items)
@@ -50,7 +50,7 @@ class Player:
             s = "\n    "
             item_descriptions = s + s.join(str(item) for item in self.current_room.items)
             item_plural = "item" if num_items == 1 else "items"
-            print(f"There are {num_items} {item_plural} here:{item_descriptions}\n")
+            print(f"You see {num_items} {item_plural} here:{item_descriptions}\n")
         else:
             print("There is nothing of interest here.\n")
     
@@ -60,10 +60,10 @@ class Player:
                 if item.isheavy == False:
                     self.inventory.append(item)
                     self.current_room.items.remove(item)
-                    item.on_take()
+                    item.take()
                     break
                 else:
-                    item.on_take()
+                    item.take()
                     break
             else:
                 print(f"\nYou can't find the \"{item_name}\".\n")
@@ -73,8 +73,14 @@ class Player:
             if item.name == item_name:
                 self.current_room.items.append(item)
                 self.inventory.remove(item)
-                item.on_drop()
+                item.drop()
                 print(f"The {item_name} now lays on the ground.\n")
                 break
+            else:
+                print(f"\nYou can't find the \"{item_name}\".\n")
+    
+    def look_at_inventory(self):
+        if len(self.inventory) >= 1:
+            print(self.__str__())
         else:
-            print(f"\nYou can't find the \"{item_name}\".\n")
+            print("There are no items in your inventory.")
