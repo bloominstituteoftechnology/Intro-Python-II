fade537ded954c546f5ee17f57b557722471e5ef
Chris <chris.gonzales.82@gmail.com>
2020-08-05 12:46:12 -0700

    Day 1 MVP

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,18 @@
 from room import Room
+from player import Player
+import argparse
+import sys
+
+# Argument Parser
+
+# explore_parser = argparse.ArgumentParser()
+# explore_parser.add_argument("n", "--north", help="Moves player north")
+# explore_parser.add_argument("w", "--west", help="Moves player west")
+# explore_parser.add_argument("s", "--south", help="Moves player south")
+# explore_parser.add_argument("e", "--east", help="Moves player east")
+# explore_parser.add_argument("q", "--quit", help="Quits the game")
+
+# args = explore_parser.parse_args()
 
 # Declare all the rooms
 
@@ -37,15 +51,32 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
-# Make a new player object that is currently in the 'outside' room.
+directions = ['n', 's', 'e', 'w']
+done = False
 
+# Make a new player object that is currently in the 'outside' room.
+player1 = Player('Chris', room["outside"])
 # Write a loop that:
-#
+while not done:
 # * Prints the current room name
+    print(f'\n{player1.current_room}')
 # * Prints the current description (the textwrap module might be useful here).
+    print(f'\ntest room')
 # * Waits for user input and decides what to do.
-#
+    commands = input('> ').split(',')
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
-#
+    for command in commands:
 # If the user enters "q", quit the game.
+        if command == 'q':
+            sys.exit(1)
+        elif command == 'n':
+            print('n pressed')
+            #check if the player can move to the north
+            # if there is, set that room to the player's location
+        elif command == 's':
+            print('s pressed')
+        elif command == 'e':
+            print('e pressed')
+        elif command == 'w':
+            print('w pressed')
