f82eb9c9b0d835175e3c14305d0df32da2064625
Lambda_School_Loaner_277 <lambda_school_loaner_277@Lambda-School-Loaner-277s-MacBook-Pro.local>
2020-08-05 18:19:48 -0400

    Second Commit

--- src/adv.py
+++ src/adv.py
@@ -22,6 +22,10 @@ to north. The smell of gold permeates the air."""),
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
+
+    'den': Room("Strange Den", """You stumble into the den of a tall lanky eldritch creature.
+It quickly grabs hold of you with it's fingers, each one matching the length of your arms.
+As it draws you closer, everything fades to black."""),
 }
 
 
@@ -50,15 +54,15 @@ player = Player(
 item = {
     'lighter': Item("lighter", "Half empty lighter", 1),
     'survival_knife': Item("survival knife", "Fishing knife", 2),
-    'note1': Item("bloody dress", "Bloddy dress on the floor",  3),
-    'note2': Item("torn note", "A half of a note", 4),
-    'note3': Item("old shoe", "Old dress shoe", 5),
+    'bloody dress': Item("bloody dress", "Bloody dress on the floor",  3),
+    'torn note': Item("torn note", "A half of a note", 4),
+    'old shoe': Item("old shoe", "Old dress shoe", 5),
 }
 
 # Add Items to Rooms:
 room['outside'].addItem(item['survival_knife'])
 room['foyer'].addItem(item['lighter'])
-room['foyer'].addItem(item['bloddy dress'])
+room['foyer'].addItem(item['bloody dress'])
 room['overlook'].addItem(item['torn note'])
 room['narrow'].addItem(item['old shoe'])
 # Write a loop that:
@@ -71,3 +75,52 @@ room['narrow'].addItem(item['old shoe'])
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+# Start The Game:
+game_running = True
+
+# Intro Text:
+print(f"\nYour name is {player.name}...\n")
+print("The sky thunders as it begins to pour.\nYou see an eerie cave entrance straight ahead of you.\nTo view your controls, enter 'C'.\n")
+
+# Create Input Variable:
+player_input = ""
+
+while game_running == True:
+
+    # Quit Function:
+    if player_input.lower() == "q":
+        print("\nThank you for playing!")
+        quit()
+
+   # Take Item Function:
+    if player.current_room.items != []:
+        for i in player.current_room.items:
+            if player_input.lower() == f"take {i.name}":
+                player.takeItem(i)
+                player.current_room.removeItem(i)
+    else:
+        if "take" in player_input.lower():
+            print("There is nothing to take.")
+
+    # Drop Item Function:
+    if player.items != []:
+        for i in player.items:
+            if player_input.lower() == f"drop {i.name}":
+                player.dropItem(i)
+                player.current_room.addItem(i)
+    else:
+        if "drop" in player_input:
+            print("You don't have any items...")
+
+    # Drop Item Function:
+        if player.items != []:
+            for i in player.items:
+                if player_input.lower() == f"drop {i.name}":
+                    player.dropItem(i)
+                    player.current_room.addItem(i)
+        else:
+            if "drop" in player_input:
+                print("You don't have any items...")
+
+                # Repeating Message:
+        player_input = str(input("What will you do?: "))
