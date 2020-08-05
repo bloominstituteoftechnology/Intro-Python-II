0ee888cd46434351b2a347197fe93f87801cf5e1
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-08-05 15:28:19 -0700

    MVP Day 1

--- src/player.py
+++ src/player.py
@@ -1,2 +1,18 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from src.room import Room
+
+class Player():
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+        self.item = []
+
+    def move(self, direction):
+        '''This function controls player movement'''
+        new_room = self.current_room.get_direction(direction)
+        if new_room is not None:
+            self.current_room = new_room
+            print(self.current_room)
+        else:
+            print("You cannot move in this direction.")
--- src/room.py
+++ src/room.py
@@ -1,2 +1,44 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+        self.item = []
+
+    def __repr__(self):
+        returnString = f"---------------\n\n{self.name}\n\n  {self.description}\n\n---------------"
+        returnString += f"\n\n[{self.get_exit()}]\n\n"
+        return returnString
+
+    def get_direction(self, direction):
+        """This function points the player to the inputted direction """
+        if direction in ["n", "N"]:
+            return self.n_to
+        elif direction in ["s", "S"]:
+            return self.s_to
+        elif direction in ["w", "W"]:
+            return self.w_to
+        elif direction in ["e", "E"]:
+            return self.e_to
+        else:
+            return None
+
+    def get_exit(self):
+        exits = ["Only Available Directions are --->"]
+        if self.n_to is not None:
+            exits.append("n")
+        if self.s_to is not None:
+            exits.append("s")
+        if self.e_to is not None:
+            exits.append("e")
+        if self.w_to is not None:
+            exits.append("w")
+        return ", ".join(exits)
