b3d80658ca0c1c7301c8db40035435a925827d66
Griffinw15 <wilson.griffin15@gmail.com>
2020-08-06 17:58:10 -0600

    trying branch

--- src/player.py
+++ src/player.py
@@ -1,2 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, location):
+        self.location = location
+​
+    def try_direction(self, command):
+        attribute = command  + '_to'
+​
+        # see if the current room has the attribute 
+        # we can use a Python function called `hasattr`
+        if hasattr(self.location, attribute):
+            # use `getattr` to actually move to the room 
+            self.location = getattr(self.location, attribute)
+        else:
+            print("You can't go that way!\n")
