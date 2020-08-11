80218ca109b3666cd3d24bfbb3df1bac51cc72ce
Andrew Haberman <a.haberman25@gmail.com>
2020-08-11 14:45:16 -0400

    started project

--- src/adv.py
+++ src/adv.py
@@ -1,10 +1,11 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mouth beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -21,6 +22,10 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
+player = {
+    'name': '',
+    'room': ''
+}
 
 # Link rooms together
 
@@ -38,14 +43,26 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+start_input = input("Would you like to play a game? \n")
+while True:
+    cur_room = room['outside']    
+    if start_input == 'q':
+        break
+    elif start_input == 'y' or 'yes':
+        name_input = input("Choose a name: \n")
+        player_info = player[name_input]
+    
 
 # Write a loop that:
 #
 # * Prints the current room name
+    print(f"{player_info}")
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
 #
+    
 # If the user enters a cardinal direction, attempt to move to the room there.
+
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
+class Player(Room):
+    def __init__(self, name):
+        super().__init__(name, description)
+        self.name = name
+        
+    def __str__(self):
+        return f"{self.name}: {self.room}"
