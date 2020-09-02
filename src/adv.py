36497227dac33353de3eaaecf2c263e3cb9340cc
Cody Morley <codymorleyinc@gmail.com>
2020-09-02 16:30:12 -0700

    Day 1 MVP
    Have a working game for day 1.

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -37,8 +38,12 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
-# Make a new player object that is currently in the 'outside' room.
+directions = ['n', 's', 'e', 'w']
 
+# Make a new player object that is currently in the 'outside' room.
+player_name = input('What is your name, adventurer? ')
+player = Player(player_name, room['outside'])
+print(f"Good luck, {player.name}")
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +54,34 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+# print(f"{player.name}, you find yourself in {player.current_room.name}. \n{player.current_room.description}")
+player.look()
+
+while True:
+    user_input = input(f"Where would you like to go {player.name}? ").lower().split()
+
+    if len(user_input) > 2 or len(user_input) < 1:
+        print(f"Sorry, {player.name}, that's not a valid one or two word command. Would you like 'help'?")
+    
+    else:
+        if user_input[0] == "q" or user_input[0] == "quit":
+            print(f"Thanks for playing {player.name}!") 
+            break
+
+        if user_input[0] == "h" or user_input[0] == "help":
+            print("Commands:\n'n' - Move North\n's' - Move South\n'e' - Move East\n'w' - Move West\n'l' or 'look' - Look around the current room\n'h' or 'help' - Help Menu\n'q' or 'quit' - Exit Game\n")
+            continue
+
+        if user_input[0] == "l" or user_input[0] == "look":
+            player.look()
+            continue
+
+        if user_input[0] in directions:
+            try:
+                player.change_room(user_input[0])
+                print(f"You are in the {player.current_room.name}. \n{player.current_room.description}")
+            except AttributeError:
+                print(f"{player.name}'s adventure lies elsewhere.'")
+        else:
+            print('Movement not allowed! Please enter a direction (n, s, e, w) to move around the map')
--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+     def __init__(self, name, description):
+         super().__init__()
+         self.name = name
+         self.description = description
+
+     def __str__(self):
+         return f"{self.name}: \n{self.description}"
