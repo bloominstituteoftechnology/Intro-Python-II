60694b3b441d6d1ed69f6251bdcf633724d1643f
bekah <elisaalvarez211@gmail.com>
2020-09-03 00:24:15 -0700

    MVP this game is horrible

--- src/player.py
+++ src/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+   def __init__(self,name,location):
+       self.name= name
+       self.location = location
+   def __str__(self):
+       return "{} is located {}" .format(self.name,self.location)
