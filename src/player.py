95c30d7a042aff7a950a4941ce451cd462e49a10
Cody Morley <codymorleyinc@gmail.com>
2020-09-03 13:18:33 -0700

    Player
    added properties and methods to player to allow an inventory system. 
    Added an empty array to the rooms class to hold items

--- src/player.py
+++ src/player.py
@@ -1,11 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 from room import Room
+from item import Item
 
 class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
     
     def __str__(self):
         print(f"{self.name} is currently in {self.current_room}.")
@@ -16,3 +18,24 @@ class Player:
 
     def look(self):
         print(f"{self.name}, you find yourself in {self.current_room.name}. \n{self.current_room.description}")
+
+    def show_inventory(self):
+        if len(self.inventory) <= 0:
+            print(f"You haven't picked up anything yet, {self.name}.")
+        elif len(self.inventory) == 1:
+            print(f"You have 1 item in your inventory, {self.name}")
+            print(f"{self.inventory[0].name}: {self.inventory[0].description}")
+        else:
+            print(f"You have {len(self.inventory)} items in your inventory.")
+            for item in self.inventory:
+                print(f"\n{item.name}: {item.description}")
+    
+    def take(self, item):
+        self.inventory.append(item)
+        self.current_room.items.remove(item)
+
+    def drop(self, item):
+        for i in self.inventory:
+            if i == item:
+                self.inventory.remove(item)
+        self.current_room.items.append(item)
--- src/room.py
+++ src/room.py
@@ -1,11 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from item import Item
 
 class Room:
      def __init__(self, name, description):
          super().__init__()
          self.name = name
          self.description = description
+         self.items = []
 
      def __str__(self):
          return f"{self.name}: \n{self.description}"
