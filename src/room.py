d1a25f0c7909f4f416b76079af097fd9b3b950ed
nimu77 <nirmal.thapa77@outlook.com>
2020-09-03 15:48:01 -0700

    added item file and working on adding items to rooms

--- src/room.py
+++ src/room.py
@@ -1,11 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
-
+from item import Item
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=[]):
         self.name = name
         self.description = description
+        self.items = Item('sword', 'deadly weapon')
         # self.n_to = n_to
         # self.s_to = s_to
         # self.e_to = e_to
@@ -14,4 +15,4 @@ class Room:
     def __str__(self):
         return f'{self.name}, {self.description}'
 
-    def 
+    # def 
