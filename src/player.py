6fae3ad436f5104facdfe08b0e23d06ef914ae46
Desi Curry <Blackice@Desis-MacBook-Air.local>
2020-09-04 13:18:26 -0400

    Day 2 MVP

--- src/player.py
+++ src/player.py
@@ -5,6 +5,13 @@ class Player():
     def __init__(self, name, room):
         self.name = name
         self.room = room
+        self.inventory = []
 
     def __str__(self):
         return f"Player: {self.name}, {self.room}"
+
+    def take(self, item):
+        self.inventory.append(item)
+
+    def drop(self, item):
+        del self.inventory[item]
--- src/room.py
+++ src/room.py
@@ -1,10 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from item import Item
 
 class Room:
-    def __init__(self, room_name, room_description):
+    def __init__(self, room_name, room_description, item):
         self.room_name = room_name
         self.room_description = room_description
+        self.item = [item]
         self.n_to = None
         self.s_to = None
         self.e_to = None
@@ -12,3 +14,9 @@ class Room:
 
     def __str__(self):
         return f"Room: {self.room_name}, Description: {self.room_description}"
+
+    def remove_item(self, item):
+        del self.item[item]
+
+    def add_item(self, item):
+        self.item.append(item)
