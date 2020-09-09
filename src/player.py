6a3321f0e0a728d9b519a2e2cab569faa53ef752
ameralhomdy <ameralhomdy97@gmail.com>
2020-09-08 21:19:49 -0700

    finished player.py

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, current_room):
+        self.current_room = current_room
+
+    def move(self, direction):
+        next_room = self.current_room.get_direction(direction)
+        if next_room != None:
+            self.current_room = next_room
+        else:
+            print("You can't move that way!!")
