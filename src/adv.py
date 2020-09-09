ac6df7dfe12496c46f8077ffd361dc39cd1f73b4
Julio Escalera <27julioescalera27@gmail.com>
2020-09-08 20:22:16 -0700

    Day 1 MVP

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -36,16 +37,45 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
-
+username = input("Please put in your name: ")
 # Make a new player object that is currently in the 'outside' room.
-
+player = Player(username, room['outside'])
 # Write a loop that:
-#
-# * Prints the current room name
+
+while player.current_room:
+    # * Prints the current room name
+    print(f"Current room: {player.current_room.name}")
 # * Prints the current description (the textwrap module might be useful here).
+    print(f"{player.current_room.description}")
+    print("-------------------------------------------------------------------------")
 # * Waits for user input and decides what to do.
-#
+    selection = input("Please pick a direction to go in (North = n, South = s, West = w, East = e): ")
+    print('-------------------------------------------------------------------------')
 # If the user enters a cardinal direction, attempt to move to the room there.
+    if selection == 'n':
+        if player.current_room.n_to is None:
+            print(f"There is nothing north of {player.current_room.name}. Please pick another direction.")
+        else:
+            player.current_room = player.current_room.n_to
+    elif selection == 's':
+        if player.current_room.s_to is None:
+            print(f"There is nothing south of {player.current_room.name}. Please pick another direction.")
+        else:
+            player.current_room = player.current_room.s_to
+    elif selection == 'w':
+        if player.current_room.w_to is None:
+            print(f"There is nothing west of {player.current_room.name}. Please pick another direction.")
+        else:
+            player.current_room = player.current_room.w_to
+    elif selection == 'e':
+        if player.current_room.e_to is None:
+            print(f"\nThere is nothing east of {player.current_room.name}. Please pick another direction.")
+        else:
+            player.current_room = player.current_room.e_to
+    elif selection == 'q':
+        print("Thank you for playing!")
+        player.current_room = False # Get out of game
 # Print an error message if the movement isn't allowed.
-#
+    else:
+        print(selection + " wasn't allowed. Please pick another valid direction.")
 # If the user enters "q", quit the game.
