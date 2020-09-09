e890e21bdcefc4ab62b88beb1860f10a88882199
Claudia Maciel <clc_80@yahoo.com>
2020-09-08 18:40:37 -0700

    Added player and room classes

--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        # The room should also have n_to, s_to, e_to, and w_to attributes which point to the room in that respective direction.
+
+    def __str__(self):
+        return f"This is {self.name}"
