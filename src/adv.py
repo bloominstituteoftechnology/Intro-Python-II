cbce9e30166467fdd8e5229658acb9f4c760d59c
Temtsel-Och Ulziisaikhan <temkalways@gmai.com>
2020-09-10 17:18:20 -0400

    Completed MVP

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,9 @@
 from room import Room
+from player import Player 
+from item import Item
+import sys
+
+
 
 # Declare all the rooms
 
@@ -33,9 +38,8 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
+
 # Main
-#
 
 # Make a new player object that is currently in the 'outside' room.
 
@@ -49,3 +53,107 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+player = Player("player1", room['outside'])
+
+room['treasure'].item_list.append(Item("sword", "Level 1 sword"))
+room['overlook'].item_list.append(Item("map", "Level 1 map"))
+
+while True:
+    print(f"Current room: {player.current_room.name}")
+    print(f"Description: {player.current_room.description}")
+
+    print("Items in the room: ")
+    for item in player.current_room.item_list: 
+        print(f"{item.name} - {item.description}")
+
+    print("Player inventory: ")
+    for item in player.inventory:
+        print(f"{item.name} - {item.description}")
+
+    move = input("Enter a move: ")
+
+    if len(move.split()) == 1:
+        if move != "s" and move != "n" and move != "e" and move != "w" and move != "q":
+            print("Invalid move!!!")
+            print("Enter 's' to move south")
+            print("Enter 'n' to move north")
+            print("Enter 'w' to move west")
+            print("Enter 'e' to move east")
+            print("Enter 'q' to quit the game")
+
+        elif move == "q":
+            print("The player quit the game!")
+            sys.exit()
+            
+        elif move == "s":
+            if player.current_room.n_to:
+                player.current_room = player.current_room.n_to
+            else:
+                print("Unable to move in that direction!")
+
+        elif move == "n":
+            if player.current_room.s_to:
+                player.current_room = player.current_room.s_to
+            else:
+                print("Unable to move in that direction!")
+
+        elif move == "w":
+            if player.current_room.e_to:
+                player.current_room = player.current_room.e_to
+            else:
+                print("Unable to move in that direction!")
+
+        elif move == "e":
+            if player.current_room.w_to:
+                player.current_room = player.current_room.w_to
+            else:
+                print("Unable to move in that direction!")
+
+    elif len(move.split()) == 2: 
+        if move.split()[0] == "get":
+            if len(player.current_room.item_list) > 0:
+                for item in player.current_room.item_list:
+                    if item.name == move.split()[1]:
+                        player.current_room.item_list.remove(item)
+                        player.inventory.append(item)
+                    else:
+                        print(f"There is no {move.split()[1]} in this room!")
+            else:
+                print("There are no items in this room!")
+
+        elif move.split()[0] == "take":
+            if len(player.current_room.item_list) > 0:
+                for item in player.current_room.item_list:
+                    if item.name == move.split()[1]:
+                        player.current_room.item_list.remove(item)
+                        player.inventory.append(item)
+                    else:
+                        print(f"There is no {move.split()[1]} in this room!")
+            else:
+                print("There are no items in this room!")
+
+        elif move.split()[0] == "drop":
+            if len(player.inventory) > 0:
+                for item in player.inventory:
+                    if item.name == move.split()[1]:
+                        player.inventory.remove(item)
+                        player.current_room.item_list.append(item)
+                    else:
+                        print(f"You do not have {move.split()[1]} in your inventory!")
+
+            else: 
+                print("You have no items in your inventory!")
+
+        else:
+            print("Invalid move!!!")
+
+    else:
+        print("Invalid move!!!")
+
+
+
+
+
+
+
