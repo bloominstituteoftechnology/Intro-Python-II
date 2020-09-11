bef5cbe656c452f25ce46bbc88ee321444e5c838
Julio Escalera <27julioescalera27@gmail.com>
2020-09-10 20:37:54 -0700

    Incomplete MVP for Day 2 - Need to Add verb and item commands to parser

--- src/player.py
+++ src/player.py
@@ -1,14 +1,19 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, storage):
         self.name = name
         self.current_room = current_room
+        self.storage = storage
 
     def __str__(self):
-        return f"Name: {self.name}\nCurrent room: {self.current_room}"
+        return f"Name: {self.name}\n" \
+               f"Current room: {self.current_room}\n" \
+               f"Stuff on player: {self.storage}"
 
 
 if __name__ == "__main__":
-    a = Player("Bob", "Bob's Room") # __repr__ works
+    a = Player("Bob", "Bob's Room")  # __repr__ works
     print(a)
--- src/room.py
+++ src/room.py
@@ -1,17 +1,39 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from player import Player
+
 class Room:
-    def __init__(self, name, description, n_to=None, w_to=None, e_to=None, s_to=None):
+    def __init__(self, name, description, storage=[], n_to=None, w_to=None, e_to=None, s_to=None):
         self.n_to = n_to
         self.w_to = w_to
         self.e_to = e_to
         self.s_to = s_to
         self.name = name
         self.description = description
+        self.storage = storage
+
+    def
+
+
+    # def pick_item(self, item):
+    #     for i in self.storage:
+    #         if item == i:
+    #             self.storage.remove(i)
+    #             print(f"You picked up {item}")
+    #             return self.storage
+    #         else:
+    #             print(f"There's no such thing as {item}.")
+
+
 
     def __str__(self):
-        return f"Name of room: {self.name}\nDescription: {self.description}\nNorth: {self.n_to}\nWest: {self.w_to}\n" \
-               f"East: {self.e_to}\nSouth: {self.s_to}"
+        return f"Name of room: {self.name}\n" \
+               f"Description: {self.description}\n" \
+               f"Stuff that room has: {self.storage}\n" \
+               f"North: {self.n_to}\n" \
+               f"West: {self.w_to}\n" \
+               f"East: {self.e_to}\n" \
+               f"South: {self.s_to}"
 
 if __name__ == "__main__":
     a = Room("asdf", "asjd;kl;") # __str__ works
