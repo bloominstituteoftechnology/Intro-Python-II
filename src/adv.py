5bb65f43c4ae91303400f36db1f0232019daf880
Ash827 <ashley.eddings89@gmail.com>
2020-09-11 00:57:58 -0400

    Almost finished

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
@@ -20,6 +21,9 @@ to north. The smell of gold permeates the air."""),
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
+
+    'kitchen': Room("The Kitchen", """You check out some refreshments and grab
+a quick bite to eat before moving along. """),
 }
 
 
@@ -33,10 +37,19 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
+room['kitchen'].w_to = room['overlook']
 
-#
-# Main
-#
+# MVP 2 - Add items to rooms
+
+room['outside'].add_item('bricks')
+room['foyer'].add_item('mirror')
+room['foyer'].add_item('envelope opener')
+room['foyer'].add_item('matches')
+room['overlook'].add_item('magic wand')
+room['narrow'].add_item('sword')
+room['narrow'].add_item('one ring to rule them all')
+room['treasure'].add_item('stick')
+room['kitchen'].add_item('butcher knife')
 
 # Make a new player object that is currently in the 'outside' room.
 
@@ -50,8 +63,43 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+#
+# Thank you for the inspiration Anthony! I added in a place to add your name too!
 
-new_player = Player("Ashley", room['outside'])
+name = input("Welcome to my castle adventurer! Please, tell me your name. ")
+
+player = Player(name, room['outside'])
+
+print(f" Welcome, {player.name}! Let's explore!")
+
+
+def display_room_description(room):
+    end_game = False
+    print(f'You are here: {room.name}.')
+    print(room.description)
+    print("""Enter a direction (n, s, e, or w) to travel through the castle. 
+    Use i to look for items that may be hidden in each room. """)
+
+display_room_description(player.current_room)
+
+end_game = False
+
+
+def item(player, item):
+    end_game = False
+    interface = input("There is an item in here. You can 'g' get or 'd' drop it. ")
+
+    if interface == 'g':
+        player.inventory.append(item)
+        print("This new tool could be useful!")
+        player.current_room.item_taken(item)
+        game_input(player)
+    elif interface == 'd':
+        print("Moving on..")
+        game_input(player)
+    else:
+        print("Does not compute, try again. ")
+
 
 def game_input(command, player):
     end_game = False
@@ -79,21 +127,21 @@ def game_input(command, player):
             display_room_description(player.current_room)
         else:
             print("Made a wrong turn, try again!")
+    elif command == 'i':
+        print(player.hunt_for_items())
+        object = player.hunt_for_items()
+
+        if player.hunt_for_items != "Nothing here, better keep looking":
+            item(player, object)
+        else:
+            game_input(player)
     elif command == 'q':
         end_game = True
     else:
         print('Please enter a valid direction')
     return end_game
 
-
-def display_room_description(room):
-    print(f'You are in {room.name}')
-    print(room.description)
-end_game = False
-
-display_room_description(new_player.current_room)
-
 while end_game == False:
-    end_game = game_input(str(input()), new_player)
+    end_game = game_input(str(input()), player)
 
 print('Thanks for playing!')
--- src/room.py
+++ src/room.py
@@ -2,10 +2,17 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items = None):
         self.name = name
         self.description = description
         self.n_to = None
         self.e_to = None
         self.s_to = None
         self.w_to = None
+        self.items = items
+
+    def add_item(self, item_name):
+        self.item_name = item_name
+
+    def item_taken(self, item):
+        self.items = None
