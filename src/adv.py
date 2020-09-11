f0231c55fdcae1126f358f6cea77d1da83e7139d
seanacres <6416690+seanacres@users.noreply.github.com>
2020-09-11 15:17:43 -0400

    Got MVP working for day 2

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,28 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
+sword = Item("sword", "This sword has no function whatsoever")
+compass = Item("compass", "You seriously need a compass?")
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", [sword, compass]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", []),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", []),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", []),
 }
 
 
@@ -51,7 +54,7 @@ room['treasure'].s_to = room['narrow']
 #
 # If the user enters "q", quit the game.
 directions = ['n', 's', 'e', 'w']
-player = Player(room['outside'])
+player = Player(room['outside'], [])
 
 while True:
     print("\n")
@@ -60,12 +63,22 @@ while True:
     print(player.current_room.description)
     print("\n")
     
-    user_input = input('Where would you like to go? >>>')
+    user_input = input('Where would you like to go? >>>').strip().lower().split()
+    num_words = len(user_input)
 
-    if user_input == 'q':
-        print('Thanks for playing!')
-        break
-    elif user_input in directions:
-        player.move(user_input)
+    if num_words == 1:
+        user_input = user_input[0]
+        if user_input == 'q':
+            print('Thanks for playing!')
+            break
+        elif user_input in directions:
+            player.move(user_input)
+    elif num_words == 2:
+        verb = user_input[0]
+        verb_item = user_input[1]
+        if verb == 'get' or verb == 'take':
+            player.add_item(verb_item)
+        elif verb == 'drop':
+            player.drop_item(verb_item)
     else:
         print("Whoops, enter a valid command like: n, s, e, w")
--- src/player.py
+++ src/player.py
@@ -2,8 +2,9 @@
 # currently.
 
 class Player:
-    def __init__(self, current_room):
+    def __init__(self, current_room, items):
         self.current_room = current_room
+        self.items = items
     
     def move(self, direction):
         next_room = getattr(self.current_room, f'{direction}_to', None)
@@ -13,3 +14,22 @@ class Player:
         else:
             print("\n")
             print("You can't move that way")
+    
+    def add_item(self, item_name):
+        for item in self.current_room.items:
+            if item.name == item_name:
+                self.current_room.items.remove(item)
+                self.items.append(item)
+                item.on_take(item.name)
+                break
+        else:
+            print('That item is not here')
+
+    def drop_item(self, item_name):
+        for item in self.items:
+            if item.name == item_name:
+                self.items.remove(item)
+                item.on_drop(item.name)
+                break        
+        else:
+            print("You can't drop something you don't have")
