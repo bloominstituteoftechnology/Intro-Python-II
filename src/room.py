d653235a920fbf1d9825d9af0ff06fe9df718a17
Chad Parker <chad@chadparker.com>
2020-09-10 20:02:19 -0700

    Update Room class with storage & __str__

--- src/room.py
+++ src/room.py
@@ -2,6 +2,19 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, storage):
         self.name = name
         self.description = description
+        self.storage = storage
+    
+    def __str__(self):
+        output = "\n"
+        output += f"{self.name}\n"
+        output += f"{self.description}\n"
+        output += "Exits to the: "
+        output += " [North] " if hasattr(self, "n_to") else ""
+        output += " [South] " if hasattr(self, "s_to") else ""
+        output += " [East] " if hasattr(self, "e_to") else ""
+        output += " [West] " if hasattr(self, "w_to") else ""
+
+        return output
