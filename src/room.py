7cc12bcfae3e7701ba841dc5039301d076d86e83
tauan-longaretti <tauanlongaretti@gmail.com>
2020-09-11 20:01:49 -0400

    updated room class to contain a list of items and declare a function that prints the items in a room

--- src/room.py
+++ src/room.py
@@ -1,15 +1,18 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items = []):
         self.name = name
         self.description = description
+        self.items = items
 
-    def print_name(self):
-        print(f'{self.name}')
+    def print_room_info(self):
+        print(f'\n{self.name}\n\n{self.description}\n')
 
-    def print_description(self):
-        print(f'{self.description}')
-
-    def outside_routes(self):
-        return()
+    def check_items(self):
+        if len(self.items) > 0:
+            print(f'This room contains the following items:')
+            for i in self.items:
+                print(f'{i.name}: {i.description}')
+        else:
+            print('No items available in this room') 
