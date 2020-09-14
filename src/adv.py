af6f6672fe2e505c136580e406cf6e93d2da6019
Chad Parker <chad@chadparker.com>
2020-09-14 12:00:06 -0700

    Change room links from object references to string ids

--- src/adv.py
+++ src/adv.py
@@ -30,14 +30,14 @@ earlier adventurers. The only exit is to the south.""",
 
 # Link rooms together
 
-rooms['outside'].n_to = rooms['foyer']
-rooms['foyer'].s_to = rooms['outside']
-rooms['foyer'].n_to = rooms['overlook']
-rooms['foyer'].e_to = rooms['narrow']
-rooms['overlook'].s_to = rooms['foyer']
-rooms['narrow'].w_to = rooms['foyer']
-rooms['narrow'].n_to = rooms['treasure']
-rooms['treasure'].s_to = rooms['narrow']
+rooms['outside'].n_to = 'foyer'
+rooms['foyer'].s_to = 'outside'
+rooms['foyer'].n_to = 'overlook'
+rooms['foyer'].e_to = 'narrow'
+rooms['overlook'].s_to = 'foyer'
+rooms['narrow'].w_to = 'foyer'
+rooms['narrow'].n_to = 'treasure'
+rooms['treasure'].s_to = 'narrow'
 
 #
 # Main
