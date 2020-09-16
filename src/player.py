874e050a0e8644bcb9ca910c63fa6305f9e0b7f5
Chad Parker <chad@chadparker.com>
2020-09-16 12:18:47 -0700

    Add Player.storageString()

--- src/player.py
+++ src/player.py
@@ -2,7 +2,10 @@
 # currently.
 
 class Player:
-    def __init__(self, name, starting_room, storage):
+    def __init__(self, name, starting_room, storage=[]):
         self.name = name
         self.current_room = starting_room
         self.storage = storage
+    
+    def storageString(self):
+        return ', '.join([item.name for item in self.storage])
