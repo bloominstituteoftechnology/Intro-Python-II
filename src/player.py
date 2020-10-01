607e930b1e178b36a04606a96ba7db326a580c1a
Chad Parker <chad@chadparker.com>
2020-09-30 18:02:33 -0700

    Add removeItem() method to Player and Room

--- src/player.py
+++ src/player.py
@@ -9,3 +9,22 @@ class Player:
     
     def storageString(self):
         return ', '.join([item.name for item in self.storage])
+    
+    def removeItem(self, item_name_to_remove):
+        """
+        Removes item from Player storage. Returns removed item, or False if not found.
+        """
+        items_to_keep = []
+        item_found = None
+
+        for item in self.storage:
+            if item.name == item_name_to_remove:
+                item_found = item
+            else:
+                items_to_keep.append(item)
+        self.storage = items_to_keep
+        
+        if item_found:
+            return item_found
+        else:
+            return False
--- src/room.py
+++ src/room.py
@@ -18,3 +18,22 @@ class Room:
         output += " [West] " if hasattr(self, "w_to") else ""
 
         return output
+
+    def removeItem(self, item_name_to_remove):
+        """
+        Removes item from Room storage. Returns removed item, or False if not found.
+        """
+        items_to_keep = []
+        item_found = None
+
+        for item in self.storage:
+            if item.name == item_name_to_remove:
+                item_found = item
+            else:
+                items_to_keep.append(item)
+        self.storage = items_to_keep
+        
+        if item_found:
+            return item_found
+        else:
+            return False
