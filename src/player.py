7154b5b7adb5c820403c0c5b08112cf01bf80f40
Rees Harper <rees-harper@lambdastudents.com>
2021-05-03 15:56:04 -0400

    copy pasted my previous code for this project

--- src/player.py
+++ src/player.py
@@ -1,2 +1,47 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+from typing import Optional, Dict
+
+from room import Room
+from item import Item
+
+
+class Player:
+    def __init__(self,
+                 name: str,
+                 current_room: Optional[Room] = None,
+                 inventory: Optional[Dict[Item, int]] = None
+                 ):
+        self.name = name
+        self.current_room = current_room
+        self.inventory = inventory if inventory is not None else {}
+
+    def __str__(self):
+        return f"""
+        -----------------
+        Player
+        -----------------
+        name: {self.name}
+        current_room: {self.current_room}
+        inventory: {self.inventory}
+        -----------------
+        """
+
+    def grab_item(self, item: Item):
+        if item not in self.inventory:
+            self.inventory[item] = 0
+        self.inventory[item] += 1
+
+        # print(f'\nYou now have the {item.name} in your inventory.')
+        # print(f'  * Your current inventory:\n    {self.inventory}')
+
+    def drop_item(self, item: Item):
+        if item.name not in self.inventory:
+            return
+
+        self.inventory[item] -= 1
+        if self.inventory[item] == 0:
+            del self.inventory[item]
+
+        print(f'  * {item} has been removed from your inventory.')
--- src/room.py
+++ src/room.py
@@ -1,2 +1,67 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+from typing import Dict, Optional
+from item import Item
+
+
+class Room:
+    def __init__(self, name: str,
+                 description: Optional[str] = None,
+                 items: Optional[Dict[Item, int]] = None
+                 ):
+        self.name = name
+        self.description = description if description is not None else "No description given!"
+        self.items = items if items is not None else {}
+        self.branches = {
+            "north": None,
+            "east": None,
+            "south": None,
+            "west": None,
+        }
+
+    def __repr__(self):
+        return f"Room({self.name}, {self.description}, {self.items})"
+
+    def __str__(self):
+        n = self.branches["north"]
+        e = self.branches["east"]
+        s = self.branches["south"]
+        w = self.branches["west"]
+
+        items_desc = ", ".join([f"{item}" for item in self.items])
+
+        return f"""
+----------------------
+Room: {self.name}
+- Description: {self.description}
+- Items: [{items_desc}]
+- Branches: 
+    north={n.name if n is not None else "None"}, 
+    east={e.name if e is not None else "None"}, 
+    south={s.name if s is not None else "None"}
+    west={w.name if w is not None else "None"}
+        """
+
+    def add_item(self, item: Item):
+        if item not in self.items:
+            self.items[item] = 0
+        self.items[item] += 1
+
+    def remove_item(self, item: Item):
+        if item not in self.items:
+            return
+        self.items[item] -= 1
+        if self.items[item] == 0:
+            del self.items[item]
+
+    def create_branch_map(self,
+                          north: Optional["Room"],
+                          east: Optional["Room"],
+                          south: Optional["Room"],
+                          west: Optional["Room"],
+                          ):
+        self.branches["north"] = north
+        self.branches["east"] = east
+        self.branches["south"] = south
+        self.branches["west"] = west
